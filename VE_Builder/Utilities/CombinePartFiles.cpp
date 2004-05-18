e598886a1c69fbfb0fc7eee1cb9a3a890d9927ae
Steve Kirstukas <sjk60@yahoo.com>
2004-05-18 17:19:32 +0000

    added header block with license info

--- VE_Builder/Utilities/CombinePartFiles.cpp
+++ VE_Builder/Utilities/CombinePartFiles.cpp
@@ -1,14 +1,45 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2004 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: CombinePartFiles.cpp,v $
+ * Date modified: $Date: 2004/03/23 16:32:48 $
+ * Version:       $Revision: 1.3 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+// a utility created by hgx to merge fluent particle files
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
-//#include <stdlib.h>
 #include <string>
 
 int main (int argc, char **argv)
 {
    std::string inPartFileName;
-   
-   
    
    char textLine[256];
    int i,j;
@@ -26,7 +57,6 @@ int main (int argc, char **argv)
          {
             inPartFile.getline(textLine,256);
          }
-
       }
       
       std::ofstream outPartFile;
@@ -37,14 +67,10 @@ int main (int argc, char **argv)
       {
          inPartFile.getline(textLine,256);
          outPartFile<<textLine<<std::endl;
-         
       }
       inPartFile.close();
       outPartFile.close();
-
    }
 
    return 0;
-
-
 }
