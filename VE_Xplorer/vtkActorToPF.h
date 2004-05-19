05f9b5d8978b75d0b01724e77ef4a659709bdfd7
Doug McCorkle <doug.mccorkle@gmail.com>
2004-05-19 02:23:10 +0000

    New version of vtkActorToPF

--- VE_Xplorer/vtkActorToPF.h
+++ VE_Xplorer/vtkActorToPF.h
@@ -20,6 +20,13 @@
 #include <Performer/pr/pfGeoSet.h>
 #include <Performer/pr/pfLPointState.h>
 #include <Performer/pfutil.h>
+#include "vtkActor.h"
+#include "vtkPolyDataMapper.h"
+#ifdef VTK4
+#include "vtkCellArray.h"
+#endif
+//#include "vtkCommon.h"
+//#include "vtkGraphics.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,24 +34,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-// extra includes needed by VTK4.3 (sjk)
-#include <vtkCellArray.h>
-#include <vtkCellData.h>
-#include <vtkImageData.h>
-#include <vtkPointData.h>
-#include <vtkPolyData.h>
-#include <vtkActor.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkProperty.h>
-#include <vtkTexture.h>
-
 // vtkActorToPF - translates vtkActor to pfGeode. If geode is NULL, new one
 //   will be created. Optional verbose parameter prints debugging and
 //   performance information.
 pfGeode* vtkActorToPF(vtkActor *actor, pfGeode *geode = NULL, int verbose=0);
 
 // helper functions
-void vtkActorToGeoSets(vtkActor *actor, pfGeoSet *gsets[], int verbose=0);
+void vtkActorToGeoSets(vtkActor *actor, pfGeoSet *gsets[], int verbose);
 pfGeoSet *processPrimitive(vtkActor *a, vtkCellArray *prims, int pType, int v);
 void updateTexture(vtkActor *actor, pfGeoSet *gset, pfGeoState *gstate, int v);
 
