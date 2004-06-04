1c00df0e03911f70d44fc81fe0a69544f079ca69
B. Dotson <bdotson@unknown.nil>
2004-06-04 16:23:27 +0000

    conversion of setup.tsh to bourne shell for use by cron job

---
+++ VE_Installer/setup.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+# this is a bourne shell script
+# sets up environment to build and/or run VE-Xplorer
+
+#OMNI_HOME : comment out definitions to make a non-corba version - that's all you have to do
+
+#this is typically defined in your .cshrc file
+#VE_SUITE_HOME=/home/vr/Applications/TSVEG/VE_Suite
+
+#echo "uname is" `uname`
+#echo "uname -n is" `uname -n`
+export CFD_LIBS=`uname`_LIB
+export VE_LIBS=`uname`
+echo "CFD_LIBS =" ${CFD_LIBS}
+
+echo "HOSTTYPE =" $HOSTTYPE
+#iris4d on sgi, i386-linux on lagarto, i586-linux on abbott, i686 on cron job
+
+case "$HOSTTYPE" in
+   "") 
+   #echo "HOSTTYPE is empty"
+   export CFDHOSTTYPE=`uname`
+;;
+   ?*iris*) 
+   #echo "HOSTTYPE contains iris"
+   export CFDHOSTTYPE=${HOSTTYPE}
+;;
+   ?*linux*) 
+   #echo "HOSTTYPE contains linux"
+   export CFDHOSTTYPE=${HOSTTYPE}
+;;
+   *)
+   #echo "HOSTTYPE does not contain linux or iris"
+   # get here when cronjob returns HOSTTYPE = i686: switch to usual settings
+   export CFDHOSTTYPE=i386-linux
+;;
+esac
+
+echo "CFDHOSTTYPE =" ${CFDHOSTTYPE}
+
+#export TAO_BUILD=TRUE
+export TAO_BUILD=FALSE
+
+export PFNFYLEVEL=2
+export VPR_DEBUG_NFY_LEVEL=0
+export VPR_DEBUG_ENABLE=1
+export PFSHAREDSIZE=534773700
+export WX_HOME_DIR=/home/users/mccdo/wxWindows/wxGTK
+export OMNIORB_CONFIG=${VE_SUITE_HOME}/VE_Installer/omniORB4.cfg
+export OMNINAMES_LOGDIR=${VE_SUITE_HOME}/VE_Installer
+VTK_HOME_DIR=/home/users/mccdo/vtk-builds
+
+if [ `uname` = "IRIX64" ]; then
+   #echo "specifying libraries for irix"
+   export JDK_HOME=/usr/java2
+   export VTK_BASE_DIR=${VTK_HOME_DIR}/IRIX32
+   export VJ_BASE_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.irix-n32-pthread
+   export VJ_DEPS_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.irix-n32-deps
+#   export LD_LIBRARYN32_PATH=/home/users/mccdo/VE_Suite/VE_Installer/arenasize/libs
+   export LD_LIBRARYN32_PATH=${VJ_BASE_DIR}/lib32:${VTK_BASE_DIR}/lib/vtk:${VJ_DEPS_DIR}/lib32
+   export LD_LIBRARYN32_PATH=${LD_LIBRARYN32_PATH}:/home/users/jhynek/Pigs/Oinks/OpenAL/openal/linux/bin/lib
+   export LD_LIBRARYN32_PATH=${LD_LIBRARYN32_PATH}:/home/users/mccdo/software/IRIX32/lib
+   export WX_HOME=${WX_HOME_DIR}/irix-65
+   export BOOST_INCLUDES=${VJ_DEPS_DIR}
+
+   if [ ${TAO_BUILD} = "TRUE" ]; then
+      export ACE_ROOT=/home/users/mccdo/ACE_TAO/Irix-vrac/ACE_wrappers
+      export TAO_ROOT=${ACE_ROOT}/TAO
+      export LD_LIBRARYN32_PATH=${LD_LIBRARYN32_PATH}:${ACE_ROOT}/ace:${ACE_ROOT}/lib
+      export LD_LIBRARYN32_PATH=${LD_LIBRARYN32_PATH}:${TAO_ROOT}/TAO_IDL:${WX_HOME}/lib
+   else
+#      export OMNI_HOME=/home/vr/Juggler/irix/mipspro-omniORB-4.0.1
+#      export OMNI_HOME=/home/users/mccdo/software/IRIX32
+      export OMNI_HOME=${VJ_DEPS_DIR}
+#/home/vr/Juggler/irix/mipspro
+      export PYTHONPATH=${OMNI_HOME}/lib/python1.5/site-packages
+      export LD_LIBRARYN32_PATH=${LD_LIBRARYN32_PATH}:${OMNI_HOME}/lib:${WX_HOME}/lib
+   fi
+      
+elif [ `uname` = "Linux" ]; then
+   #echo "specifying libraries for linux"
+   export VTK_BASE_DIR=${VTK_HOME_DIR}/Linux-rh8
+
+   if [ ${CFDHOSTTYPE} = "i386-linux" ]; then
+      export JDK_HOME=/usr/java/j2sdk1.4.2_03
+      export VJ_BASE_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80
+      export VJ_DEPS_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80-deps
+   elif [ ${CFDHOSTTYPE} = "i586-linux" ]; then
+      export JDK_HOME=/usr/lib/java2
+      export VJ_BASE_DIR=/home/users/mccdo/vrjuggler-builds/Suse-9.1-alpha4
+      export VJ_DEPS_DIR=/home/users/mccdo/cppdom-0.3.2/Suse-9.1
+   else
+      export JDK_HOME=/usr/java/j2sdk1.4.2_03
+      export VJ_BASE_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80
+      export VJ_DEPS_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80-deps
+   fi
+
+   export BOOST_INCLUDES=${VJ_DEPS_DIR}/include/boost-1_31
+   export LD_LIBRARY_PATH=${VJ_BASE_DIR}/lib:${VTK_BASE_DIR}/lib/vtk:${VJ_DEPS_DIR}/lib
+   export WX_HOME=${WX_HOME_DIR}/linux-rh80
+
+   if [ ${TAO_BUILD} = "TRUE" ]; then
+      export WX_HOME=${WX_HOME_DIR}/linux-rh80
+      export ACE_ROOT=/home/users/mccdo/ACE_TAO/Suse-9-vrac/ACE_wrappers
+      #export ACE_ROOT=/home/users/mccdo/ACE_TAO/Linux-rh80-vrac/ACE_wrappers
+
+      export TAO_ROOT=${ACE_ROOT}/TAO
+      export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ACE_ROOT}/ace:${ACE_ROOT}/lib
+      export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TAO_ROOT}/TAO_IDL:${WX_HOME}/lib
+   else
+      export OMNI_HOME=/home/vr/Juggler/linux-rh80
+      #export OMNI_HOME=/home/vr/Juggler/linux-fc1
+      export PYTHONPATH=${OMNI_HOME}/lib/python2.2/site-packages
+      export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OMNI_HOME}/lib:${WX_HOME}/lib
+   fi
+
+else
+   echo "ERROR: Unsupported operating system"
+   echo "       CFD_LIBS and OMNI_HOME are undefined"
+fi
+
+export TWEEK_BASE_DIR=${VJ_BASE_DIR}
+export DZR_BASE_DIR=${VJ_BASE_DIR}/share/Doozer
+export SNX_BASE_DIR=${VJ_BASE_DIR}
+export PATH=${PATH}:${VJ_BASE_DIR}/bin:${VE_SUITE_HOME}/bin:${VE_SUITE_HOME}/bin/${VE_LIBS}:${VJ_DEPS_DIR}/bin
+export PATH=${WX_HOME}/bin:${PATH}
+export PATH=${JDK_HOME}/bin:${PATH}
+if [ ${TAO_BUILD} = "TRUE" ]; then
+   export PATH=${ACE_ROOT}/bin:${PATH}
+else
+   export PATH=${OMNI_HOME}/bin:${PATH}
+fi
+
+#echo ""
+#echo "Now you may type 'gmake' to build the application"
+#echo "              or 'gmake clean'
+#echo "              or 'gmake cleandepend'
+#echo "              or 'run' to start the application in sim mode"
+#echo "              or 'runc6' to start the application on the c6"
+#echo "              or 'runc4.closed' or 'runc4.open' to start the application on the c4"
+#echo ""
