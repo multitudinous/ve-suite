5bbd853cbf17440095766c04077c2a00733f2650
Steve Kirstukas <sjk60@yahoo.com>
2004-06-09 22:46:40 +0000

    corrected for use with only CFDHOSTTYPE

--- VE_Installer/setup.sh
+++ VE_Installer/setup.sh
@@ -2,41 +2,25 @@
 # this is a bourne shell script
 # sets up environment to build and/or run VE-Xplorer
 
-#OMNI_HOME : comment out definitions to make a non-corba version - that's all you have to do
+#OMNI_HOME: comment out definitions to make a non-corba version - that's all you have to do
 
 #this is typically defined in your .cshrc file
 #VE_SUITE_HOME=/home/vr/Applications/TSVEG/VE_Suite
 
-#echo "uname is" `uname`
-#echo "uname -n is" `uname -n`
-export CFD_LIBS=`uname`_LIB
-export VE_LIBS=`uname`
-echo "CFD_LIBS =" ${CFD_LIBS}
-
-echo "HOSTTYPE =" $HOSTTYPE
-#iris4d on sgi, i386-linux on lagarto, i586-linux on abbott, i686 on cron job
-
-case "$HOSTTYPE" in
-   "") 
-   #echo "HOSTTYPE is empty"
+if [ -e /etc/redhat-release ]; then
+   #echo "found /etc/redhat-release"
+   # extract some words from file to create something like RedHat_8.0
+   export CFDHOSTTYPE=`cat /etc/redhat-release | awk -F" " '{print $1 $2 "_" $5}'`
+elif [ -e /etc/SuSE-release ]; then
+   #echo "found /etc/SuSE-release"
+   # extract first and third words from file to create something like SuSE_9.1
+   export CFDHOSTTYPE=`head -1 /etc/SuSE-release | awk -F" " '{print $1 "_" $3}'`
+else
+   echo "uname is" `uname`
    export CFDHOSTTYPE=`uname`
-;;
-   ?*iris*) 
-   #echo "HOSTTYPE contains iris"
-   export CFDHOSTTYPE=${HOSTTYPE}
-;;
-   ?*linux*) 
-   #echo "HOSTTYPE contains linux"
-   export CFDHOSTTYPE=${HOSTTYPE}
-;;
-   *)
-   #echo "HOSTTYPE does not contain linux or iris"
-   # get here when cronjob returns HOSTTYPE = i686: switch to usual settings
-   export CFDHOSTTYPE=i386-linux
-;;
-esac
+fi
 
-echo "CFDHOSTTYPE =" ${CFDHOSTTYPE}
+echo "CFDHOSTTYPE =" $CFDHOSTTYPE
 
 #export TAO_BUILD=TRUE
 export TAO_BUILD=FALSE
@@ -50,8 +34,9 @@ export OMNIORB_CONFIG=${VE_SUITE_HOME}/VE_Installer/omniORB4.cfg
 export OMNINAMES_LOGDIR=${VE_SUITE_HOME}/VE_Installer
 VTK_HOME_DIR=/home/users/mccdo/vtk-builds
 
-if [ `uname` = "IRIX64" ]; then
-   #echo "specifying libraries for irix"
+case "$CFDHOSTTYPE" in
+   IRIX*) 
+   #echo "CFDHOSTTYPE contains IRIX"
    export JDK_HOME=/usr/java2
    export VTK_BASE_DIR=${VTK_HOME_DIR}/IRIX32
    export VJ_BASE_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.irix-n32-pthread
@@ -76,24 +61,14 @@ if [ `uname` = "IRIX64" ]; then
       export PYTHONPATH=${OMNI_HOME}/lib/python1.5/site-packages
       export LD_LIBRARYN32_PATH=${LD_LIBRARYN32_PATH}:${OMNI_HOME}/lib:${WX_HOME}/lib
    fi
-      
-elif [ `uname` = "Linux" ]; then
-   #echo "specifying libraries for linux"
+;;
+   RedHat*) 
+   #echo "CFDHOSTTYPE contains RedHat"
    export VTK_BASE_DIR=${VTK_HOME_DIR}/Linux-rh8
 
-   if [ ${CFDHOSTTYPE} = "i386-linux" ]; then
-      export JDK_HOME=/usr/java/j2sdk1.4.2_03
-      export VJ_BASE_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80
-      export VJ_DEPS_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80-deps
-   elif [ ${CFDHOSTTYPE} = "i586-linux" ]; then
-      export JDK_HOME=/usr/lib/java2
-      export VJ_BASE_DIR=/home/users/mccdo/vrjuggler-builds/Suse-9.1-alpha4
-      export VJ_DEPS_DIR=/home/users/mccdo/cppdom-0.3.2/Suse-9.1
-   else
-      export JDK_HOME=/usr/java/j2sdk1.4.2_03
-      export VJ_BASE_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80
-      export VJ_DEPS_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80-deps
-   fi
+   export JDK_HOME=/usr/java/j2sdk1.4.2_03
+   export VJ_BASE_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80
+   export VJ_DEPS_DIR=/home/vr/Juggler/2.0/vrjuggler-2.0-alpha4.linux-rh80-deps
 
    export BOOST_INCLUDES=${VJ_DEPS_DIR}/include/boost-1_31
    export LD_LIBRARY_PATH=${VJ_BASE_DIR}/lib:${VTK_BASE_DIR}/lib/vtk:${VJ_DEPS_DIR}/lib
@@ -113,11 +88,39 @@ elif [ `uname` = "Linux" ]; then
       export PYTHONPATH=${OMNI_HOME}/lib/python2.2/site-packages
       export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OMNI_HOME}/lib:${WX_HOME}/lib
    fi
+;;
+   SuSE*) 
+   #echo "CFDHOSTTYPE contains SuSE"
+   export VTK_BASE_DIR=${VTK_HOME_DIR}/Linux-rh8
 
-else
+   export JDK_HOME=/usr/lib/java2
+   export VJ_BASE_DIR=/home/users/mccdo/vrjuggler-builds/Suse-9.1-alpha4
+   export VJ_DEPS_DIR=/home/users/mccdo/cppdom-0.3.2/Suse-9.1
+
+   export BOOST_INCLUDES=${VJ_DEPS_DIR}/include/boost-1_31
+   export LD_LIBRARY_PATH=${VJ_BASE_DIR}/lib:${VTK_BASE_DIR}/lib/vtk:${VJ_DEPS_DIR}/lib
+   export WX_HOME=${WX_HOME_DIR}/linux-rh80
+
+   if [ ${TAO_BUILD} = "TRUE" ]; then
+      export WX_HOME=${WX_HOME_DIR}/linux-rh80
+      export ACE_ROOT=/home/users/mccdo/ACE_TAO/Suse-9-vrac/ACE_wrappers
+      #export ACE_ROOT=/home/users/mccdo/ACE_TAO/Linux-rh80-vrac/ACE_wrappers
+
+      export TAO_ROOT=${ACE_ROOT}/TAO
+      export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ACE_ROOT}/ace:${ACE_ROOT}/lib
+      export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TAO_ROOT}/TAO_IDL:${WX_HOME}/lib
+   else
+      export OMNI_HOME=/home/vr/Juggler/linux-rh80
+      #export OMNI_HOME=/home/vr/Juggler/linux-fc1
+      export PYTHONPATH=${OMNI_HOME}/lib/python2.2/site-packages
+      export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OMNI_HOME}/lib:${WX_HOME}/lib
+   fi
+;;
+   *)
    echo "ERROR: Unsupported operating system"
-   echo "       CFD_LIBS and OMNI_HOME are undefined"
-fi
+   echo "       OMNI_HOME, etc. are undefined"
+;;
+esac
 
 export TWEEK_BASE_DIR=${VJ_BASE_DIR}
 export DZR_BASE_DIR=${VJ_BASE_DIR}/share/Doozer
