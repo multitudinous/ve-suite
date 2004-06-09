74e08d58f436dc6619b86a85441b6eea9b31061a
Doug McCorkle <doug.mccorkle@gmail.com>
2004-06-09 18:06:25 +0000

    New code I am working on. No in the Makefile or used anywhere.

---
+++ VE_Xplorer/cfdVEBaseClass.h
@@ -0,0 +1,64 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2004 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: cfdExecutive.h,v $
+ * Date modified: $Date: 2004-05-18 13:44:18 -0700 (Tue, 18 May 2004) $
+ * Version:       $Rev: 382 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef CFD_VE_BASECLASS_H
+#define CFD_VE_BASECLASS_H
+
+#include "cfdDCS.h"
+class cfdModuleGeometry;
+
+class cfdVEBaseClass, public cfdDCS // Inherit from wxBase class to enable string instantiation
+{
+   public:
+      cfdVEBaseClass( void );
+      ~cfdVEBaseClass( void );
+
+      // Methods to do scene graph manipulations
+      // New methods may have to be added later
+      void AddSelfToSG( void );
+      void RemoveSelfFromSG( void );
+
+      // Change state information for geometric representation
+      void MakeTransparent( void );
+      void SetColor( float* );
+      
+      // transform object based 
+      void SetTransforms( float*, float*, float* );
+
+      void GetDataFromUnit( void );
+      void MakeGeodeByUserRequest( int );
+   private:
+      cfdModuleGeometry*  geometryNode;
+
+      pfDCS*   worldNode;
+};
+
+#endif
