1991971a668915617299c5c2c1c70d8a0db45c8a
Maguire <maguire@unknown.nil>
2004-06-11 16:43:46 +0000

    adding Water

--- futuregen/Unit/V21Helper/V21Helper.h
+++ futuregen/Unit/V21Helper/V21Helper.h
@@ -1,11 +1,15 @@
 #ifndef V21HELPER_H
 #define V21HELPER_H
 
-#include <Gas/Gas.h>
+#include <Datatypes/Gas.h>
 #include <Therm/thermo.h>
+
+//#include <Datatypes/Water.h>
+//#include <Steam67/Steam67.h>
+
 #include <interface.h>
 #include <SummaryValues/summary_values.h>
-
+class Water;
 class V21Helper {
 
 public:
@@ -16,11 +20,17 @@ public:
   int IntToGas (Interface *it, Gas &gs);
   int GasToInt (Gas *gs, Interface &it);
   
+  int IntToWat (Interface *it, Water &wt);
+  int WatToInt (Water *wt, Interface &it);
+
   int IntToSum (Interface *it, summary_values &sv);
   int SumToInt (summary_values* sv, Interface &it);
 
+  thermo  *thermo_database;
+  //steam67 *steam67_database;
+ 
 private:
-  thermo *thermo_database;
+
 };
 
 
