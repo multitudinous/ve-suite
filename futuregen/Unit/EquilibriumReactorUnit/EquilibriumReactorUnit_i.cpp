17659cb0c099da0af6f53825d303ed8167683fe8
Yang <yang@unknown.nil>
2004-06-15 22:48:35 +0000

    add in the EquilibriumReactor Module, the first module without UI input

---
+++ futuregen/Unit/EquilibriumReactorUnit/EquilibriumReactorUnit_i.cpp
@@ -0,0 +1,247 @@
+#include "V21Helper.h"
+#include "EquilibriumReactorUnit_i.h"
+
+// Implementation skeleton constructor
+Body_Unit_i::Body_Unit_i (Body::Executive_ptr exec, std::string name)
+  : executive_(Body::Executive::_duplicate(exec))
+{
+  UnitName_=name;
+  return_state = 0;
+}
+  
+// Implementation skeleton destructor
+Body_Unit_i::~Body_Unit_i (void)
+  {
+  }
+  
+void Body_Unit_i::StartCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+{
+  // Add your implementation here
+  const char* igas;
+  const char* ogas;
+  bool rv;
+  Package p;
+  string therm_path="thermo";
+  const char* result;
+  summary_values summaries;
+  
+  fflush(NULL);
+  std::cout<<"cp1\n";
+  igas = executive_->GetImportData(id_, 0); //port 0 will be the gas input port;
+  std::cout<<"cp2\n";
+  if (!igas)
+    {
+      error("Missing input input.");
+      return;
+    }
+  
+  p.SetSysId("gas_in.xml");
+  p.Load(igas, strlen(igas)); 
+  
+  std::cout<<"cp2\n";
+  Gas *gas_in_data = new Gas();
+  
+  V21Helper gashelper(therm_path.c_str());
+  gashelper.IntToGas(&(p.intfs[0]), *gas_in_data);
+  
+  // Check incoming
+  if(gas_in_data->gas_composite.T <= 200 || gas_in_data->gas_composite.T >= 3000) {
+    warning("Incoming gas temperature out of range.");
+  }
+  
+  Gas *gas_out_data = new Gas;
+  gas_out_data->copy(*gas_in_data);
+  gas_out_data->gas_composite.equilb();
+  
+  // Check incoming
+  if(gas_out_data->gas_composite.T <= 200 || gas_out_data->gas_composite.T >= 3000) {
+    warning("Outgoing gas temperature out of range.");
+  }
+  
+  
+  //fill out the output stream  
+  p.intfs.resize(1); //each port has its own package
+  gashelper.GasToInt(gas_out_data, p.intfs[0]);
+ 
+  p.SetPackName("ExportData");
+  p.SetSysId("gasout.xml");
+  ogas = p.Save(rv);
+  executive_->SetExportData(id_, 0, ogas);
+
+  p.intfs.clear();
+  result = p.Save(rv);
+  std::cout<<"cp5\n";
+  executive_->SetModuleResult(id_, result); //marks the end the execution
+  delete gas_out_data;
+  delete gas_in_data;
+  std::cout<<"cp6\n";
+  }
+  
+void Body_Unit_i::StopCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    string msg;
+    msg = UnitName_+" : Instant calculation, already finished\n";
+    executive_->SetModuleMessage(id_,msg.c_str());
+  }
+  
+void Body_Unit_i::PauseCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    string msg;
+    msg = UnitName_+" : Instant calculation, already finished\n";
+    executive_->SetModuleMessage(id_,msg.c_str());
+  }
+  
+void Body_Unit_i::Resume (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    string msg;
+    msg = UnitName_+" : Instant calculation, already finished\n";
+    executive_->SetModuleMessage(id_,msg.c_str());
+  }
+  
+char * Body_Unit_i::GetStatusMessage (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    const char *status;
+    bool rv;
+    Package p;
+    p.SetPackName("Status");
+    p.SetSysId("status.xml");
+    p.intfs.resize(1);
+    p.intfs[0].setInt("return_state", return_state);
+    status = p.Save(rv);
+    return CORBA::string_dup(status);
+  }
+  
+char * Body_Unit_i::GetUserData (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    std::cout<<UnitName_<<" :GetUserData called"<<endl;
+    return CORBA::string_dup(data_.c_str());
+  }
+  
+void Body_Unit_i::SetParams (
+    const char * param
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    //No UI variables
+  }
+  
+void Body_Unit_i::SetID (
+    CORBA::Long id
+    ACE_ENV_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    id_=id;
+    std::cout<<UnitName_<<" :SetID called"<<endl;
+  }
+  
+CORBA::Long Body_Unit_i::GetID (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    std::cout<<UnitName_<<" :GetID called"<<endl;
+    return id_;
+  }
+  
+void Body_Unit_i::SetName (
+    const char * name
+    ACE_ENV_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    UnitName_ = std::string(name);
+    std::cout<<UnitName_<<" :SetName called"<<endl;
+  }
+  
+char * Body_Unit_i::GetName (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    std::cout<<UnitName_<<" :GetName called"<<endl;
+    return CORBA::string_dup(UnitName_.c_str());
+  }
+
+void Body_Unit_i::error (std::string msg)
+{
+  Package p;
+  const char* result;
+  bool rv;
+  p.SetPackName("result");
+  p.SetSysId("result.xml");
+  msg+="\n";
+  executive_->SetModuleMessage(id_, msg.c_str());
+  p.intfs.clear();
+  result = p.Save(rv);
+  return_state = 1;
+  executive_->SetModuleResult(id_, result); //this marks the end the execution
+}
+
+void Body_Unit_i::warning (std::string msg)
+{
+  msg+="\n";
+  executive_->SetModuleMessage(id_, msg.c_str());
+}
