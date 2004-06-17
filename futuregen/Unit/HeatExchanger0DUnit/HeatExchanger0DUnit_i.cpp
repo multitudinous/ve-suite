78bc0ab4551e43c7ac951b7515bfb0185c0a4529
Yang <yang@unknown.nil>
2004-06-17 23:19:32 +0000

    commit the HeatExchanger0D

---
+++ futuregen/Unit/HeatExchanger0DUnit/HeatExchanger0DUnit_i.cpp
@@ -0,0 +1,252 @@
+#include "V21Helper.h"
+#include "HeatExchanger0DUnit_i.h"
+
+// Implementation skeleton constructor
+Body_Unit_i::Body_Unit_i (Body::Executive_ptr exec, std::string name)
+  : executive_(Body::Executive::_duplicate(exec))
+{
+  UnitName_=name;
+  return_state = 0;
+}
+  
+// Implementation skeleton destructor
+Body_Unit_i::~Body_Unit_i (void)
+  {
+  }
+  
+void Body_Unit_i::StartCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    const char* igas;
+    const char* ogas;
+    bool rv;
+    Package p;
+    string therm_path="thermo";
+    int i;
+    const char* result;
+    summary_values summaries;
+    double Q;
+
+    igas = executive_->GetImportData(id_, 0); //port 0 will be the gas input port;
+    if (!igas)
+      {
+	error("Missing input input.");
+	return;
+      }
+    p.SetSysId("gas_in.xml");
+    p.Load(igas, strlen(igas)); 
+    
+    Gas *gas_in = new Gas();
+    Gas *gas_out = new Gas;
+    V21Helper gashelper(therm_path.c_str());
+    gashelper.IntToGas(&(p.intfs[0]), *gas_in);
+    
+    double d_temp = desired_temp;
+    
+    gas_out->copy(*gas_in);
+    
+    gas_out->gas_composite.T = d_temp;
+    
+    Q = (gas_out->gas_composite.M / gas_out->gas_composite.mw()) *
+      (gas_out->gas_composite.enthalpy() - gas_in->gas_composite.enthalpy());
+    
+    summaries.insert_summary_val("Temperature Change UNITS:K FORMAT:10.2f",
+				 d_temp - gas_in->gas_composite.T);
+    summaries.insert_summary_val("Q UNITS:MW FORMAT:10.2f", Q/1e6);
+    
+    p.intfs.resize(1); //each port has its own package
+    p.SetPackName("ExportData");
+    p.SetSysId("test.xml");
+    
+    gashelper.GasToInt(gas_out, p.intfs[0]);
+    ogas = p.Save(rv);
+    executive_->SetExportData(id_, 0, ogas);
+
+    p.intfs.resize(1);
+    gashelper.SumToInt(&summaries, p.intfs[0]);
+    result = p.Save(rv);
+    std::cout<<"cp5\n";
+    executive_->SetModuleResult(id_, result); //marks the end the execution
+    delete gas_out;
+    delete gas_in;
+
+    std::cout<<"cp6\n";
+  }
+  
+void Body_Unit_i::StopCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    string msg;
+    msg = UnitName_+" : Instant calculation, already finished\n";
+    executive_->SetModuleMessage(id_,msg.c_str());
+  }
+  
+void Body_Unit_i::PauseCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    string msg;
+    msg = UnitName_+" : Instant calculation, already finished\n";
+    executive_->SetModuleMessage(id_,msg.c_str());
+  }
+  
+void Body_Unit_i::Resume (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    string msg;
+    msg = UnitName_+" : Instant calculation, already finished\n";
+    executive_->SetModuleMessage(id_,msg.c_str());
+  }
+  
+char * Body_Unit_i::GetStatusMessage (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    const char *status;
+    bool rv;
+    Package p;
+    p.SetPackName("Status");
+    p.SetSysId("status.xml");
+    p.intfs.resize(1);
+    p.intfs[0].setInt("return_state", return_state);
+    status = p.Save(rv);
+    return CORBA::string_dup(status);
+  }
+  
+char * Body_Unit_i::GetUserData (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    std::cout<<UnitName_<<" :GetUserData called"<<endl;
+    return CORBA::string_dup(data_.c_str());
+  }
+  
+void Body_Unit_i::SetParams (
+    const char * param
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    if (param!=NULL)
+      std::cout<<param<<std::endl;
+    std::cout<<UnitName_<<" :SetParams called"<<endl;
+    Package p;
+        
+    p.SetSysId("gui.xml");
+    p.Load(param, strlen(param));
+    //Now make use of p.intfs to get your GUI vars out
+    desired_temp = p.intfs[0].getDouble("desired_temp");
+    
+  }
+  
+void Body_Unit_i::SetID (
+    CORBA::Long id
+    ACE_ENV_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    id_=id;
+    std::cout<<UnitName_<<" :SetID called"<<endl;
+  }
+  
+CORBA::Long Body_Unit_i::GetID (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    std::cout<<UnitName_<<" :GetID called"<<endl;
+    return id_;
+  }
+  
+void Body_Unit_i::SetName (
+    const char * name
+    ACE_ENV_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    UnitName_ = std::string(name);
+    std::cout<<UnitName_<<" :SetName called"<<endl;
+  }
+  
+char * Body_Unit_i::GetName (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    std::cout<<UnitName_<<" :GetName called"<<endl;
+    return CORBA::string_dup(UnitName_.c_str());
+  }
+
+void Body_Unit_i::error (std::string msg)
+{
+  Package p;
+  const char* result;
+  bool rv;
+  p.SetPackName("result");
+  p.SetSysId("result.xml");
+  msg+="\n";
+  executive_->SetModuleMessage(id_, msg.c_str());
+  p.intfs.clear();
+  result = p.Save(rv);
+  return_state = 1;
+  executive_->SetModuleResult(id_, result); //this marks the end the execution
+}
+
+void Body_Unit_i::warning (std::string msg)
+{
+  msg+="\n";
+  executive_->SetModuleMessage(id_, msg.c_str());
+}
