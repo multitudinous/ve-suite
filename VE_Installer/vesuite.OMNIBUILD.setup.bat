58c8f3a227fc6491c79a9787f63579602944a65e
Biv <biv@unknown.nil>
2004-07-13 21:16:15 +0000

    biv--fixing some windows build issues--note the changes in the readme and setup files, mainly stating that path names w/ spaces cause problems. . .

--- VE_Installer/vesuite.OMNIBUILD.setup.bat
+++ VE_Installer/vesuite.OMNIBUILD.setup.bat
@@ -12,7 +12,7 @@ REM (local or on network) machine, otherwise
 REM user may run into runtime problems when loading config files across
 REM the network
 
-set VJ_BASE_DIR=C:\vrjuggler
+set VJ_BASE_DIR=C:\vrjuggler-2.0-alpha4.win32-vc71
 set VJ_DEPS_DIR=C:\vrjuggler-2.0-alpha4.win32-vc71-deps
 
 REM location of VE_Suite directory
@@ -20,8 +20,12 @@ REM example C:\VE_Dev\VE_Suite
 set VE_SUITE_HOME=C:\Documents and Settings\Doug McCorkle\Desktop\VE_Suite
 
 REM location of VTK
-set VTK_HOME=C:\Documents and Settings\Doug McCorkle\Desktop\VE_Suite_Software\VTK-4.4-LatestRelease\VTK
+set VTK_HOME=C:\VTK
+REM location of java
 set JAVA_HOME=C:\j2sdk1.4.2_04
+REM OmniORB build environment varibles
+set OMNI_HOME=C:\omniORB-4.0.3
+
 REM
 REM Everything below is relative to the above environment so
 REM user shouldn't have to modify UNLESS "local" builds(debug for 
@@ -44,7 +48,6 @@ set VJ_CFG_PATH=%VJ_BASE_DIR%\share\vrjuggler\data\definitions
 set NSPR_ROOT=%VJ_DEPS_DIR%
 
 REM OmniORB build environment varibles
-set OMNI_HOME=C:\omniORB-4.0.3
 set OMNIORB_CONFIG=%VE_SUITE_HOME%\VE_Installer\omniORB4.cfg
 set OMNINAMES_LOGDIR=%VE_SUITE_HOME%\VE_Installer
 
@@ -53,5 +56,5 @@ set PYTHONPATH=%VJ_DEPS_DIR%\lib\python
 set Path=%Path%;%OMNI_HOME%\bin\x86_win32;%VJ_DEPS_DIR%\bin;%VJ_DEPS_DIR%\lib;%VJ_BASE_DIR%\lib;%VTK_HOME%\bin\debug;%VE_SUITE_HOME%\bin
 
 REM only change this if your Visual Studio .NET is installed somewhere else
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe" %VE_SUITE_HOME%\VE_Suite.sln
+"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe" ..\VE_Suite.sln
 cmd
