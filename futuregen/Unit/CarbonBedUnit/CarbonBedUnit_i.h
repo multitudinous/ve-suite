873e1c7289befd7441526685826a8523b25c8c29
Yang <yang@unknown.nil>
2004-08-16 17:53:38 +0000

    add in the CarbonBedUnit

---
+++ futuregen/Unit/CarbonBedUnit/CarbonBedUnit_i.h
@@ -0,0 +1,137 @@
+#ifndef UNIT_I_H_
+#define UNIT_I_H_
+
+#include "moduleS.h"
+#include "package.h" //so it can use the xerces stuff
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+//Class Body_Unit_i
+class  Body_Unit_i : public virtual POA_Body::Unit
+{
+ public:
+  //Constructor 
+  Body_Unit_i (Body::Executive_ptr exec, std::string name);
+  
+  //Destructor 
+  virtual ~Body_Unit_i (void);
+  
+  std::string UnitName_;
+  CORBA::Long id_;
+  std::string status_;
+  std::string data_;
+
+ protected:
+  Body::Executive_var executive_;
+  int return_state;
+  void error(std::string msg);
+  void warning(std::string msg);
+
+  long   carbon_type;
+  double press_drop;
+  double part_diam;
+  double bulk_density;
+  double temp;
+  double press;
+  double cr_time;
+  double porosity;
+  double res_time;
+
+  double Meters2Feet(double meters);
+
+ public:
+
+virtual void StartCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual void StopCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual void PauseCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual void Resume (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual char * GetStatusMessage (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual char * GetUserData (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual void SetParams (
+    const char * param
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual void SetID (
+    CORBA::Long id
+    ACE_ENV_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual CORBA::Long GetID (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual void SetName (
+    const char * name
+    ACE_ENV_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+
+virtual char * GetName (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ));
+};
+
+#endif
