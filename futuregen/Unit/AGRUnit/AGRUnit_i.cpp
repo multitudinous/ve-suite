d3d16606c97d219801f61533d7c60dd89cf469fe
Yang <yang@unknown.nil>
2004-08-16 20:04:21 +0000

    the Unit for AGR. code not working yet

---
+++ futuregen/Unit/AGRUnit/AGRUnit_i.cpp
@@ -0,0 +1,242 @@
+#include "V21Helper.h"
+#include "AGRUnit_i.h"
+
+// Implementation skeleton constructor
+Body_Unit_i::Body_Unit_i (Body::Executive_ptr exec, std::string name)
+  : executive_(Body::Executive::_duplicate(exec))
+{
+  UnitName_=name;
+  return_state = 0;
+}
+  
+// Implementation skeleton destructor
+Body_Unit_i::~Body_Unit_i (void)
+  {
+  }
+  
+void Body_Unit_i::StartCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    const char* igas;
+    const char* ogas;
+    bool rv;
+    Package p;
+    string therm_path="thermo";
+    const char* result;
+    summary_values summaries;
+
+    fflush(NULL);
+
+    igas = executive_->GetImportData(id_, 0); //port 0 will be the gas input port;
+
+    if (string(igas)=="")
+      {
+	error("AGR: Missing input input.");
+	return;
+      }
+
+    p.SetSysId("gas_in.xml");
+    p.Load(igas, strlen(igas)); 
+
+    Gas *gas_in_data = new Gas;
+
+    V21Helper gashelper(therm_path.c_str());
+    gashelper.IntToGas(&(p.intfs[0]), *gas_in_data);
+
+    // fill in summary table
+    summaries.clear();
+    
+    summaries.insert_summary_val("Amine Feed Rate UNITS:GPM FORMAT:10.2f", 403);
+    summaries.insert_summary_val("H2S UNITS:ppm FORMAT:10.2f", 1.0);
+    summaries.insert_summary_val("CO2 UNITS:mol_frac FORMAT:10.2f", 7.1);
+    
+    p.intfs.resize(1);
+    
+    gashelper.SumToInt(&summaries, p.intfs[0]);
+    result = p.Save(rv);
+    
+    executive_->SetModuleResult(id_, result); //marks the end the execution
+    
+    if(gas_in_data)  delete gas_in_data;
+    // if(gas_out_data) delete gas_out_data;
+  }
+
+void Body_Unit_i::StopCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    string msg;
+    msg = UnitName_+" : Instant calculation, already finished\n";
+    executive_->SetModuleMessage(id_,msg.c_str());
+  }
+  
+void Body_Unit_i::PauseCalc (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    string msg;
+    msg = UnitName_+" : Instant calculation, already finished\n";
+    executive_->SetModuleMessage(id_,msg.c_str());
+  }
+  
+void Body_Unit_i::Resume (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    string msg;
+    msg = UnitName_+" : Instant calculation, already finished\n";
+    executive_->SetModuleMessage(id_,msg.c_str());
+  }
+  
+char * Body_Unit_i::GetStatusMessage (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    const char *status;
+    bool rv;
+    Package p;
+    p.SetPackName("Status");
+    p.SetSysId("status.xml");
+    p.intfs.resize(1);
+    p.intfs[0].setInt("return_state", return_state);
+    status = p.Save(rv);
+    return CORBA::string_dup(status);
+  }
+  
+char * Body_Unit_i::GetUserData (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    std::cout<<UnitName_<<" :GetUserData called"<<endl;
+    return CORBA::string_dup(data_.c_str());
+  }
+  
+void Body_Unit_i::SetParams (
+    const char * param
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    if (string(param)=="")
+      return;
+    std::cout<<UnitName_<<" :SetParams called"<<endl;
+    Package p;
+        
+    p.SetSysId("gui.xml");
+    p.Load(param, strlen(param));
+    //Now make use of p.intfs to get your GUI vars out
+    solv_type = p.intfs[0].getInt("solv_type");
+    solv_mw = p.intfs[0].getDouble("solv_mw");
+    solv_den = p.intfs[0].getDouble("solv_den");
+  }
+  
+void Body_Unit_i::SetID (
+    CORBA::Long id
+    ACE_ENV_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    id_=id;
+    std::cout<<UnitName_<<" :SetID called"<<endl;
+  }
+  
+CORBA::Long Body_Unit_i::GetID (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    std::cout<<UnitName_<<" :GetID called"<<endl;
+    return id_;
+  }
+  
+void Body_Unit_i::SetName (
+    const char * name
+    ACE_ENV_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    UnitName_ = std::string(name);
+    std::cout<<UnitName_<<" :SetName called"<<endl;
+  }
+  
+char * Body_Unit_i::GetName (
+    ACE_ENV_SINGLE_ARG_DECL
+  )
+  ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , Error::EUnknown
+  ))
+  {
+    // Add your implementation here
+    std::cout<<UnitName_<<" :GetName called"<<endl;
+    return CORBA::string_dup(UnitName_.c_str());
+  }
+
+void Body_Unit_i::error (std::string msg)
+{
+  Package p;
+  const char* result;
+  bool rv;
+  p.SetPackName("result");
+  p.SetSysId("result.xml");
+  msg+="\n";
+  executive_->SetModuleMessage(id_, msg.c_str());
+  p.intfs.clear();
+  result = p.Save(rv);
+  return_state = 1;
+  executive_->SetModuleResult(id_, result); //this marks the end the execution
+}
+
+void Body_Unit_i::warning (std::string msg)
+{
+  msg+="\n";
+  executive_->SetModuleMessage(id_, msg.c_str());
+}
+
