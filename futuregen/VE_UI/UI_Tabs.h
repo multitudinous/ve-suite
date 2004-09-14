a0a70042c758282c82f3ab0bb0097392b2bee486
Doug McCorkle <doug.mccorkle@gmail.com>
2004-09-14 12:51:43 +0000

    update to remove old code

--- futuregen/VE_UI/UI_Tabs.h
+++ futuregen/VE_UI/UI_Tabs.h
@@ -23,98 +23,6 @@
 class UI_VertTab;
 class UI_TransTab;
 class UI_ModelData;
-///copied from original Tabs.h//////
-/*enum cfdGeodeEnum {
-   // Everything below has to be mirrored exactly in 
-   // VE_Suite/VE_Xplorer/UMI/hello/config/mapping.config
-   CONTOUR, X_CONTOUR, Y_CONTOUR, Z_CONTOUR,
-   X_CONTOURS, Y_CONTOURS, Z_CONTOURS, 
-   MOMENTUM, X_MOMENTUM, Y_MOMENTUM,Z_MOMENTUM,
-   X_MOMENTUMS, Y_MOMENTUMS, Z_MOMENTUMS, 
-   VECTOR, X_VECTOR, Y_VECTOR, Z_VECTOR,
-   X_VECTORS, Y_VECTORS, Z_VECTORS, 
-   STREAMLINES, ISOSURFACE,
-   IMAGE_EX,   //yang-REI: changed due to conflict with /usr/include/Performer/image.h
-   POLYDATA,
-   SWITCH_CURSOR,
-   ANIMATED_STREAMLINES,
-   ANIMATED_IMAGES,
-   PARTICLES,
-   TRANS_GEOM,
-   X_TRANSIENT,
-   Y_TRANSIENT,
-   Z_TRANSIENT,
-   PARTICLE_TRANSIENT,
-   // Everything below has to be mirrored exactly in 
-   // VE_Suite/VE_Xplorer/UMI/hello/config/mapping2.config
-   // The first of the following non-geode related commands specifies an 
-   // offset: this offset must be the same as that used to set up the
-   // instance of id_mapper class called "my_mapper2" in client.java
-   CHANGE_SCALAR=100,
-   CHANGE_SCALAR_RANGE, //101
-   UPDATE_GEOMETRY,//102
-   SEND_DRAW,//103
-   UPDATE_SEND_PARAM,//104
-   RECORD_SCENE,//105
-   CLEAR_ALL,//106
-   SET_TRANSIENT_OPTIONS,
-   TRANSIENT_RESET,
-   TRANSIENT_BACKWARD,
-   LOAD_PFB_FILE,
-   CLEAR_PFB_FILE,
-   TRANSIENT_FORWARD,
-   TRANSIENT_STOP,
-   COMPUTE_STREAMLINES,
-   USE_LAST_STREAMLINE_SEEDPOINTS,
-   CHANGE_STREAMLINE_CURSOR,
-   NO_CURSOR,
-   POINT_CURSOR,
-   X_LINE_CURSOR,
-   Y_LINE_CURSOR,
-   Z_LINE_CURSOR,
-   X_PLANE_CURSOR,
-   Y_PLANE_CURSOR,
-   Z_PLANE_CURSOR,
-   BLUE_MENU_TOGGLE,
-   SCALAR_BAR_TOGGLE,
-   CHANGE_STEADYSTATE_DATASET,
-   CHANGE_VECTOR_THRESHOLD,
-   CHANGE_VECTOR_MASK_RATIO,
-   CHANGE_VECTOR_SCALE,
-   SCALE_BY_VECTOR_MAGNITUDE,
-   BACKWARD_INTEGRATION,
-   FORWARD_INTEGRATION,
-   TWO_DIRECTION_INTEGRATION,
-   CHANGE_PROPAGATION_TIME,
-   CHANGE_INT_STEP_LENGTH,
-   CHANGE_STEP_LENGTH,
-   CHANGE_CONTOUR_FILL,
-   UPDATE_SOUNDS,
-   CHANGE_PARTICLE_VIEW_OPTION,
-   CHANGE_SPHERE_SIZE,
-   //CURRENT_VIEW_LOC,
-   //LOAD_VIEW_LOC,
-   //REMOVE_VIEW_LOC,
-   //MOVE_TO_VIEW_LOC,
-   LOAD_POINT,
-   WRITE_POINTS_TO_FILE,
-   READ_POINTS_FROM_FILE,
-   MOVE_TO_SELECTED_LOCATION,
-   DESIGN_PARMS,
-   UPDATE_DESIGNPARMS,
-   EXIT,
-   //biv--added these for the navigation page
-   GUI_NAV,
-   NAV_UP,
-   NAV_DOWN,
-   NAV_LEFT,
-   NAV_RIGHT,
-   NAV_FWD,
-   NAV_BKWD,
-   NAV_CCW,
-   NAV_CW*/
-//};
-//end Tabs.h copy/////////
 
 //////////////////////////////////////////////
 //the control for the different pages "tabs"//
@@ -194,6 +102,7 @@ public:
    VjObs::scalar_p_var   soundNameArray;
    VjObs::scalar_p_var   teacher_attrib;
    VjObs::scalar_p_var   viewlocNameArray;
+   VjObs::Models_var   _models;
    //VjObs::scalar_p_var   viewlocNewPointName;
    std::string viewlocNewPointName;
    std::string   dest;
