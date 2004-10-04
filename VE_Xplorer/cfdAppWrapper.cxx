12ca22a3d9b30924a88694d92fbaa807c9cbc450
Doug McCorkle <doug.mccorkle@gmail.com>
2004-10-04 23:29:13 +0000

    Update for new tao and scene graph stuff

---
+++ VE_Xplorer/cfdAppWrapper.cxx
@@ -0,0 +1,76 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2004 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: filename,v $
+ * Date modified: $Date: date $
+ * Version:       $Rev: 999999 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#include "cfdAppWrapper.h"
+#include <vrj/Kernel/Kernel.h>
+#include "cfdApp.h"
+#include <iostream>
+#include "cfdThread.h"
+
+#include "cfdVjObsWrapper.h"
+using namespace std;
+cfdAppWrapper::cfdAppWrapper( int argc,  char* argv[] )
+{
+   this->argc = argc;
+   this->argv = argv;
+	cfdThread* _thread = new cfdThread();
+   _thread->corba_run=new vpr::ThreadMemberFunctor<cfdAppWrapper>(this, &cfdAppWrapper::init );
+	_thread->new_thread=new vpr::Thread(_thread->corba_run);
+
+}
+
+cfdAppWrapper::~cfdAppWrapper( void )
+{
+   delete _thread;
+   delete _cfdApp;
+}
+
+void cfdAppWrapper::init( void * )
+{
+   vrj::Kernel* kernel = vrj::Kernel::instance(); // Declare a new Kernel
+   _cfdApp = new cfdApp();  // Delcare an instance of my application
+
+   for ( int i = 1; i < argc; i++ )          // Configure the kernel
+   {
+      kernel->loadConfigFile( argv[i] );  
+   }
+   
+   kernel->start();                          // Start the kernel thread
+
+   kernel->setApplication( _cfdApp );    // Give application to kernel
+   
+   kernel->waitForKernelStop();              // Block until kernel stops
+}
+
+void cfdAppWrapper::SetWrapper( cfdVjObsWrapper* input )
+{
+   _cfdApp->SetWrapper( input );
+}
