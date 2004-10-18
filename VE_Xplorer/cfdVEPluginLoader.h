a3135518ee4c9c91dc9bfb2d209ee98397024101
Steve Kirstukas <sjk60@yahoo.com>
2004-10-18 18:20:51 +0000

    added header and keywords date and revision

--- VE_Xplorer/cfdVEPluginLoader.h
+++ VE_Xplorer/cfdVEPluginLoader.h
@@ -1,3 +1,36 @@
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2004 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: cfdVEPluginLoader.h,v $
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
 #ifndef CFD_PLUGINLOADER_H
 #define CFD_PLUGINLOADER_H
 #include <wx/wx.h>
@@ -34,31 +67,32 @@ class cfdVEPluginLoader : public wxObject
 {
   //DECLARE_DYNAMIC_CLASS(cfdVEPluginLoader)
 
-    public:
+   public:
 
-  cfdVEPluginLoader();
-  ~cfdVEPluginLoader();
-  
-  bool LoadPlugins(wxString dir);
-  //Load all the dlls in the given dir
+      cfdVEPluginLoader();
+      ~cfdVEPluginLoader();
 
-  void RegisterPlugins();
+      bool LoadPlugins(wxString dir);
+      //Load all the dlls in the given dir
 
-  //Instantiate a instance of the plug_in. This instance is not used for any network composition but for information.
-  void RegisterPlugin(wxClassInfo* info);
+      void RegisterPlugins();
 
-  char* GetPluginName(int);
+      //Instantiate an instance of the plug_in. This instance is not used for any network composition but for information.
+      void RegisterPlugin(wxClassInfo* info);
 
-  int GetNumberOfPlugins();
+      char* GetPluginName(int);
 
-  cfdVEBaseClass* CreateObject( char* ); 
-  // private:
-  vector<cfdVEBaseClass*> plugins;
-  //Keep the list of the first intance of each plugin
-  vector<const wxClassInfo*> plugin_cls; 
-  //The classinfo obj of the each plugin, will be use to generate more instances
-  
-   vector<wxPluginLibrary *> libs;
+      int GetNumberOfPlugins();
+
+      cfdVEBaseClass* CreateObject( char* ); 
+
+   //private:
+      vector<cfdVEBaseClass*> plugins;
+      //Keep the list of the first intance of each plugin
+      vector<const wxClassInfo*> plugin_cls; 
+      //The classinfo obj of the each plugin, will be use to generate more instances
+
+      vector<wxPluginLibrary *> libs;
 };
 
 #endif
