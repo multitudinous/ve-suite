1d1c2df6191e8709d759b850ef74201b6c555baf
Doug McCorkle <doug.mccorkle@gmail.com>
2004-12-30 23:37:18 +0000

    more fixes to get osg to compile on linux

--- VE_Installer/dzr.osg.mk
+++ VE_Installer/dzr.osg.mk
@@ -1,5 +1,11 @@
 EXTRA_CXXFLAGS+= -D_OSG -DUSE_DEPRECATED_API
 include $(DZR_BASE_DIR)/ext/vrjuggler/dzr.vrjuggler.glapp.mk
 EXTRA_INCLUDES+= -I$(OSG_HOME)/include
-EXTRA_LIBS+= -L$(OSG_HOME)/lib32 -losg -losgDB -losgGA -losgUtil \
-            -lOpenThreads -losgFX
+
+ifeq (${CFDUNAME},IRIX64)
+   EXTRA_LIBS+= -L$(OSG_HOME)/lib32 -losg -losgDB -losgGA -losgUtil \
+            -lOpenThreads -losgFX -losgGL2
+else
+   EXTRA_LIBS+= -L$(OSG_HOME)/lib -losg -losgDB -losgGA -losgUtil \
+            -lOpenThreads -losgFX -losgGL2
+endif
