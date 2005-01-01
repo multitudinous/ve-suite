f0d246afcad91db91f550f48112b06a295c89774
Steve Kirstukas <sjk60@yahoo.com>
2005-01-01 18:08:50 +0000

    Added TAO_MACHINE and TAO_PORT definitions to setup.tsh and cleaned up some shell scripts to use these nameServer parameters

--- bin/runTAONameService.sh
+++ bin/runTAONameService.sh
@@ -1,9 +1,7 @@
 #!/bin/csh -f
 # -f means fast: don't read .cshrc file for set variables
-# used for transient data processing: to concatenate many planes into one polydata object
 
-setenv TAO_MACHINE costello.vrac.iastate.edu
-setenv TAO_PORT 1234
+# Start the nameserver, wait a few seconds, then start the executive...
 ${TAO_ROOT}/orbsvcs/Naming_Service/Naming_Service -ORBEndPoint iiop://${TAO_MACHINE}:${TAO_PORT} &
 sleep 5
 Exe_server -ORBInitRef NameService=corbaloc:iiop:${TAO_MACHINE}:${TAO_PORT}/NameService &
