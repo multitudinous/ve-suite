cb9d5e1965643ac99374a90cc78ca9c34c6e7dbe
Doug McCorkle <doug.mccorkle@gmail.com>
2005-01-02 16:58:34 +0000

    fixes to allow configuration of cluster version to be completed by config file. still need to change vjobswrapper.

--- VE_Conductor/impl/VjObs_i.cxx
+++ VE_Conductor/impl/VjObs_i.cxx
@@ -66,7 +66,7 @@ void VjObs_i::InitCluster( void )
   // Cluster Stuff
    vpr::GUID new_guid("15c09c99-ed6d-4994-bbac-83587d4400d1");
    std::string hostname = "abbott.vrac.iastate.edu";
-   this->mStates.init(new_guid,hostname);
+   this->mStates.init(new_guid);//,hostname);
    //cluster::ApplicationData* hack = dynamic_cast<cluster::ApplicationData*>(&(*this->mStates));
    //hack->setIsLocal(hostname == cluster::ClusterNetwork::instance()->getLocalHostname());
 #endif // _CLUSTER
