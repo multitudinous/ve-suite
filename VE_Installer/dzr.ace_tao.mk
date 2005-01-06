db11846f58929948ba0f3bed40fdf714fe354942
Doug McCorkle <doug.mccorkle@gmail.com>
2005-01-06 03:26:59 +0000

    initial commit to use installed version of ace tao on unix. I realize that there are duplicate ace/tao env vars in setup.tsh but those will be cleaned up in due time. please leave them in for the time being. this change is also only on irix so the linux build will be broken until tomorrow.

--- VE_Installer/dzr.ace_tao.mk
+++ VE_Installer/dzr.ace_tao.mk
@@ -3,22 +3,21 @@ CFDUNAME = $(shell uname)
 
 ifeq (${CFDUNAME},Linux)
 EXTRA_CXXFLAGS+= -W -Wall -Wpointer-arith -pipe -O3 -g -D_TAO -D_REENTRANT -DACE_HAS_AIO_CALLS -D_GNU_SOURCE -DACE_HAS_EXCEPTIONS -D__ACE_INLINE__
-EXTRA_LIBS+=  -L${TAO_ROOT}/Linux/lib -L${ACE_ROOT}/Linux/lib 
-EXTRA_INCLUDES+= -I${TAO_ROOT}/Linux/include -I${ACE_ROOT}/Linux/include 
+EXTRA_LIBS+=  -L${TAO_HOME}/Linux/lib -L${ACE_HOME}/Linux/lib 
+EXTRA_INCLUDES+= -I${TAO_HOME}/Linux/include -I${ACE_HOME}/Linux/include 
 endif
 
 ifeq (${CFDUNAME},IRIX64)
-EXTRA_CXXFLAGS+=  -DACE_HAS_EXCEPTIONS -DACE_IRIX_VERS=65 -D_SGI_MP_SOURCE -DACE_HAS_XT  -D__ACE_INLINE__
-EXTRA_LIBS+=  -L${TAO_ROOT}/IRIX32/lib -L${ACE_ROOT}/IRIX32/lib 
-EXTRA_INCLUDES+= -I${TAO_ROOT}/IRIX32/include -I${ACE_ROOT}/IRIX32/include 
+EXTRA_CXXFLAGS+= -D_TAO -DACE_HAS_EXCEPTIONS -DACE_IRIX_VERS=65 -D_SGI_MP_SOURCE -DACE_HAS_XT  -D__ACE_INLINE__
+EXTRA_LIBS+=  -L${TAO_HOME}/IRIX32/lib -L${ACE_HOME}/IRIX32/lib 
+EXTRA_INCLUDES+= -I${TAO_HOME}/IRIX32/include -I${ACE_HOME}/IRIX32/include 
 endif
 
 EXTRA_LIBS+= -lTAO_PortableServer -lTAO_IORInterceptor -lTAO_ObjRefTemplate -lTAO_Valuetype \
                -lTAO -lACE -lpthread -lTAO_CosNaming -lTAO_Svc_Utils -lTAO_IORTable \
                -lTAO_Messaging -lTAO_PortableServer -lpthread -lTAO_BiDirGIOP
 
-CPPFLAGS+= ${EXTRA_CXXFLAGS} ${EXTRA_INCLUDES}
-EXTRA_INCLUDES+= -I${TAO_ROOT}/include -I${ACE_ROOT}/include 
+EXTRA_INCLUDES+= -I${TAO_HOME}/include -I${ACE_HOME}/include 
 
 
 LDFLAGS+= ${EXTRA_LIBS}
