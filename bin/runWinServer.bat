0a25d0ffb8fb176190106508f41daca6e930bdf4
Steve Kirstukas <sjk60@yahoo.com>
2005-01-10 23:19:30 +0000

    Now that Doug has bin/runTAONameService.bat working, bin/runWinServer.bat is obsolete.

--- bin/runWinServer.bat
+++
@@ -1,5 +0,0 @@
-REM Specify the environment variables
-call ..\VE_Installer\setup.bat
-
-%VE_SUITE_HOME%\futuregen\Executive\Debug\WinServer.exe -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService
-cmd
