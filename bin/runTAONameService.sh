078a7cfd18c41532493da50219422aa0c6143c76
Doug McCorkle <doug.mccorkle@gmail.com>
2005-01-13 15:26:51 +0000

    changes to run tao

--- bin/runTAONameService.sh
+++ bin/runTAONameService.sh
@@ -1,5 +1,7 @@
 #!/bin/csh -f
 # -f means fast: don't read .cshrc file for set variables
+setenv TAO_MACHINE abbott.vrac.iastate.edu
+setenv TAO_PORT 1234
 
 # Start the nameserver, wait a few seconds, then start the executive...
 ${TAO_ROOT}/orbsvcs/Naming_Service/Naming_Service -ORBEndPoint iiop://${TAO_MACHINE}:${TAO_PORT} &
