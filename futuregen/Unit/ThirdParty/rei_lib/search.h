193b6673f44a74253c9d183bdd1e5c462da52cc1
Doug McCorkle <doug.mccorkle@gmail.com>
2005-01-16 02:47:26 +0000

    more directory moves...

--- futuregen/Unit/ThirdParty/rei_lib/search.h
+++
@@ -1,9 +0,0 @@
-#ifndef REI_SEARCH_H
-#define REI_SEARCH_H
-
-/* function prototypes */
-
-void search_i(struct src_cell **scell,unsigned int ni,float x,int *ilo);
-void search_j(struct src_cell **scell,unsigned int nj,float y,int *jlo);
-
-#endif
