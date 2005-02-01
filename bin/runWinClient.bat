ee5518da4dc69ad0df48ca10101a9cd68ee6b1f3
Doug McCorkle <doug.mccorkle@gmail.com>
2005-02-01 22:38:24 +0000

    commented out deere specific code.

--- bin/runWinClient.bat
+++ bin/runWinClient.bat
@@ -5,7 +5,7 @@ call ..\VE_Installer\setup.bat
 
 REM Go to where your GUIPlugins directory is...
 REM cd %VE_SUITE_HOME%\VE_Conductor\Framework\
-cd %VE_SUITE_HOME%\VE_Conductor\Framework\
+REM cd %VE_SUITE_HOME%\..\deere\harvester
 
-%VE_SUITE_HOME%\VE_Conductor\Framework\WinClient.exe -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService 
-REM cmd
+%VE_SUITE_HOME%\VE_Conductor\Framework\WinClient.exe -ORBDebugLevel 10 -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService 
+cmd
