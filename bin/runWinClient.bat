b4eb1672480e30be3b18eac8fa588c20ad312224
Doug McCorkle <doug.mccorkle@gmail.com>
2005-02-16 23:20:14 +0000

    fixes so that you don't have to disable network interfaces on win32 to work with the nameservice

--- bin/runWinClient.bat
+++ bin/runWinClient.bat
@@ -5,7 +5,6 @@ call ..\VE_Installer\setup.bat
 
 REM Go to where your GUIPlugins directory is...
 REM cd %VE_SUITE_HOME%\VE_Conductor\Framework\
-REM cd %VE_SUITE_HOME%\..\deere\harvester
 
-WinClientd.exe -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService 
+WinClientd.exe -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService -ORBDottedDecimalAddresses 1
 cmd
