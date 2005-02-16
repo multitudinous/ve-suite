3f36c5bb30fed420a8f0f22656638cfcf986340a
Doug McCorkle <doug.mccorkle@gmail.com>
2005-02-16 23:18:27 +0000

    fixes so that you don't have to disable network interfaces on win32 to work with the nameservice

--- bin/runTAONameService.bat
+++ bin/runTAONameService.bat
@@ -3,6 +3,6 @@ call ..\VE_Installer\setup.bat
 
 start /B %TAO_ROOT%/orbsvcs/Naming_Service/Naming_Service.exe -ORBEndPoint iiop://%TAO_MACHINE%:%TAO_PORT%
 PING 1.1.1.1 -n 1 -w 5000 >NUL
-start /B WinServerd.exe -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService
+start /B WinServerd.exe -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService -ORBDottedDecimalAddresses 1 
 cmd
-REM -m 0 -ORBDebugLevel 10 -ORBDottedDecimalAddresses 1
+REM -ORBDebugLevel 10
