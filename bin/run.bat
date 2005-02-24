d647ab0c08ddb9518f96bac6d1641db86f27b0f9
Steve Kirstukas <sjk60@yahoo.com>
2005-02-24 17:12:07 +0000

    Specifying full path for the executable

--- bin/run.bat
+++ bin/run.bat
@@ -3,7 +3,8 @@ REM Double click this file to start the app in the test directory
 REM Specify the environment variables
 call ..\VE_Installer\setup.bat
 
+REM Go to where your parameter file is...
 cd %VE_SUITE_HOME%\VE_TestSuite
 
-project_taod.exe %VJ_BASE_DIR%/share/vrjuggler/data/configFiles/simstandalone.jconf -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService -ORBDottedDecimalAddresses 1
+%VE_SUITE_HOME%\bin\win32\project_taod.exe %VJ_BASE_DIR%/share/vrjuggler/data/configFiles/simstandalone.jconf -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService -ORBDottedDecimalAddresses 1
 cmd
