ec689e5bf12bb6cfd448b0d519b385ee656a57ba
Doug McCorkle <doug.mccorkle@gmail.com>
2005-03-01 16:22:51 +0000

    Removed full path since path is specified in the setup file.

--- bin/run.bat
+++ bin/run.bat
@@ -6,5 +6,5 @@ call ..\VE_Installer\setup.bat
 REM Go to where your parameter file is...
 cd %VE_SUITE_HOME%\VE_TestSuite
 
-%VE_SUITE_HOME%\bin\win32\project_taod.exe %VJ_BASE_DIR%/share/vrjuggler/data/configFiles/simstandalone.jconf -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService -ORBDottedDecimalAddresses 1
+project_taod.exe %VJ_BASE_DIR%/share/vrjuggler/data/configFiles/simstandalone.jconf -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService -ORBDottedDecimalAddresses 1
 cmd
