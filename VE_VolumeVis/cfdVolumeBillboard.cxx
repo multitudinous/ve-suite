b003a8ee40bc733eba8895b92c20b21a0e47b969
Biv <biv@unknown.nil>
2005-05-21 15:18:18 +0000

    overriding billboard to begin debugging. . .not fixed but I wanted to check in so I could work on it at the labs

---
+++ VE_VolumeVis/cfdVolumeBillboard.cxx
@@ -0,0 +1,24 @@
+#ifdef _OSG
+#include "cfdVolumeBillboard.h"
+
+bool cfdVolumeBillboard::computeMatrix(osg::Matrix& modelview,
+                                   const osg::Vec3& eye_local,
+                                   const osg::Vec3& pos_local) const
+{
+   //osg::Billboard::computeMatrix(modelview,eye_local,pos_local);
+   osg::Matrix matrix;
+   osg::Vec3 ev(eye_local-pos_local);
+   osg::Vec3f eyeTemp; 
+   osg::Vec3f center;
+   osg::Vec3f up;
+   modelview.getLookAt(eyeTemp, center, up);
+   //billboards matrix is basically looking at 
+   //the camera from the rotation center
+   matrix.makeLookAt(_positionList[0],eyeTemp,up);
+   
+   //matrix.setTrans(pos_local);
+   modelview.preMult(matrix);
+   return true;
+   
+}
+#endif
