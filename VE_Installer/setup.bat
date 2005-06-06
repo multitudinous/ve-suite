8d757fd5d7eddaddba9d75580b290e0d2caffe8a
Doug McCorkle <doug.mccorkle@gmail.com>
2005-06-06 02:50:30 +0000

    updates for new osg flags and new path dir for wx and vesuite

--- VE_Installer/setup.bat
+++ VE_Installer/setup.bat
@@ -5,17 +5,16 @@ set VJ_BASE_DIR=C:\vrjuggler-2.0-beta2.win32-vc71
 set VJ_DEPS_DIR=C:\vrjuggler-2.0-beta2.win32-vc71-deps
 set VTK_HOME=C:\VTK_4.4_WinXP
 set OMNI_HOME=C:\vrjuggler-2.0-beta2.win32-vc71-deps
-set WX_HOME=C:\wxWindows-2.4.2
+set WX_HOME=C:\wxWidgets-2.6.0
 set JAVA_HOME=C:\j2sdk1.4.2_06
 set ACE_ROOT=C:\ACE_wrappers
 set TAO_ROOT=%ACE_ROOT%\TAO
 set XERCESCROOT=C:\xerces-c-src_2_5_0
-set WX_ROOT=C:\wxWindows-2.4.2
+set WX_ROOT=C:\wxWidgets-2.6.0
 set OSGHOME=C:\OSG_OP_OT-0.9.8-2\OpenSceneGraph
-
-REM set TAO_MACHINE=lego.vrac.iastate.edu
 set TAO_MACHINE=localhost
-set TAO_PORT=1237
+set TAO_PORT=1239
+set SNX_BASE_DIR=%VJ_BASE_DIR%
 
 REM NOTE: On Windows the juggler location must point to a Windows
 REM (local or on network) machine, otherwise user may run into runtime
@@ -37,6 +36,7 @@ set VPR_DEBUG_NFY_LEVEL=1
 set NO_PERF_PLUGIN=TRUE
 set NO_RTRC_PLUGIN=TRUE
 set PFNFYLEVEL=0
+set OSG_THREAD_SAFE_REF_UNREF=1
 
 REM Juggler dependencies
 set JCCL_DEFINITION_PATH=%VJ_BASE_DIR%\share\vrjuggler\data\definitions
@@ -50,7 +50,7 @@ REM Python build environment variables
 set PYTHONPATH=%VJ_DEPS_DIR%\lib\python
 
 set Path=%Path%;%VJ_DEPS_DIR%\bin;%VJ_DEPS_DIR%\lib;%VJ_BASE_DIR%\lib
-set Path=%Path%;%VE_SUITE_HOME%\bin;%VTK_HOME%\bin;%WX_HOME%\lib
+set Path=%Path%;%VE_SUITE_HOME%\bin;%VTK_HOME%\bin;%WX_HOME%\lib\vc_dll
 set Path=%Path%;%OMNI_HOME%\bin\x86_win32;%VE_SUITE_HOME%\bin\win32
 set Path=%Path%;%ACE_ROOT%\bin;%XERCESCROOT%\Build\Win32\VC7\Debug
-set Path=%Path%;%OSGHOME%\bin
+set Path=%Path%;%OSGHOME%\bin;%VE_SUITE_HOME%\lib\win32
