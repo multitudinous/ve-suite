7c000ed9ae7049413e795cb956245965e2591662
Doug McCorkle <doug.mccorkle@gmail.com>
2005-06-11 21:16:10 +0000

    updates to support osg on 64 bit linux and 32 bit linux

--- VE_Installer/dzr.osg.mk
+++ VE_Installer/dzr.osg.mk
@@ -1,14 +1,25 @@
 EXTRA_CXXFLAGS+= -D_OSG -DUSE_DEPRECATED_API
 include $(DZR_BASE_DIR)/ext/vrjuggler/dzr.vrjuggler.glapp.mk
 EXTRA_INCLUDES+= -I$(OSG_HOME)/include
+CFDPLATFORM = $(shell uname -i)
 
-ifeq (${CFDUNAME},IRIX64)
-   EXTRA_LIBS+= -L$(OSG_HOME)/lib32 -losg -losgDB -losgGA -losgUtil \
-            -lOpenThreads -losgFX -losgGL2
-else
-   EXTRA_LIBS+= -L$(OSG_HOME)/lib -losg -losgDB -losgGA -losgUtil \
+ifeq (${CFDPLATFORM},x86_64)
+   EXTRA_LIBS+= -L$(OSG_HOME)/lib -L$(OSG_HOME)/lib64 -losg -losgDB -losgGA -losgUtil \
             -lOpenThreads -losgFX 
-endif
-ifeq (${CFD_USE_SHADERS},TRUE)
-   EXTRA_LIBS+= -losgNV -losgNVCg -losgNVExt -lCg -lCgGL
+
+   ifeq (${CFD_USE_SHADERS},TRUE)
+      EXTRA_LIBS+= -losgNV -losgNVCg -losgNVExt -lCg -lCgGL
+   endif
+else
+   ifeq (${CFDUNAME},IRIX64)
+      EXTRA_LIBS+= -L$(OSG_HOME)/lib32 -losg -losgDB -losgGA -losgUtil \
+            -lOpenThreads -losgFX 
+   else
+      EXTRA_LIBS+= -L$(OSG_HOME)/lib -losg -losgDB -losgGA -losgUtil \
+            -lOpenThreads -losgFX 
+   endif
+
+   ifeq (${CFD_USE_SHADERS},TRUE)
+      EXTRA_LIBS+= -losgNV -losgNVCg -losgNVExt -lCg -lCgGL
+   endif
 endif
