13d308587f1fdfe4dcf3f9ebcd4f30163b821724
Biv <biv@unknown.nil>
2005-06-27 03:41:00 +0000

    dll import/export defines

---
+++ VE_Installer/VEConfig.h
@@ -0,0 +1,78 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2005 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: cfdVEBaseClass.h,v $
+ * Date modified: $Date: 2005-05-25 23:32:59 -0500 (Wed, 25 May 2005) $
+ * Version:       $Rev: 2321 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef VE_SUITE_CONFIG_
+#define VE_SUITE_CONFIG_ 1
+
+#if defined(_MSC_VER)
+    #pragma warning( disable : 4244 )
+    #pragma warning( disable : 4251 )
+    #pragma warning( disable : 4267 )
+    #pragma warning( disable : 4275 )
+    #pragma warning( disable : 4290 )
+    #pragma warning( disable : 4786 )
+    #pragma warning( disable : 4305 )
+    #pragma warning( disable : 4996 )
+#endif
+
+#if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__) || defined( __MWERKS__)
+	#  ifdef VE_SCENEGRAPH_LIBRARY
+	#    define VE_SCENEGRAPH_EXPORTS   __declspec(dllexport)
+	#  else
+	#    define VE_SCENEGRAPH_EXPORTS   __declspec(dllimport)
+	#  endif /* VE_SCENEGRAPH_LIBRARY */
+
+	#  ifdef VE_XPLORER_LIBRARY
+	#    define VE_XPLORER_EXPORTS   __declspec(dllexport)
+	#  else
+	#    define VE_XPLORER_EXPORTS   __declspec(dllimport)
+	#  endif /* VE_XPLORER_LIBRARY */
+
+	#  ifdef VE_PLUGINS_LIBRARY
+	#    define VE_PLUGINS_EXPORTS   __declspec(dllexport)
+	#  else
+	#    define VE_PLUGINS_EXPORTS   __declspec(dllimport)
+	#  endif /* VE_PLUGINS_LIBRARY */
+
+	#  ifdef VE_TEXTURE_BASED_LIBRARY
+	#    define VE_TEXTURE_BASED_EXPORTS   __declspec(dllexport)
+	#  else
+	#    define VE_TEXTURE_BASED_EXPORTS   __declspec(dllimport)
+	#  endif /* VE_TEXTURE_BASED_LIBRARY */
+#else
+	#  define VE_SCENEGRAPH_EXPORTS
+	#  define VE_XPLORER_EXPORTS
+	#  define VE_CONDUCTOR_EXPORTS
+	#  define VE_PLUGINS_EXPORTS
+	#  define VE_TEXTURE_BASED_EXPORTS
+#endif
+
+#endif
