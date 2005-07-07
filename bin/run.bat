ca3069700da2d2f80ea3e23e7483675038cb6126
Biv <biv@unknown.nil>
2005-07-07 15:30:50 +0000

    accidently committed the wrong files to the repository

--- bin/run.bat
+++ bin/run.bat
@@ -4,11 +4,7 @@ REM Specify the environment variables
 call ..\VE_Installer\setup.bat
 
 REM Go to where your parameter file is...
-REM cd %VE_SUITE_HOME%\VE_TestSuite
-REM cd C:\VE_Suite_112204\VE_TestSuite\dClarke
-REM cd C:\VE_Suite_112204\VE_TestSuite\cpicker
-REM cd C:\VE_Suite_112204\VE_TestSuite\deereEngine
-cd C:\VE_Suite_112204\VE_TestSuite\angran
+cd %VE_SUITE_HOME%\VE_TestSuite
 
-project_tao_osgd.exe %VJ_BASE_DIR%/share/vrjuggler/data/configFiles/simstandalone.jconf -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService -ORBDottedDecimalAddresses 1
+project_taod.exe %VJ_BASE_DIR%/share/vrjuggler/data/configFiles/simstandalone.jconf -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService -ORBDottedDecimalAddresses 1
 cmd
