50565d5151eeeb01eaf147dedac715daa5945bb3
Doug McCorkle <doug.mccorkle@gmail.com>
2005-07-08 14:22:23 +0000

    added error handling for corba code in main function

--- VE_Xplorer/cfdVEXplorer.cxx
+++ VE_Xplorer/cfdVEXplorer.cxx
@@ -45,66 +45,86 @@ using namespace VE_Xplorer;
 
 int main(int argc, char* argv[])
 {
-#ifdef _TAO
-   CORBA::ORB_var orb = CORBA::ORB_init( argc, argv,"VE_Suite_ORB" );
-#else
-   CORBA::ORB_var orb = CORBA::ORB_init( argc, argv,"omniORB4" ); 
-#endif // _TAO
-   if ( CORBA::is_nil( orb.in() ) )
-      exit(0);
+   try
+   {
+   #ifdef _TAO
+      CORBA::ORB_var orb = CORBA::ORB_init( argc, argv,"VE_Suite_ORB" );
+   #else
+      CORBA::ORB_var orb = CORBA::ORB_init( argc, argv,"omniORB4" ); 
+   #endif // _TAO
+      if ( CORBA::is_nil( orb.in() ) )
+         exit(0);
   
-   //Here is the part to contact the naming service and get the reference for the executive
-   CORBA::Object_var naming_context_object =
-     orb->resolve_initial_references ("NameService"); 
-   CORBA::String_var sior1(orb->object_to_string( naming_context_object.in() ) );
-   std::cout << "|  IOR of the server side : " << std::endl << sior1 << std::endl;
-   CosNaming::NamingContext_var naming_context = CosNaming::NamingContext::_narrow (naming_context_object.in());
+      //Here is the part to contact the naming service and get the reference for the executive
+      CORBA::Object_var naming_context_object =
+         orb->resolve_initial_references ("NameService"); 
+      CORBA::String_var sior1(orb->object_to_string( naming_context_object.in() ) );
+      std::cout << "|  IOR of the server side : " << std::endl << sior1 << std::endl;
+      CosNaming::NamingContext_var naming_context = CosNaming::NamingContext::_narrow (naming_context_object.in());
 
-   //Here is the code to set up the server
-   CORBA::Object_var poa_object = orb->resolve_initial_references ("RootPOA"); // get the root poa
-   PortableServer::POA_var poa = PortableServer::POA::_narrow(poa_object.in());
-   PortableServer::POAManager_var poa_manager = poa->the_POAManager();
+      //Here is the code to set up the server
+      CORBA::Object_var poa_object = orb->resolve_initial_references ("RootPOA"); // get the root poa
+      PortableServer::POA_var poa = PortableServer::POA::_narrow(poa_object.in());
+      PortableServer::POAManager_var poa_manager = poa->the_POAManager();
 
-#ifdef _TAO
-   // Create policy with BiDirPolicy::BOTH
-   CORBA::PolicyList policies( 1 );
-   policies.length( 1 );
+   #ifdef _TAO
+      // Create policy with BiDirPolicy::BOTH
+      CORBA::PolicyList policies( 1 );
+      policies.length( 1 );
 
-   CORBA::Any pol;
-   pol <<= BiDirPolicy::BOTH;
-   policies[ 0 ] = orb->create_policy( BiDirPolicy::BIDIRECTIONAL_POLICY_TYPE,
+      CORBA::Any pol;
+      pol <<= BiDirPolicy::BOTH;
+      policies[ 0 ] = orb->create_policy( BiDirPolicy::BIDIRECTIONAL_POLICY_TYPE,
                      pol);
 
-   // Create POA as child of RootPOA with the above policies.  This POA 
-   // will receive request in the same connection in which it sent 
-   // the request 
+      // Create POA as child of RootPOA with the above policies.  This POA 
+      // will receive request in the same connection in which it sent 
+      // the request 
 
-   PortableServer::POA_var child_poa = poa->create_POA ("childPOA",
+      PortableServer::POA_var child_poa = poa->create_POA ("childPOA",
                        poa_manager.in(),
                        policies);
 
-   // Creation of childPOA is over. Destroy the Policy objects. 
-   for (CORBA::ULong i = 0; i < policies.length (); ++i)
-   {
+      // Creation of childPOA is over. Destroy the Policy objects. 
+      for (CORBA::ULong i = 0; i < policies.length (); ++i)
+      {
          policies[i]->destroy();
+      }
+   #endif // _TAO
+
+      poa_manager->activate();
+
+      cfdVjObsWrapper* vjobsWrapper = new cfdVjObsWrapper();
+   #ifdef _TAO
+      vjobsWrapper->init( naming_context.in(), orb.in(), child_poa.in(), NULL, argc, argv );
+   #else
+      vjobsWrapper->init( naming_context.in(), orb.in(), argc, argv );
+   #endif // _TAO
+      cfdAppWrapper* appWrapper = new cfdAppWrapper( argc, argv, vjobsWrapper );
+
+      orb->run();
+
+      // VjObsWrapper is now deleted in cfdApp on exit
+      // delete vjobsWrapper;
+      delete appWrapper;
    }
-#endif // _TAO
-
-   poa_manager->activate();
-
-   cfdVjObsWrapper* vjobsWrapper = new cfdVjObsWrapper();
-#ifdef _TAO
-   vjobsWrapper->init( naming_context.in(), orb.in(), child_poa.in(), NULL, argc, argv );
-#else
-   vjobsWrapper->init( naming_context.in(), orb.in(), argc, argv );
-#endif // _TAO
-   cfdAppWrapper* appWrapper = new cfdAppWrapper( argc, argv, vjobsWrapper );
-
-   orb->run();
-
-   // VjObsWrapper is now deleted in cfdApp on exit
-   // delete vjobsWrapper;
-   delete appWrapper;
+   catch ( CORBA::SystemException& ) 
+   {
+      std::cerr << "Caught CORBA::SystemException." << std::endl
+                  << " The nameserver is probably not started yet or " << std::endl
+                  << " the computer name and port number passed into " << std::endl
+                  << " VE-Xplorer do not match the computer name and " << std::endl
+                  << " port number specified in the VES script.      " << std::endl
+                  << " Please start the nameserver with -> VES -nserv . " << std::endl;
+   }
+   catch ( CORBA::Exception& ) 
+   {
+      std::cerr << "Caught CORBA::Exception." << std::endl;
+   }
+   catch ( ... ) 
+   {
+      std::cerr << "Caught unknown exception." << std::endl;
+   }
 
    return 0;
 }
