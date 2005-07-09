a0c4428c56a213274688f1872d7bbbb05d6e5d5d
Doug McCorkle <doug.mccorkle@gmail.com>
2005-07-09 13:25:17 +0000

    fix for irix build

--- VE_Installer/mk/dzr.wx.mk
+++ VE_Installer/mk/dzr.wx.mk
@@ -1,2 +1,7 @@
 EXTRA_CXXFLAGS+= $(shell wx-config --cxxflags) -DWXUSINGDLL
 EXTRA_LIBS+= $(shell wx-config --libs)
+
+CFDUNAME = $(shell uname)
+ifeq (${CFDUNAME},IRIX64)
+EXTRA_LIBS+= -L/usr/freeware/lib32 
+endif
