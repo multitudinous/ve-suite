654bed415306ed5820b0bb20b06bebb45ea9d415
Biv <biv@unknown.nil>
2005-09-19 13:49:36 +0000

    translator base class added for win32

---
+++ VE_Builder/Translator/cfdTranslatorToVTK/cfdTranslatorToVTK.cxx
@@ -0,0 +1,187 @@
+#include "VE_Builder/Translator/cfdTranslatorToVTK/cfdTranslatorToVTK.h"
+#include <bitset>
+#include <sstream>
+#include <vtkDataset.h>
+#include "VE_Xplorer/readWriteVtkThings.h"
+using namespace VE_Builder;
+////////////////////////////////////////
+//Constructor                         //
+////////////////////////////////////////
+cfdTranslatorToVTK::cfdTranslatorToVTK()
+{
+   _nFoundFiles = 0;
+
+   _fileExtension = ".*";
+   _inputDir = "./";
+   _outputDir = "./";
+
+   _preTCbk = 0;
+   _postTCbk = 0;
+   _translateCbk = 0;
+
+   _outputDataset = 0;
+}
+/////////////////////////////////////////
+cfdTranslatorToVTK::~cfdTranslatorToVTK()
+{
+   if(_outputDataset)
+   {
+      _outputDataset->Delete();
+   }
+   
+   _outfileNames.clear();
+   _infileNames.clear();
+}
+////////////////////////////////////////////////////////////////////   
+void cfdTranslatorToVTK::SetFileExtension(std::string fileExtension)
+{
+   _fileExtension = fileExtension;
+}
+/////////////////////////////////////////////////////////////
+void cfdTranslatorToVTK::SetInputDirectory(std::string inDir)
+{
+   _inputDir = inDir;
+}
+///////////////////////////////////////////////////////////////
+void cfdTranslatorToVTK::SetOutputDirectory(std::string outDir)
+{
+   _outputDir = outDir;
+}
+//////////////////////////////////////////////////////////////////////////////   
+void cfdTranslatorToVTK::SetPreTranslateCallback(PreTranslateCallback* preTCbk)
+{
+   _preTCbk = preTCbk;
+}
+/////////////////////////////////////////////////////////////////////////////////   
+void cfdTranslatorToVTK::SetPostTranslateCallback(PostTranslateCallback* postTCbk)
+{
+   _postTCbk = postTCbk;
+}
+/////////////////////////////////////////////////////////////////////   
+void cfdTranslatorToVTK::SetTranslateCallback(TranslateCallback* tCbk)
+{
+   _translateCbk = tCbk;
+}
+////////////////////////////////////////////////////////////////////////
+void cfdTranslatorToVTK::SetNumberOfFoundFiles(unsigned int nFilesFound)
+{
+   _nFoundFiles = nFilesFound;
+}
+//////////////////////////////////////////////////   
+std::string cfdTranslatorToVTK::GetFileExtension()
+{
+   return _fileExtension;
+}
+//////////////////////////////////////////////////   
+std::string cfdTranslatorToVTK::GetInputDirectory()
+{
+   return _inputDir;
+}
+////////////////////////////////////////////////////   
+std::string cfdTranslatorToVTK::GetOutputDirectory()
+{
+   return _outputDir;
+}
+////////////////////////////////////////////////////////
+unsigned int cfdTranslatorToVTK::GetNumberOfFoundFiles()
+{
+   return _nFoundFiles;
+}
+//////////////////////////////////////////////////////////////
+bool cfdTranslatorToVTK::TranslateToVTK(int argc, char** argv)
+{
+   //this may be modified later to handle multiple files
+   if(_preTCbk)
+   {
+      _preTCbk->Preprocess(argc,argv,this);
+   }
+   //just in case the Preprocess didn't set this
+   if(!_nFoundFiles && _infileNames.size())
+   {
+      _nFoundFiles = _infileNames.size();
+   }
+
+   std::bitset<16> status;
+
+   for(unsigned int i = 0; i < _nFoundFiles; i++)
+   {
+      if(_translateCbk)
+      {
+         _translateCbk->Translate(_outputDataset,this);
+      }
+      if(_postTCbk)
+      {
+         _postTCbk->PostProcess(this);
+      }
+      status.set(i,_writeToVTK(i));
+   }
+   for(unsigned int i = 0; i < status.size(); i++)
+   {
+      if(!status.test(i))
+      {
+        return false;
+      }
+   }
+   return true;
+}
+//////////////////////////////////////////////////////////
+bool cfdTranslatorToVTK::_writeToVTK(unsigned int fileNum)
+{
+   if(_outputDataset)
+   {
+      if(_outfileNames.empty())
+      {
+         std::stringstream tempName;
+         tempName<<_outputDir<<"/flowdata_"<<fileNum<<".vtk"<<"\0";
+         _outfileNames.push_back(tempName.str());
+      }
+      VE_Util::writeVtkThing( _outputDataset, (char*)_outfileNames.at(fileNum).c_str(),0);
+      return true;
+   }else{
+      std::cout<<"Invalid output vtk dataset!!!"<<std::endl;
+      std::cout<<"cfdTranslatorToVTK::_writeToVTK"<<std::endl;
+      return false;
+   }
+}
+////////////////////////////////////////////////////////////////////////////////////
+//This is the default behavior to look for the input and output directory from the//
+//command line.                                                                   //
+////////////////////////////////////////////////////////////////////////////////////
+void cfdTranslatorToVTK::PreTranslateCallback::Preprocess(int argc, char** argv,
+		                                              cfdTranslatorToVTK* toVTK)
+{
+   if(toVTK)
+   {
+      std::string inDir;
+      if(_extractOptionFromCmdLine(argc,argv,std::string("-i"),inDir))
+      {
+         toVTK->SetInputDirectory(inDir);
+         toVTK->SetNumberOfFoundFiles(1);
+      }
+      std::string outDir;
+      if(_extractOptionFromCmdLine(argc,argv,std::string("-o"),outDir))
+      {
+         toVTK->SetOutputDirectory(outDir);
+      }
+   }
+}
+/////////////////////////////////////////////////////////////////////////////////
+bool cfdTranslatorToVTK::PreTranslateCallback::_extractOptionFromCmdLine(int argc,
+                                                               char** argv,
+                                                      std::string optionFlag,
+                                                      std::string& optionArg)
+{
+   std::cout << "Number of arguments: " << argc << std::endl;
+   for(int i = 0; i < argc; i++)
+   {
+      std::string curArg(argv[i]);
+      if(curArg == optionFlag)
+      {
+         optionArg = std::string(argv[++i]);
+         std::cout<<"Found option: "<<optionFlag<<":"<<optionArg<<std::endl;
+
+         return true;
+      }
+   }
+   return false;
+}
---
+++ VE_Builder/Translator/cfdTranslatorToVTK/cfdTranslatorToVTK.h
@@ -0,0 +1,104 @@
+#ifndef _CFD_TRANSLATOR_TO_VTK_H_
+#define _CFD_TRANSLATOR_TO_VTK_H_
+#include <string>
+#include <vector>
+
+
+class vtkDataSet;
+#include "VE_Installer/include/VEConfig.h"
+namespace VE_Builder{
+class VE_BUILDER_EXPORTS cfdTranslatorToVTK{
+public:
+   cfdTranslatorToVTK();
+   virtual ~cfdTranslatorToVTK();
+   
+   void SetFileExtension(std::string fileExtension);
+   void SetNumberOfFoundFiles(unsigned int nFilesFound);
+   void SetInputDirectory(std::string inDir);
+   void SetOutputDirectory(std::string inDir);
+
+   /////////////////////////////////////////////////////
+   //The idea for these callback classes is similar to//
+   //the setup in OSG (www.openscenegraph.org)        //
+   /////////////////////////////////////////////////////
+   
+   ////////////////////////////////////////////////////////
+   //Any preprocessing can be easily setup to happen here//
+   ////////////////////////////////////////////////////////
+   class PreTranslateCallback{
+   public:
+      PreTranslateCallback(){};
+      virtual ~PreTranslateCallback(){};
+      virtual void Preprocess(int argc, char** argv,
+		              cfdTranslatorToVTK* toVTK);
+   protected:
+      bool _extractOptionFromCmdLine(int argc,char** argv,
+                                  std::string optionFlag,
+                                  std::string& optionArg);
+   };
+   /////////////////////////////////////////////////////////
+   //translate callback must be defined or nothing happens//
+   /////////////////////////////////////////////////////////
+   class TranslateCallback{
+   public:
+      TranslateCallback(){};
+      virtual ~TranslateCallback(){};
+      //////////////////////////////////////////////////
+      //ouputDataset should be populated              //
+      //appropriately by the translate callback.      //
+      //////////////////////////////////////////////////
+      virtual void Translate(vtkDataSet*& outputDataset,
+		             cfdTranslatorToVTK* toVTK) = 0;
+   protected:
+   };
+   ///////////////////////////////////////////////////////////
+   //Any post-processing can be easily setup to happen here //
+   ///////////////////////////////////////////////////////////
+   class PostTranslateCallback{
+   public:
+      PostTranslateCallback();
+      virtual ~PostTranslateCallback();
+      virtual void PostProcess(cfdTranslatorToVTK* toVTK) = 0;
+   protected:
+   };
+
+   void SetPreTranslateCallback(PreTranslateCallback* preTCbk);
+   void SetPostTranslateCallback(PostTranslateCallback* postTCbk);
+   void SetTranslateCallback(TranslateCallback* tCbk);
+
+   /////////////////////////////////////////
+   //main translation calling method      //
+   //Basically makes the following calls: //
+   //PreTranslateCallback::Preprocess();  //
+   //TranslateCallback::Translate();      // 
+   //PostTranslateCallback::PostProcess();//
+   //_writeVTKFile();                      //
+   /////////////////////////////////////////
+   bool TranslateToVTK(int argc, char** argv);
+   
+   std::string GetFileExtension();
+   std::string GetInputDirectory();
+   std::string GetOutputDirectory();
+
+   unsigned int GetNumberOfFoundFiles();
+   std::string GetFile(unsigned int fileNumber);
+
+protected:
+   bool _writeToVTK(unsigned int whichFile);
+   unsigned int _nFoundFiles;
+
+   std::string _fileExtension;
+   std::string _inputDir;
+   std::string _outputDir;
+   std::vector<std::string> _infileNames;
+
+   std::vector<std::string> _outfileNames;
+
+   PreTranslateCallback* _preTCbk;
+   PostTranslateCallback* _postTCbk;
+   TranslateCallback* _translateCbk;
+
+   vtkDataSet* _outputDataset;
+};
+}
+#endif //_CFD_TRANSLATOR_TO_VTK_H_
