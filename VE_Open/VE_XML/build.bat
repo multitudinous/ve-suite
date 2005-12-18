2e18b7c82bc6dfd1544c6b164a506e53aff832e7
Doug McCorkle <doug.mccorkle@gmail.com>
2005-12-18 01:54:29 +0000

    removed unneeded files

--- VE_Open/VE_XML/build.bat
+++
@@ -1,8 +0,0 @@
-REM Double click this file to start Visual Studio with the projects loaded
-
-REM Specify the environment variables
-call setup.bat
-
-REM only change this if your Visual Studio .NET is installed somewhere else
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe" %VE_SUITE_HOME%\..\VE_XML\VE_XML.sln
-cmd
