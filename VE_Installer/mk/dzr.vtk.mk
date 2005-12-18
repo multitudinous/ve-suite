93bd16babcea26f6ee1de82e54231535c3b783ad
Doug McCorkle <doug.mccorkle@gmail.com>
2005-12-18 02:09:33 +0000

    new changes for lib builds

--- VE_Installer/mk/dzr.vtk.mk
+++ VE_Installer/mk/dzr.vtk.mk
@@ -6,6 +6,9 @@ EXTRA_LIBS+= -L${VTK_BASE_DIR}/lib/vtk -lvtkImaging -lvtkGraphics\
              -lvtkCommon -lvtkHybrid -lvtkIO -lvtkFiltering\
              -lvtkRendering -lvtkParallel
 
+DSO_PLUGIN_DEPS+= -L${VTK_BASE_DIR}/lib/vtk -lvtkImaging -lvtkGraphics\
+                  -lvtkCommon -lvtkHybrid -lvtkIO -lvtkFiltering\
+                  -lvtkRendering -lvtkParallel
 #vtkActorToPf needs to know which version of VTK we are using:
 EXTRA_CXXFLAGS+= -DVTK44 -DVTK_STREAMS_FWD_ONLY
 
