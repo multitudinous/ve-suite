3e8100cf5c6725743e783c42af0db905ca8a0df8
Biv <biv@unknown.nil>
2006-01-10 19:38:43 +0000

    getting rid of the compile of the xsd file...

--- VE_Open/VE_XML/VE_CAD/build.bat
+++ VE_Open/VE_XML/VE_CAD/build.bat
@@ -4,5 +4,5 @@ REM Specify the environment variables
 call setup.bat
 
 REM only change this if your Visual Studio .NET is installed somewhere else
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe" %VE_SUITE_HOME%\VE_Open\VE_XML\VE_CAD\VE_CAD.sln
+"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe" %VE_SUITE_HOME%\VE_Open\VE_XML\VE_CAD\CAD.sln
 cmd
