009c486073e59dd88915c2cc4508dad717628ba3
Biv <biv@unknown.nil>
2006-01-12 21:45:22 +0000

    Removed file/folder

--- VE_Open/VE_XML/VE_CAD/build.bat
+++
@@ -1,8 +0,0 @@
-REM Double click this file to start Visual Studio with the projects loaded
-
-REM Specify the environment variables
-call setup.bat
-
-REM only change this if your Visual Studio .NET is installed somewhere else
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe" %VE_SUITE_HOME%\VE_Open\VE_XML\VE_CAD\CAD.sln
-cmd
