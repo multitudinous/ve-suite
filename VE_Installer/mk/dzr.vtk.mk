e13675533c5010017f40a683620a755867ddf3af
Doug McCorkle <doug.mccorkle@gmail.com>
2006-02-13 23:53:58 +0000

    update to build vesuite against vtk 5.0 on linux

--- VE_Installer/mk/dzr.vtk.mk
+++ VE_Installer/mk/dzr.vtk.mk
@@ -1,14 +1,20 @@
 ################### vtk-specific (vtk4 and higher) Makefile ###################
-EXTRA_INCLUDES+= -I${VTK_BASE_DIR}/include/vtk 
+EXTRA_INCLUDES+= -I${VTK_BASE_DIR}/include/vtk-5.0
 EXTRA_INCLUDES+= -I${VE_SUITE_HOME}
 
-EXTRA_LIBS+= -L${VTK_BASE_DIR}/lib/vtk -lvtkImaging -lvtkGraphics\
+EXTRA_LIBS+= -L${VTK_BASE_DIR}/lib -lvtkImaging -lvtkGraphics\
              -lvtkCommon -lvtkHybrid -lvtkIO -lvtkFiltering\
-             -lvtkRendering -lvtkParallel
+             -lvtkRendering -lvtkParallel -lvtkexpat\
+            -lvtkpng -lvtktiff -lvtksys -lvtkjpeg\
+            -lvtkexoIIc -lvtkftgl -lvtkfreetype -lvtkDICOMParser\
+            -lvtkzlib -lvtkMPEG2Encode -lvtkNetCDF 
 
-DSO_PLUGIN_DEPS+= -L${VTK_BASE_DIR}/lib/vtk -lvtkImaging -lvtkGraphics\
+DSO_PLUGIN_DEPS+= -L${VTK_BASE_DIR}/lib -lvtkImaging -lvtkGraphics\
                   -lvtkCommon -lvtkHybrid -lvtkIO -lvtkFiltering\
-                  -lvtkRendering -lvtkParallel
+                  -lvtkRendering -lvtkParallel -lvtkexpat\
+                  -lvtkpng -lvtktiff -lvtksys -lvtkjpeg\
+                  -lvtkexoIIc -lvtkftgl -lvtkfreetype -lvtkDICOMParser\
+                  -lvtkzlib -lvtkMPEG2Encode -lvtkNetCDF 
 #vtkActorToPf needs to know which version of VTK we are using:
 EXTRA_CXXFLAGS+= -DVTK44 -DVTK_STREAMS_FWD_ONLY
 
