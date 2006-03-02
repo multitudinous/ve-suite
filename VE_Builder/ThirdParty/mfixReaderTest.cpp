6f53ebd5180ffe5d484becd58e55a59df7c3f4d2
Doug McCorkle <doug.mccorkle@gmail.com>
2006-03-02 18:42:07 +0000

    update to show better functionality of mfix translator

--- VE_Builder/ThirdParty/mfixReaderTest.cpp
+++ VE_Builder/ThirdParty/mfixReaderTest.cpp
@@ -19,15 +19,18 @@ int main(int argc, char **argv)
 
 	vtkMFIXReader *reader = vtkMFIXReader::New();
 	reader->SetFileName(argv[1]);
-	reader->SetTimeStep(50);
 	reader->Update();
+	int TimeStepRange[2]; 
+	reader->GetTimeStepRange(TimeStepRange);
+	cout << "Number of Timesteps = " << TimeStepRange[0] << ", "<< TimeStepRange[1] << endl;
+	reader->SetTimeStep(atoi(argv[2]));
 
 	vtkUnstructuredGrid *grid;
 	grid = reader->GetOutput();
 
 	vtkXMLUnstructuredGridWriter *Writer = vtkXMLUnstructuredGridWriter::New();
 	Writer->SetInput(grid);
-	Writer->SetFileName(argv[2]);
+	Writer->SetFileName(argv[3]);
 	Writer->Write();
 
 	return 1;
