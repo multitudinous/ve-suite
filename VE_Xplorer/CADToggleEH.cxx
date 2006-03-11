26309db0f37e4ea73b6d5f97b3e12fa2288e07ab
Biv <biv@unknown.nil>
2006-03-11 00:12:22 +0000

    adding these to the build

---
+++ VE_Xplorer/CADToggleEH.cxx
@@ -0,0 +1,106 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: SceneGraphBuilder.cxx,v $
+ * Date modified: $Date: 2006-01-10 13:45:28 -0600 (Tue, 10 Jan 2006) $
+ * Version:       $Rev: 3477 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#include "VE_Xplorer/CADToggleEH.h"
+#include "VE_Xplorer/cfdModel.h"
+#include "VE_Xplorer/cfdFILE.h"
+#include "VE_SceneGraph/cfdDCS.h"
+#include "VE_SceneGraph/cfdClone.h"
+
+#include "VE_Open/XML/XMLObject.h"
+#include "VE_Open/XML/Command.h"
+#include "VE_Open/XML/DataValuePair.h"
+#include "VE_Open/XML/CAD/CADNode.h"
+#include <iostream>
+using namespace VE_EVENTS;
+using namespace VE_CAD;
+////////////////////////////////////////////////////////////////////////////
+//Constructor                                                             //
+////////////////////////////////////////////////////////////////////////////
+CADToggleEventHandler::CADToggleEventHandler()
+:VE_EVENTS::CADEventHandler()
+{
+}
+///////////////////////////////////////////////////////////////////////////////////////
+CADToggleEventHandler::CADToggleEventHandler(const CADToggleEventHandler& rhs)
+:VE_EVENTS::CADEventHandler(rhs)
+{
+   
+}
+/////////////////////////////////////////////////////
+///Destructor                                      //
+/////////////////////////////////////////////////////
+CADToggleEventHandler::~CADToggleEventHandler()
+{
+}
+///Equal operator
+//////////////////////////////////////////////////////////////////////////////////////////////////
+CADToggleEventHandler& CADToggleEventHandler::operator=(const CADToggleEventHandler& rhs)
+{
+   if(this != &rhs)
+   {
+      VE_EVENTS::CADEventHandler::operator=(rhs);
+   }
+   return *this;
+}
+//////////////////////////////////////////////////////////////////////////
+void CADToggleEventHandler::_operateOnNode(VE_XML::XMLObject* xmlObject)
+{
+   try
+   {
+      VE_XML::Command* command = dynamic_cast<VE_XML::Command*>(xmlObject);
+      VE_XML::DataValuePair* toggleValue = command->GetDataValuePair("Toggle Value");
+      VE_XML::DataValuePair* nodeID = command->GetDataValuePair("Node ID");
+      VE_XML::DataValuePair* nodeType = command->GetDataValuePair("Node Type");
+
+      //This assumes the part/assembly is there already
+      if(nodeType->GetDataString() == std::string("Assembly"))
+      {
+         _activeModel->GetAssembly(nodeID->GetUIntData())->ToggleDisplay(toggleValue->GetDataString());
+      }
+      else if(nodeType->GetDataString() == std::string("Part"))
+      {
+         std::cout<<"---Toggle part---"<<std::endl;
+         std::cout<<"---"<<toggleValue->GetDataString()<<"---"<<std::endl;
+         _activeModel->GetPart(nodeID->GetUIntData())->GetNode()->ToggleDisplay(toggleValue->GetDataString());
+         std::cout<<"---Toggled part---"<<std::endl;
+      }
+      else if(nodeType->GetDataString() == std::string("Clone"))
+      {
+         _activeModel->GetClone(nodeID->GetUIntData())->GetClonedGraph()->ToggleDisplay(toggleValue->GetDataString());
+      }
+   }
+   catch(...)
+   {
+      std::cout<<"Error!!"<<std::endl;
+      std::cout<<"---Invalid node specified to toggle!---"<<std::endl;
+   }
+}
---
+++ VE_Xplorer/CADToggleEH.h
@@ -0,0 +1,70 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: SceneGraphBuilder.cxx,v $
+ * Date modified: $Date: 2006-01-10 13:45:28 -0600 (Tue, 10 Jan 2006) $
+ * Version:       $Rev: 3477 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef VE_CAD_TOGGLE_EVENT_HANDLER_H
+#define VE_CAD_TOGGLE_EVENT_HANDLER_H
+/*!\file CADToggleEventHandler.h
+  CADToggleEventHandler API
+  */
+/*!\class CADToggleEventHandler
+ * Class to toggle CADNode on/off.
+ */
+namespace VE_XML
+{
+   class XMLObject;
+}
+namespace VE_CAD
+{
+   class CADNode;
+}
+#include "VE_Xplorer/CADEventHandler.h"
+namespace VE_EVENTS
+{
+class CADToggleEventHandler: public CADEventHandler{
+public:
+   ///Constructor
+   CADToggleEventHandler();
+
+   ///Copy Constructor
+   CADToggleEventHandler(const CADToggleEventHandler& rhs);
+
+   ///Destructor
+   virtual ~CADToggleEventHandler();
+
+   ///Equal operator
+   CADToggleEventHandler& operator=(const CADToggleEventHandler& rhs);
+protected:
+   ///Toggle a CADNode on/off.
+   ///\param command The Command containing the CADNode to toggle.
+   void _operateOnNode(VE_XML::XMLObject* command);
+};
+}
+#endif// VE_EVENT_HANDLER_H
