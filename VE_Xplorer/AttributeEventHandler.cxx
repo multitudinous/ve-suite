a3bb89761bd13004afbc60c1f33f7cb477bbcb93
Biv <biv@unknown.nil>
2006-04-12 23:41:41 +0000

    adding the attribute eventhandler class files

---
+++ VE_Xplorer/AttributeEventHandler.cxx
@@ -0,0 +1,102 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: SceneGraphBuilder.cxx,v $
+ * Date modified: $Date: 2006-01-10 13:45:28 -0600 (Tue, 10 Jan 2006) $
+ * Version:       $Rev: 3477 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#include "VE_Xplorer/AttributeEventHandler.h"
+#include "VE_Xplorer/cfdModel.h"
+#include "VE_Xplorer/cfdGlobalBase.h"
+#include "VE_Xplorer/cfdModelHandler.h"
+
+#include "VE_Open/XML/XMLObject.h"
+#include <iostream>
+
+using namespace VE_EVENTS;
+using namespace VE_CAD;
+using namespace VE_SceneGraph;
+//////////////////////////////////////////////////////////
+///Constructor                                          //
+//////////////////////////////////////////////////////////
+AttributeEventHandler::AttributeEventHandler()
+:VE_EVENTS::EventHandler()
+{
+   _activeModel = 0;
+}
+////////////////////////////////////////////////////////////
+AttributeEventHandler::AttributeEventHandler(const AttributeEventHandler& rhs)
+:VE_EVENTS::EventHandler()
+{
+   _activeModel = rhs._activeModel;
+}
+////////////////////////////////////
+///Destructor                     //
+////////////////////////////////////
+AttributeEventHandler::~AttributeEventHandler()
+{
+}
+///////////////////////////////////////////////////////////////////////////
+void AttributeEventHandler::SetGlobalBaseObject(VE_Xplorer::cfdGlobalBase* model)
+{
+   try
+   {
+      if(model)
+      {
+         _activeModel = dynamic_cast<VE_Xplorer::cfdModel*>(model);
+      }
+      else
+      {
+         _activeModel = VE_Xplorer::cfdModelHandler::instance()->GetActiveModel();
+      }
+   }
+   catch(...)
+   {
+      _activeModel = 0;
+      std::cout<<"Invalid object passed to CADEventHandler!"<<std::endl;
+   }
+}
+///////////////////////////////////////////////////////
+///Exectute the event                                //
+///////////////////////////////////////////////////////
+void AttributeEventHandler::Execute(VE_XML::XMLObject* veXMLObject)
+{
+   if(_activeModel)
+   {
+      //this is overridden in derived classes
+      _operateOnNode(veXMLObject);
+   }
+}
+///////////////////////////////////////////////////////////////////////
+AttributeEventHandler& AttributeEventHandler::operator=(const AttributeEventHandler& rhs)
+{
+   if(this != &rhs)
+   {
+      _activeModel = rhs._activeModel;
+   }
+   return *this;
+}
---
+++ VE_Xplorer/AttributeEventHandler.h
@@ -0,0 +1,91 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: SceneGraphBuilder.cxx,v $
+ * Date modified: $Date: 2006-01-10 13:45:28 -0600 (Tue, 10 Jan 2006) $
+ * Version:       $Rev: 3477 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef ATTRIBUTE_EVENT_HANDLER_H
+#define ATTRIBUTE_EVENT_HANDLER_H
+/*!\file AttributeEventHandler.h
+  AttributeEventHandler API
+  */
+/*!\class AttributeEventHandler
+ * Base class for CADAttribute event handling.
+ */
+
+#include "VE_Xplorer/EventHandler.h"
+namespace VE_XML
+{
+   class XMLObject;
+}
+namespace VE_CAD
+{
+   class CADAttribute;
+}
+namespace VE_Xplorer
+{
+   class cfdGlobalBase;
+   class cfdModel;
+}
+#include "VE_Installer/include/VEConfig.h"
+
+namespace VE_EVENTS{
+class VE_XPLORER_EXPORTS AttributeEventHandler : public EventHandler{
+public:
+   ///Constructor
+   AttributeEventHandler();
+
+   ///Copy Constructor
+   AttributeEventHandler(const AttributeEventHandler& ceh);
+
+   ///Destructor
+   virtual ~AttributeEventHandler();
+
+   ///Set the cfdModel.
+   ///\param model The cfdModel to execute the Command on\n.
+   ///Default uses the active cfdModel from cfdModelHandler\n
+   ///Otherwise, the cfdModel passed in is used.
+   void SetGlobalBaseObject(VE_Xplorer::cfdGlobalBase* model=0);
+   
+   ///Exectute the event
+   ///\param xmlObject The current xmlObject event.
+   void Execute(VE_XML::XMLObject* command); 
+
+   ///Equal operator
+   AttributeEventHandler& operator=(const AttributeEventHandler& rhs);
+   
+protected:
+   ///The internal operation on the CADNode.
+   ///\param veXMLObject The veXMLObject to execute.
+   virtual void _operateOnNode(VE_XML::XMLObject* veXMLObject) = 0;
+
+   VE_Xplorer::cfdModel* _activeModel;///<The active cfdModel;
+   VE_CAD::CADAttribute* _activeAttribute;///<The CADNode.
+};
+}
+#endif// VE_EVENT_HANDLER_H
