b798ea67c372c0a88b60558e69445bb06434e1b2
Doug McCorkle <doug.mccorkle@gmail.com>
2006-06-28 23:12:30 +0000

    added needed files for new translator

---
+++ VE_Builder/Translator/DataLoader/StarCDTranslator.cxx
@@ -0,0 +1,82 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: filename,v $
+ * Date modified: $Date: 2006-06-24 19:43:51 -0500 (Sat, 24 Jun 2006) $
+ * Version:       $Rev: 4734 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#include "VE_Builder/Translator/DataLoader/StarCDTranslator.h"
+#include "VE_Builder/Translator/DataLoader/starReader.h"
+#include <vtkDataSet.h>
+#include <vtkUnstructuredGrid.h>
+//#include <vtkCellDataToPointData.h>
+//#include <vtkPointData.h>
+
+#include <iostream>
+
+using namespace VE_Builder;
+////////////////////////////////////////
+//Constructors                        //
+////////////////////////////////////////
+StarCDTranslator::StarCDTranslator()
+{
+
+   SetTranslateCallback(&starToVTK);
+   SetPreTranslateCallback(&_cmdParser);
+}
+/////////////////////////////////////////
+StarCDTranslator::~StarCDTranslator()
+{
+
+}
+//////////////////////////////////////////////////////////////////////////
+void StarCDTranslator::StarCDPreTranslateCbk::Preprocess(int argc,char** argv,
+                                               VE_Builder::cfdTranslatorToVTK* toVTK)
+{
+   PreTranslateCallback::Preprocess( argc, argv, toVTK );
+}
+////////////////////////////////////////////////////////////////////////////////
+void StarCDTranslator::StarCDTranslateCbk::Translate(vtkDataSet*& outputDataset,
+		                                     VE_Builder::cfdTranslatorToVTK* toVTK)
+{
+   VE_Builder::StarCDTranslator* starCDToVTK =
+              dynamic_cast<VE_Builder::StarCDTranslator*>(toVTK);
+   if ( starCDToVTK )
+   {
+      starReader* star = new starReader( starCDToVTK->GetFile(0).c_str() );
+      star->ReadParameterFile();
+      
+      if ( !outputDataset )
+      {
+         outputDataset = vtkUnstructuredGrid::New();
+      }
+      outputDataset->ShallowCopy( star->GetUnsGrid() );
+      delete star;
+      outputDataset->Update();
+   }
+}
+ 
---
+++ VE_Builder/Translator/DataLoader/StarCDTranslator.h
@@ -0,0 +1,71 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2006-06-24 19:43:51 -0500 (Sat, 24 Jun 2006) $
+ * Version:       $Rev: 4734 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef _STARCD_TRANSLATOR_H_
+#define _STARCD_TRANSLATOR_H_
+
+
+#include "VE_Builder/Translator/cfdTranslatorToVTK/cfdTranslatorToVTK.h"
+
+namespace VE_Builder
+{
+class VE_USER_BUILDER_EXPORTS StarCDTranslator : public VE_Builder::cfdTranslatorToVTK
+{
+public:
+   StarCDTranslator();
+   virtual ~StarCDTranslator();
+ 
+   class VE_USER_BUILDER_EXPORTS StarCDTranslateCbk: public VE_Builder::cfdTranslatorToVTK::TranslateCallback
+   {
+   public:
+      StarCDTranslateCbk(){};
+      virtual ~StarCDTranslateCbk(){};
+      //////////////////////////////////////////////////
+      //ouputDataset should be populated              //
+      //appropriately by the translate callback.      //
+      //////////////////////////////////////////////////
+      virtual void Translate(vtkDataSet*& outputDataset,
+		                     cfdTranslatorToVTK* toVTK);
+   protected:
+   };
+   class VE_USER_BUILDER_EXPORTS StarCDPreTranslateCbk: public VE_Builder::cfdTranslatorToVTK::PreTranslateCallback{
+   public:
+      StarCDPreTranslateCbk(){};
+      virtual ~StarCDPreTranslateCbk(){};
+      void Preprocess(int argc,char** argv,VE_Builder::cfdTranslatorToVTK* toVTK);
+   protected:
+   };
+protected:
+   StarCDPreTranslateCbk _cmdParser;
+   StarCDTranslateCbk starToVTK;
+};
+
+}
+#endif//_STARCD_TRANSLATOR_H_
