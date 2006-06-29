f8773092aab97ad7a9cb05e39fae47f3528d248d
Biv <biv@unknown.nil>
2006-06-29 20:20:25 +0000

    updated project files for new output directories

--- VE_Installer/setup.bat
+++ VE_Installer/setup.bat
@@ -1,18 +1,22 @@
 REM Edit this file to reflect your local environment for VE_Suite
 
-set VE_SUITE_HOME=C:\TSVEG\VE_Suite
-set VJ_BASE_DIR=C:\vrjuggler-2.0.0-win32-vc71
-set VJ_DEPS_DIR=C:\vrjuggler-2.0.0-win32-vc71-deps
-set VTK_HOME=C:\VTK_4.4_WinXP
-set WX_HOME=C:\wxWidgets-2.6.1
+set VE_SUITE_HOME=C:\devEnv\VE_Suite_1.0
+set VE_INSTALL_DIR=C:\devEnv\VE_Suite_1.0
+
+set VJ_BASE_DIR=C:\devEnv\vrjuggler-2.0.1-win32-vc71
+set VJ_DEPS_DIR=C:\devEnv\vrjuggler-2.0.1-win32-vc71-deps
+set VTK_HOME=C:\devEnv\VES.1.0-Deps\vtk-5.0.0_Install
+set WX_HOME=C:\devEnv\VES.1.0-deps\wxWidgets-2.6.3
+set WX_ROOT=C:\devEnv\VES.1.0-Deps\wxWidgets-2.6.3
 set JAVA_HOME=C:\j2sdk1.4.2_06
-set ACE_ROOT=C:\ACE_wrappers
+set ACE_ROOT=C:\devEnv\VES.1.0-Deps\ACE_wrappers
 set TAO_ROOT=%ACE_ROOT%\TAO
-set XERCESCROOT=C:\xerces-c-src_2_6_0
-set WX_ROOT=C:\wxWidgets-2.6.1
-set OSGHOME=C:\OSG_OP_OT-0.9.9\OpenSceneGraph
+set XERCESCROOT=C:\devEnv\VES.1.0-Deps\xerces-c-src_2_7_0
+set OSGHOME=C:\devEnv\VES.1.0-Deps\OSG_OP_OT-1.0\OpenSceneGraph
+set DEMETER_HOME=C:\devEnv\demeter-3.21
 set TAO_MACHINE=localhost
-set TAO_PORT=1239
+REM set TAO_MACHINE=costello.vrac.iastate.edu
+set TAO_PORT=1237
 set SNX_BASE_DIR=%VJ_BASE_DIR%
 
 REM NOTE: On Windows the juggler location must point to a Windows
@@ -31,7 +35,7 @@ REM set PHSHAREDSIZE=534773700
 
 REM juggler debug output level
 set VPR_DEBUG_ENABLE=1
-set VPR_DEBUG_NFY_LEVEL=1
+set VPR_DEBUG_NFY_LEVEL=2
 set NO_PERF_PLUGIN=TRUE
 set NO_RTRC_PLUGIN=TRUE
 set PFNFYLEVEL=0
@@ -50,6 +54,9 @@ set PYTHONPATH=%VJ_DEPS_DIR%\lib\python
 
 set Path=%Path%;%VJ_DEPS_DIR%\bin;%VJ_DEPS_DIR%\lib;%VJ_BASE_DIR%\lib
 set Path=%Path%;%VE_SUITE_HOME%\bin;%VTK_HOME%\bin;%WX_HOME%\lib\vc_dll
+set Path=%Path%;%VE_SUITE_HOME%\bin\Plugins\UI\win32;
+set Path=%Path%;%VE_SUITE_HOME%\bin\Plugins\GE\win32;
+set Path=%Path%;%ACE_ROOT%\bin;%ACE_ROOT%\lib;%XERCESCROOT%\Build\Win32\VC7.1\Debug
 set Path=%Path%;%VE_SUITE_HOME%\bin\win32
-set Path=%Path%;%ACE_ROOT%\bin;%XERCESCROOT%\Build\Win32\VC7\Debug
 set Path=%Path%;%OSGHOME%\bin;%VE_SUITE_HOME%\lib\win32
+set Path=%Path%;%DEMETER_HOME%\bin;
