55271e1a37439f9a8f344283f07af73e0c7f14f0
Doug McCorkle <doug.mccorkle@gmail.com>
2006-06-30 18:04:14 +0000

    added some more 64 bit compile checking fixes

--- VE_Installer/mk/dzr.boost.mk
+++ VE_Installer/mk/dzr.boost.mk
@@ -8,11 +8,11 @@ EXTRA_LIBS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33_1
 DSO_PLUGIN_DEPS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33_1
 else
    ifeq ($(shell uname -m), ia64)
-   EXTRA_LIBS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33_1
-   DSO_PLUGIN_DEPS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33_1
+   EXTRA_LIBS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33
+   DSO_PLUGIN_DEPS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33
    else
-   EXTRA_LIBS+= -L$(VJ_DEPS_DIR)/lib -lboost_filesystem-gcc-mt-1_33_1
-   DSO_PLUGIN_DEPS+= -L$(VJ_DEPS_DIR)/lib -lboost_filesystem-gcc-mt-1_33_1
+   EXTRA_LIBS+= -L$(VJ_DEPS_DIR)/lib -lboost_filesystem-gcc-mt-1_33
+   DSO_PLUGIN_DEPS+= -L$(VJ_DEPS_DIR)/lib -lboost_filesystem-gcc-mt-1_33
    endif
 endif
 endif
