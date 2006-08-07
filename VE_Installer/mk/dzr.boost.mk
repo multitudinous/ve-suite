fd93119a1c940a3d4a8bee2cd215676acc6804d8
Doug McCorkle <doug.mccorkle@gmail.com>
2006-08-07 14:29:52 +0000

    forgot to add a new file for apr

--- VE_Installer/mk/dzr.boost.mk
+++ VE_Installer/mk/dzr.boost.mk
@@ -4,8 +4,8 @@ EXTRA_LIBS+= -L$(VJ_DEPS_DIR)/lib -lboost_filesystem-1_33_1
 DSO_PLUGIN_DEPS+= -L$(VJ_DEPS_DIR)/lib -lboost_filesystem-1_33_1
 else
 ifeq ($(shell uname -m), x86_64)
-EXTRA_LIBS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33_1
-DSO_PLUGIN_DEPS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33_1
+EXTRA_LIBS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33
+DSO_PLUGIN_DEPS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33
 else
    ifeq ($(shell uname -m), ia64)
    EXTRA_LIBS+= -L$(VJ_DEPS_DIR)/lib64 -lboost_filesystem-gcc-mt-1_33
