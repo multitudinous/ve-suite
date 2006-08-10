a5352b0415af45b9e99cc2f85053c897f725c1ae
Doug McCorkle <doug.mccorkle@gmail.com>
2006-08-10 16:40:02 +0000

    corrected linux build problems with new apr util in veopen

--- VE_Installer/mk/dzr.apr.mk
+++ VE_Installer/mk/dzr.apr.mk
@@ -1,12 +1,12 @@
 ifeq ($(CFDHOSTTYPE), Darwin)
    EXTRA_INCLUDES+= $(shell apu-1-config --includes) $(shell apr-1-config --includes)
    EXTRA_CXXFLAGS+=  $(shell apr-1-config --cppflags) 
-   EXTRA_LDFLAGSS+= $(shell apu-1-config --link-ld --libs)
+   EXTRA_LIBS+= $(shell apu-1-config --link-ld --libs) $(shell apr-1-config --link-ld --libs)
    DSO_PLUGIN_DEPS+= $(shell apu-1-config --link-ld --libs)
 else
    EXTRA_INCLUDES+= $(shell apu-config --includes) $(shell apr-config --includes)
    EXTRA_CXXFLAGS+=  $(shell apr-config --cppflags) 
-   EXTRA_LDFLAGSS+= $(shell apu-config --link-ld --libs)
+   EXTRA_LIBS+= $(shell apu-config --link-ld --libs) $(shell apr-config --link-ld --libs)
    DSO_PLUGIN_DEPS+= $(shell apu-config --link-ld --libs)
 endif
 
