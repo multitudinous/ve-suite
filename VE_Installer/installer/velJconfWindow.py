83eee4ead3145c216d74f7cae5b5364dc887958f
Mike Lem <mikelem@unknown.nil>
2006-08-15 22:54:15 +0000

    Launcher: More class files added.

--- VE_Installer/installer/velJconfWindow.py
+++ VE_Installer/installer/velJconfWindow.py
@@ -1,82 +1,7 @@
 import wx
 import os
 from velBase import *
-
-class JconfDict(VelDict):
-    """Stores a list of Jconf pairs in this setup:
-    {name: path, name: path..}
-    under the variable self.dictionary.
-
-    Functions:
-        __init__()
-        Add(name, path)
-        Rename(oldName, newName)
-        Delete(name)
-        UniqueName(name)
-        GetPath(name)
-        Length() / __len__
-        GetNames()
-        WriteConfig()
-    """
-    def __init__(self):
-        """Creates a list of .jconf names/paths from the Launcher's Config."""
-        VelDict.__init__(self)
-
-    def ReadEntries(self):
-        entries = {}
-        config = wx.Config.Get()
-        config.SetPath(JCONF_CONFIG)
-        step = config.GetFirstEntry()
-        while (step[0]):
-            entries[step[1]] = config.Read(step[1])
-            step = config.GetNextEntry(step[2])
-        config.SetPath('..')
-        return entries
-
-    def Rename(self, oldName, newName):
-        """Renames the entry at oldName to newName.
-
-        Returns newName, modified until it's unique on the list."""
-        ##Don't do anything if newName == pos's old name
-        if newName == oldName:
-            return newName
-        ##Change name if it matches another one on the list.
-        path = self.dictionary[oldName]
-        self.Delete(oldName)
-        finalName = self.Add(newName, path)
-        ##Return true if name had to be changed.
-        return finalName
-
-    def UniqueName(self, name):
-        """Replaces name with a unique name in the list."""
-        ##Add a numeric suffix to name if it matches a name
-        ##already in the list.
-        suffix = ""
-        maxCount = 0
-        curNames = self.GetNames()
-        while curNames.count(name + str(suffix)) > maxCount:
-            if suffix == "":
-                suffix = '1'
-            else:
-                suffix = str(int(suffix) + 1)
-        return name + str(suffix)
-
-    def GetPath(self, name):
-        """Returns the path of name's entry."""
-        if name in self.dictionary:
-            return self.dictionary[name]
-        else:
-            return "None"
-
-    def WriteConfig(self):
-        """Writes the entire list to config."""
-        config = wx.Config.Get()
-        config.SetPath(JCONF_CONFIG)
-        for name in self.dictionary:
-            config.Write(name, self.dictionary[name])
-        config.SetPath('..')
-
-
+from velJconfDict import *
 
 class JconfWindow(wx.Dialog):
     """A window for editing a list of Jconf files.
@@ -301,5 +226,3 @@ class JconfWindow(wx.Dialog):
         self.GetParent().UpdateChJconf(self.confList.GetStringSelection())
         self.Hide()
         self.Destroy()
-
-
