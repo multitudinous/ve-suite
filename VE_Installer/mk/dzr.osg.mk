687272672c743fe151bb81c831edbd9845e2c165
Doug McCorkle <doug.mccorkle@gmail.com>
2006-08-18 00:52:01 +0000

    added producer stuff to the mk file

--- VE_Installer/mk/dzr.osg.mk
+++ VE_Installer/mk/dzr.osg.mk
@@ -10,18 +10,17 @@ endif
 
 ifeq (${CFDPLATFORM},x86_64)
    EXTRA_LIBS+= -L$(OSG_HOME)/lib -L$(OSG_HOME)/lib64 -losg -losgDB -losgGA -losgUtil \
-            -lOpenThreads -losgFX -losgText -losgSim -losgProducer
+            -lOpenThreads -losgFX -losgText -losgSim -losgProducer -lProducer
    DSO_PLUGIN_DEPS+= -L$(OSG_HOME)/lib -L$(OSG_HOME)/lib64 -losg -losgDB -losgGA -losgUtil \
-            -lOpenThreads -losgFX -losgText -losgSim -losgProducer
+            -lOpenThreads -losgFX -losgText -losgSim -losgProducer -lProducer
 else
    ifeq (${CFDUNAME},IRIX64)
       EXTRA_LIBS+= -L$(OSG_HOME)/lib32 -losg -losgDB -losgGA -losgUtil \
-            -lOpenThreads -losgFX -losgText -losgSim -losgProducer
+            -lOpenThreads -losgFX -losgText -losgSim -losgProducer -lProducer
    else
       EXTRA_LIBS+= -L$(OSG_HOME)/lib -losg -losgDB -losgGA -losgUtil \
-            -lOpenThreads -losgText -losgSim -losgProducer
+            -lOpenThreads -losgText -losgSim -losgProducer -lProducer
       DSO_PLUGIN_DEPS+= -L$(OSG_HOME)/lib -losg -losgDB -losgGA -losgUtil \
-            -lOpenThreads -losgText -losgSim -losgProducer
+            -lOpenThreads -losgText -losgSim -losgProducer -lProducer
    endif
-
 endif
