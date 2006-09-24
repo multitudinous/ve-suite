9aa9a65a469b6f42dfabd9074fd7dfce1921e6de
Mike Lem <mikelem@unknown.nil>
2006-09-24 21:17:42 +0000

    Launcher: Minor fix.

---
+++ VE_Installer/installer/structure_launcher/velArguments.py
@@ -0,0 +1,15 @@
+"""Handles arguments sent to velauncher."""
+from velCoveredConfig import *
+
+def Interpret(state, arguments):
+    """Grab 1st argument. Assign it as either a VES or script file."""
+    if not arguments:
+        return
+    arg = arguments[0]
+    if arg[-4:] == '.ves':
+        state.VesArgument(arg)
+        print "VES Argument: %s" %arg ##TESTER
+    else:
+        state.ScriptArgument(arg)
+        print "Script Argument: %s" %arg ##TESTER
+    return
