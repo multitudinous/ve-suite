1ba84f099d22e1af5897b9c4595e4ae875ce46e2
Terry Jordan <Terry.Jordan@netl.doe.gov>
2006-12-13 21:23:06 +0000

    new params dlg

--- VE_Conductor/GUIPlugin/ParamsDlg.cpp
+++ VE_Conductor/GUIPlugin/ParamsDlg.cpp
@@ -2,7 +2,8 @@
 
 BEGIN_EVENT_TABLE(ParamsDlg,wxDialog)
 	EVT_CLOSE(ParamsDlg::OnClose)
-	EVT_CHOICE(ID_WXCHOICE1,ParamsDlg::WxChoice1Selected)
+	EVT_BUTTON(ID_SETBUTTON,ParamsDlg::SetButtonClick)
+	EVT_CHOICE(ID_PARAMCHOICE,ParamsDlg::ParamChoiceSelected)
 END_EVENT_TABLE()
 
 ParamsDlg::ParamsDlg(wxWindow *parent, wxWindowID id, const wxString &title, const wxPoint &position, const wxSize& size, long style)
@@ -17,50 +18,206 @@ ParamsDlg::~ParamsDlg()
 
 void ParamsDlg::CreateGUIControls()
 {
-	WxPanel1 = new wxPanel(this, ID_WXPANEL1, wxPoint(0,0), wxSize(252,707));
-	WxGrid1 = new wxGrid(WxPanel1, ID_WXGRID1, wxPoint(0,29), wxSize(250,605));
-	WxGrid1->SetDefaultColSize(100);
-	WxGrid1->SetDefaultRowSize(25);
-	WxGrid1->SetRowLabelSize(20);
-	WxGrid1->SetColLabelSize(0);
-	WxGrid1->CreateGrid(23,2,wxGrid::wxGridSelectCells);
-	wxArrayString arrayStringFor_WxChoice1;
-	WxChoice1 = new wxChoice(WxPanel1, ID_WXCHOICE1, wxPoint(48,5), wxSize(145,21), arrayStringFor_WxChoice1, 0, wxDefaultValidator, wxT("WxChoice1"));
-	WxChoice1->SetSelection(-1);
-	SetTitle(wxT("Project2"));
+	SetTitle(wxT("ParamsDialog"));
 	SetIcon(wxNullIcon);
-	SetSize(8,8,254,709);
+	SetSize(8,8,570,426);
 	Center();
+	
+
+	SetButton = new wxButton(this, ID_SETBUTTON, wxT("Set"), wxPoint(150,105), wxSize(40,25), 0, wxDefaultValidator, wxT("SetButton"));
+
+	WxMemo3 = new wxTextCtrl(this, ID_WXMEMO3, wxT(""), wxPoint(394,108), wxSize(158,71), wxTE_MULTILINE, wxDefaultValidator, wxT("WxMemo3"));
+	WxMemo3->SetMaxLength(0);
+	WxMemo3->Enable(false);
+	WxMemo3->SetFocus();
+	WxMemo3->SetInsertionPointEnd();
+
+	PromptMemo = new wxTextCtrl(this, ID_PROMPTMEMO, wxT(""), wxPoint(269,286), wxSize(103,83), wxVSCROLL | wxTE_MULTILINE, wxDefaultValidator, wxT("PromptMemo"));
+	PromptMemo->SetMaxLength(0);
+	PromptMemo->Enable(false);
+	PromptMemo->SetFocus();
+	PromptMemo->SetInsertionPointEnd();
+
+	OptionsMemo = new wxTextCtrl(this, ID_OPTIONSMEMO, wxT(""), wxPoint(67,250), wxSize(121,118), wxVSCROLL | wxTE_MULTILINE, wxDefaultValidator, wxT("OptionsMemo"));
+	OptionsMemo->SetMaxLength(0);
+	OptionsMemo->Enable(false);
+	OptionsMemo->SetFocus();
+	OptionsMemo->SetInsertionPointEnd();
+
+	ChildrenLabel = new wxStaticText(this, ID_CHILDRENLABEL, wxT("Children"), wxPoint(392,349), wxDefaultSize, 0, wxT("ChildrenLabel"));
+
+	HasLabel = new wxStaticText(this, ID_HASLABEL, wxT("Has"), wxPoint(392,337), wxDefaultSize, 0, wxT("HasLabel"));
+
+	HasChildrenEdit = new wxTextCtrl(this, ID_HASCHILDRENEDIT, wxT(""), wxPoint(436,340), wxSize(115,21), 0, wxDefaultValidator, wxT("HasChildrenEdit"));
+	HasChildrenEdit->Enable(false);
+	HasChildrenEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	OABox = new wxStaticBox(this, ID_OABOX, wxT("Other Attributes"), wxPoint(389,320), wxSize(171,54));
+	OABox->SetForegroundColour(wxColour(*wxBLACK));
+
+	PortTypeEdit = new wxTextCtrl(this, ID_PORTTYPEEDIT, wxT(""), wxPoint(451,286), wxSize(104,21), 0, wxDefaultValidator, wxT("PortTypeEdit"));
+	PortTypeEdit->Enable(false);
+	PortTypeEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	PortTypeLabel = new wxStaticText(this, ID_PORTTYPELABEL, wxT("Port Type"), wxPoint(395,289), wxDefaultSize, 0, wxT("PortTypeLabel"));
+
+	MultiportLabel = new wxStaticText(this, ID_MULTIPORTLABEL, wxT("Multiport"), wxPoint(395,262), wxDefaultSize, 0, wxT("MultiportLabel"));
+
+	MultiportEdit = new wxTextCtrl(this, ID_MULTIPORTEDIT, wxT(""), wxPoint(451,260), wxSize(104,21), 0, wxDefaultValidator, wxT("MultiportEdit"));
+	MultiportEdit->Enable(false);
+	MultiportEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	GenderEdit = new wxTextCtrl(this, ID_GENDEREDIT, wxT(""), wxPoint(451,234), wxSize(104,21), 0, wxDefaultValidator, wxT("GenderEdit"));
+	GenderEdit->Enable(false);
+	GenderEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	GenderLabel = new wxStaticText(this, ID_GENDERLABEL, wxT("Gender"), wxPoint(395,237), wxDefaultSize, 0, wxT("GenderLabel"));
+
+	InOrOutLabel = new wxStaticText(this, ID_INOROUTLABEL, wxT("In or Out"), wxPoint(395,212), wxDefaultSize, 0, wxT("InOrOutLabel"));
+
+	InOrOutEdit = new wxTextCtrl(this, ID_INOROUTEDIT, wxT(""), wxPoint(451,208), wxSize(104,21), 0, wxDefaultValidator, wxT("InOrOutEdit"));
+	InOrOutEdit->Enable(false);
+	InOrOutEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	FCPBox = new wxStaticBox(this, ID_FCPBOX, wxT("Flowsheet Connectivity Port"), wxPoint(389,186), wxSize(171,130));
+	FCPBox->SetForegroundColour(wxColour(*wxBLACK));
+
+	TypeLabel = new wxStaticText(this, ID_TYPELABEL, wxT("Type"), wxPoint(209,98), wxDefaultSize, 0, wxT("TypeLabel"));
+
+	WxEdit16 = new wxTextCtrl(this, ID_WXEDIT16, wxT(""), wxPoint(394,85), wxSize(158,21), 0, wxDefaultValidator, wxT("WxEdit16"));
+	WxEdit16->Enable(false);
+	WxEdit16->SetBackgroundColour(wxColour(*wxWHITE));
+
+	CSBox = new wxStaticBox(this, ID_CSBOX, wxT("Completion Status"), wxPoint(388,67), wxSize(172,117));
+	CSBox->SetForegroundColour(wxColour(*wxBLACK));
+
+	DValueLabel = new wxStaticText(this, ID_DVALUELABEL, wxT("Value"), wxPoint(213,272), wxDefaultSize, 0, wxT("DValueLabel"));
+
+	ListLabel = new wxStaticText(this, ID_LISTLABEL, wxT("List"), wxPoint(11,228), wxDefaultSize, 0, wxT("ListLabel"));
+
+	MeasureLabel = new wxStaticText(this, ID_MEASURELABEL, wxT("Measure"), wxPoint(11,173), wxDefaultSize, 0, wxT("MeasureLabel"));
+
+	QuantityLabel = new wxStaticText(this, ID_QUANTITYLABEL, wxT("Quantity"), wxPoint(11,142), wxDefaultSize, 0, wxT("QuantityLabel"));
+
+	PromptLabel = new wxStaticText(this, ID_PROMPT, wxT("Prompt"), wxPoint(212,289), wxDefaultSize, 0, wxT("PromptLabel"));
+
+	DefaultValueEdit = new wxTextCtrl(this, ID_DEFAULTVALUEEDIT, wxT(""), wxPoint(269,261), wxSize(103,21), 0, wxDefaultValidator, wxT("DefaultValueEdit"));
+	DefaultValueEdit->Enable(false);
+
+	DefaultLabel = new wxStaticText(this, ID_DEFAULTLABEL, wxT("Default"), wxPoint(213,258), wxDefaultSize, 0, wxT("DefaultLabel"));
+
+	LowerLimitEdit = new wxTextCtrl(this, ID_LOWERLIMITEDIT, wxT(""), wxPoint(269,234), wxSize(103,21), 0, wxDefaultValidator, wxT("LowerLimitEdit"));
+	LowerLimitEdit->Enable(false);
+
+	LowerLimitLabel = new wxStaticText(this, ID_LOWERLIMITLABEL, wxT("Lower Limit"), wxPoint(212,237), wxDefaultSize, 0, wxT("LowerLimitLabel"));
+
+	UpperLimitLabel = new wxStaticText(this, ID_UPPERLIMITLABEL, wxT("Upper Limit"), wxPoint(212,210), wxDefaultSize, 0, wxT("UpperLimitLabel"));
+
+	UpperLimitEdit = new wxTextCtrl(this, ID_UPPERLIMITEDIT, wxT(""), wxPoint(269,208), wxSize(103,21), 0, wxDefaultValidator, wxT("UpperLimitEdit"));
+	UpperLimitEdit->Enable(false);
+
+	EnterableEdit = new wxTextCtrl(this, ID_ENTERABLEEDIT, wxT(""), wxPoint(269,182), wxSize(103,21), 0, wxDefaultValidator, wxT("EnterableEdit"));
+	EnterableEdit->Enable(false);
+
+	EnterableLabel = new wxStaticText(this, ID_ENTERABLELABEL, wxT("Enterable"), wxPoint(212,185), wxDefaultSize, 0, wxT("EnterableLabel"));
+
+	OutputLabel = new wxStaticText(this, ID_OUTPUTLABEL, wxT("Output"), wxPoint(212,159), wxDefaultSize, 0, wxT("OutputLabel"));
+
+	OutputEdit = new wxTextCtrl(this, ID_OUTPUTEDIT, wxT(""), wxPoint(269,156), wxSize(103,21), 0, wxDefaultValidator, wxT("OutputEdit"));
+	OutputEdit->Enable(false);
+
+	AVNBox = new wxStaticBox(this, ID_AVNBOX, wxT("Attributes for Variable Nodes"), wxPoint(207,134), wxSize(176,241));
+	AVNBox->SetForegroundColour(wxColour(*wxBLACK));
+
+	RecordLabel = new wxStaticText(this, ID_RECORDLABEL, wxT("Record"), wxPoint(209,86), wxDefaultSize, 0, wxT("RecordLabel"));
+
+	RecordTypeEdit = new wxTextCtrl(this, ID_RECORDTYPEEDIT, wxT(""), wxPoint(248,89), wxSize(118,21), 0, wxDefaultValidator, wxT("RecordTypeEdit"));
+	RecordTypeEdit->Enable(false);
+	RecordTypeEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	MARBox = new wxStaticBox(this, ID_MARBOX, wxT("Meta-data Attributes for Records"), wxPoint(203,70), wxSize(177,52));
+	MARBox->SetForegroundColour(wxColour(*wxBLACK));
+
+	OptionsLabel = new wxStaticText(this, ID_OPTIONSLABEL, wxT("Options"), wxPoint(11,250), wxDefaultSize, 0, wxT("OptionsLabel"));
+
+	OptionLabel = new wxStaticText(this, ID_OPTIONLABEL, wxT("Option"), wxPoint(11,216), wxDefaultSize, 0, wxT("OptionLabel"));
+
+	OptionListEdit = new wxTextCtrl(this, ID_OPTIONLISTEDIT, wxT(""), wxPoint(57,218), wxSize(132,21), 0, wxDefaultValidator, wxT("OptionListEdit"));
+	OptionListEdit->Enable(false);
+	OptionListEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	BasisLabel = new wxStaticText(this, ID_BASISLABEL, wxT("Basis"), wxPoint(11,191), wxDefaultSize, 0, wxT("BasisLabel"));
+
+	BasisEdit = new wxTextCtrl(this, ID_BASISEDIT, wxT(""), wxPoint(57,190), wxSize(132,21), 0, wxDefaultValidator, wxT("BasisEdit"));
+	BasisEdit->Enable(false);
+	BasisEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	UnitLabel = new wxStaticText(this, ID_UNITLABEL, wxT("Unit of"), wxPoint(11,159), wxDefaultSize, 0, wxT("UnitLabel"));
+
+	UnitEdit = new wxTextCtrl(this, ID_UNITEDIT, wxT(""), wxPoint(57,163), wxSize(132,21), 0, wxDefaultValidator, wxT("UnitEdit"));
+	UnitEdit->Enable(false);
+	UnitEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	PhysicalLabel = new wxStaticText(this, ID_PHYSICALLABEL, wxT("Physical"), wxPoint(11,129), wxDefaultSize, 0, wxT("PhysicalLabel"));
+
+	Quantity = new wxTextCtrl(this, ID_QUANTITY, wxT(""), wxPoint(57,133), wxSize(132,21), 0, wxDefaultValidator, wxT("Quantity"));
+	Quantity->Enable(false);
+	Quantity->SetBackgroundColour(wxColour(*wxWHITE));
+
+	ValueEdit = new wxTextCtrl(this, ID_VALUEEDIT, wxT(""), wxPoint(57,107), wxSize(91,21), 0, wxDefaultValidator, wxT("ValueEdit"));
+
+	ValueLabel = new wxStaticText(this, ID_VALUELABEL, wxT("Value"), wxPoint(11,111), wxDefaultSize, 0, wxT("ValueLabel"));
+
+	VARBox = new wxStaticBox(this, ID_, wxT("Value Related Attributes"), wxPoint(2,88), wxSize(199,287));
+	VARBox->SetForegroundColour(wxColour(*wxBLACK));
+
+	DimensionLabel = new wxStaticText(this, ID_DIMENSIONLABEL, wxT("Dimension"), wxPoint(12,66), wxDefaultSize, 0, wxT("DimensionLabel"));
+
+	DimensionEdit = new wxTextCtrl(this, ID_DIMENSIONEDIT, wxT(""), wxPoint(65,63), wxSize(121,21), 0, wxDefaultValidator, wxT("DimensionEdit"));
+	DimensionEdit->Enable(false);
+	DimensionEdit->SetBackgroundColour(wxColour(*wxWHITE));
+
+	wxArrayString arrayStringFor_ParamChoice;
+	ParamChoice = new wxChoice(this, ID_PARAMCHOICE, wxPoint(166,8), wxSize(211,21), arrayStringFor_ParamChoice, 0, wxDefaultValidator, wxT("ParamChoice"));
+	ParamChoice->SetSelection(-1);
+
+	NodePathLabel = new wxStaticText(this, ID_NODEPATHLABEL, wxT("Path to Node"), wxPoint(9,37), wxDefaultSize, 0, wxT("NodePathLabel"));
+
+	NodePath = new wxTextCtrl(this, ID_NODEPATH, wxT(""), wxPoint(81,36), wxSize(477,21), 0, wxDefaultValidator, wxT("NodePath"));
+	NodePath->Enable(false);
+	NodePath->SetBackgroundColour(wxColour(*wxWHITE));
+
+	ParameterLabel = new wxStaticText(this, ID_PARAMETERLABEL, wxT("Parameter"), wxPoint(100,10), wxDefaultSize, 0, wxT("ParameterLabel"));
+	ParameterLabel->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxBOLD, FALSE));
+	////GUI Items Creation End
 }
 
 void ParamsDlg::OnClose(wxCloseEvent& /*event*/)
 {
 	Destroy();
 }
-void ParamsDlg::AddToList(const char * element)
+
+
+//SetValueClick
+void ParamsDlg::SetValueClick(wxCommandEvent& event)
 {
-	WxChoice1->Append(wxT(element));
 }
 
-void ParamsDlg::AddResults(std::string dataName, std::vector< std::string > paramName, std::vector< std::string > paramData)
-{
-	ParamNames[dataName] = paramName;
-	ParamValues[dataName] = paramData;
-    //WxChoice1->SetSelection(0);
-}
-
+//WxChoice1Selected
 void ParamsDlg::WxChoice1Selected(wxCommandEvent& event )
 {
-	WxGrid1->ClearGrid();
-	std::string name(WxChoice1->GetString(WxChoice1->GetSelection()).c_str());
-	for(int i = 0; i < (int) ParamNames[name].size(); i++)
-	{
-		WxGrid1->SetCellValue(i, 0, wxT(ParamNames[name][i].c_str()));
-		WxGrid1->SetCellValue(i, 1, wxT(ParamValues[name][i].c_str()));
-	}
 }
 
-void ParamsDlg::SetDefaultSelection()
+//ParamChoiceSelected
+void ParamsDlg::ParamChoiceSelected(wxCommandEvent& event )
 {
-    //WxChoice1->SetSelection(0);
+}
+
+//SetButtonClick
+void ParamsDlg::SetButtonClick(wxCommandEvent& event)
+{
+}
+void ParamsDlg::AppendList(const char * input)
+{
+	ParamChoice->Append(wxT(input));
 }
--- VE_Conductor/GUIPlugin/ParamsDlg.h
+++ VE_Conductor/GUIPlugin/ParamsDlg.h
@@ -3,17 +3,15 @@
 
 #include <wx/wx.h>
 #include <wx/dialog.h>
+#include <wx/button.h>
+#include <wx/statbox.h>
 #include <wx/choice.h>
-#include <wx/grid.h>
-#include <wx/panel.h>
-#include <vector>
-#include <map>
-#include <iostream>
-#include <sstream>
+#include <wx/textctrl.h>
+#include <wx/stattext.h>
 #include "VE_Installer/include/VEConfig.h"
 
 #undef ParamsDlg_STYLE
-#define ParamsDlg_STYLE wxCAPTION | wxSYSTEM_MENU | wxDIALOG_NO_PARENT | wxMINIMIZE_BOX | wxCLOSE_BOX
+#define ParamsDlg_STYLE wxCAPTION | wxSYSTEM_MENU | wxSTAY_ON_TOP | wxDIALOG_NO_PARENT | wxMINIMIZE_BOX | wxCLOSE_BOX
 
 class wxWindow;
 class wxDialog;
@@ -28,29 +26,137 @@ class VE_GUIPLUGINS_EXPORTS ParamsDlg : public wxDialog
 {
 	private:
 		DECLARE_EVENT_TABLE();
-	public:
-		ParamsDlg(wxWindow *parent, wxWindowID id = 1, const wxString &title = wxT("Params"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = ParamsDlg_STYLE);
-		virtual ~ParamsDlg();
-		void WxChoice1Selected(wxCommandEvent& event );
-		void AddToList(const char *);
-		void SetDefaultSelection();
-		void AddResults(std::string, std::vector< std::string >, std::vector< std::string >);
-		wxChoice *WxChoice1;
-		wxGrid *WxGrid1;
-		wxPanel *WxPanel1;
-		std::map<std::string, std::vector< std::string > > ParamNames;
-		std::map<std::string, std::vector< std::string > > ParamValues;
 		
-		int sizeName;
-		int sizeValue;
+	public:
+		ParamsDlg(wxWindow *parent, wxWindowID id = 1, const wxString &title = wxT("ParamsDialog"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = ParamsDlg_STYLE);
+		virtual ~ParamsDlg();
+		void WxButton1Click(wxCommandEvent& event);
+		void SetValueClick(wxCommandEvent& event);
+		void WxChoice1Selected(wxCommandEvent& event );
+		void ParamChoiceSelected(wxCommandEvent& event );
+		void SetButtonClick(wxCommandEvent& event);
+		void AppendList(const char *);
+	
+	private:
+		wxButton *SetButton;
+		wxTextCtrl *WxMemo3;
+		wxTextCtrl *PromptMemo;
+		wxTextCtrl *OptionsMemo;
+		wxStaticText *ChildrenLabel;
+		wxStaticText *HasLabel;
+		wxTextCtrl *HasChildrenEdit;
+		wxStaticBox *OABox;
+		wxTextCtrl *PortTypeEdit;
+		wxStaticText *PortTypeLabel;
+		wxStaticText *MultiportLabel;
+		wxTextCtrl *MultiportEdit;
+		wxTextCtrl *GenderEdit;
+		wxStaticText *GenderLabel;
+		wxStaticText *InOrOutLabel;
+		wxTextCtrl *InOrOutEdit;
+		wxStaticBox *FCPBox;
+		wxStaticText *TypeLabel;
+		wxTextCtrl *WxEdit16;
+		wxStaticBox *CSBox;
+		wxStaticText *DValueLabel;
+		wxStaticText *ListLabel;
+		wxStaticText *MeasureLabel;
+		wxStaticText *QuantityLabel;
+		wxStaticText *PromptLabel;
+		wxTextCtrl *DefaultValueEdit;
+		wxStaticText *DefaultLabel;
+		wxTextCtrl *LowerLimitEdit;
+		wxStaticText *LowerLimitLabel;
+		wxStaticText *UpperLimitLabel;
+		wxTextCtrl *UpperLimitEdit;
+		wxTextCtrl *EnterableEdit;
+		wxStaticText *EnterableLabel;
+		wxStaticText *OutputLabel;
+		wxTextCtrl *OutputEdit;
+		wxStaticBox *AVNBox;
+		wxStaticText *RecordLabel;
+		wxTextCtrl *RecordTypeEdit;
+		wxStaticBox *MARBox;
+		wxStaticText *OptionsLabel;
+		wxStaticText *OptionLabel;
+		wxTextCtrl *OptionListEdit;
+		wxStaticText *BasisLabel;
+		wxTextCtrl *BasisEdit;
+		wxStaticText *UnitLabel;
+		wxTextCtrl *UnitEdit;
+		wxStaticText *PhysicalLabel;
+		wxTextCtrl *Quantity;
+		wxTextCtrl *ValueEdit;
+		wxStaticText *ValueLabel;
+		wxStaticBox *VARBox;
+		wxStaticText *DimensionLabel;
+		wxTextCtrl *DimensionEdit;
+		wxChoice *ParamChoice;
+		wxStaticText *NodePathLabel;
+		wxTextCtrl *NodePath;
+		wxStaticText *ParameterLabel;
+		
+	private:
 		enum
 		{
-			////GUI Enum Control ID Start
-			ID_WXCHOICE1 = 1003,
-			ID_WXGRID1 = 1002,
-			ID_WXPANEL1 = 1001,
-			////GUI Enum Control ID End
-			ID_DUMMY_VALUE_ //don't remove this value unless you have other enum values
+			ID_SETBUTTON = 1235,
+			ID_WXMEMO3 = 1234,
+			ID_PROMPTMEMO = 1233,
+			ID_OPTIONSMEMO = 1232,
+			ID_CHILDRENLABEL = 1231,
+			ID_HASLABEL = 1230,
+			ID_HASCHILDRENEDIT = 1229,
+			ID_OABOX = 1228,
+			ID_PORTTYPEEDIT = 1216,
+			ID_PORTTYPELABEL = 1215,
+			ID_MULTIPORTLABEL = 1214,
+			ID_MULTIPORTEDIT = 1213,
+			ID_GENDEREDIT = 1212,
+			ID_GENDERLABEL = 1211,
+			ID_INOROUTLABEL = 1210,
+			ID_INOROUTEDIT = 1209,
+			ID_FCPBOX = 1208,
+			ID_TYPELABEL = 1116,
+			ID_WXEDIT16 = 1112,
+			ID_CSBOX = 1111,
+			ID_DVALUELABEL = 1107,
+			ID_LISTLABEL = 1106,
+			ID_MEASURELABEL = 1105,
+			ID_QUANTITYLABEL = 1104,
+			ID_PROMPT = 1103,
+			ID_DEFAULTVALUEEDIT = 1101,
+			ID_DEFAULTLABEL = 1100,
+			ID_LOWERLIMITEDIT = 1099,
+			ID_LOWERLIMITLABEL = 1098,
+			ID_UPPERLIMITLABEL = 1097,
+			ID_UPPERLIMITEDIT = 1096,
+			ID_ENTERABLEEDIT = 1095,
+			ID_ENTERABLELABEL = 1094,
+			ID_OUTPUTLABEL = 1093,
+			ID_OUTPUTEDIT = 1092,
+			ID_AVNBOX = 1046,
+			ID_RECORDLABEL = 1045,
+			ID_RECORDTYPEEDIT = 1044,
+			ID_MARBOX = 1042,
+			ID_OPTIONSLABEL = 1041,
+			ID_OPTIONLABEL = 1033,
+			ID_OPTIONLISTEDIT = 1032,
+			ID_BASISLABEL = 1027,
+			ID_BASISEDIT = 1026,
+			ID_UNITLABEL = 1024,
+			ID_UNITEDIT = 1023,
+			ID_PHYSICALLABEL = 1018,
+			ID_QUANTITY = 1017,
+			ID_VALUEEDIT = 1012,
+			ID_VALUELABEL = 1011,
+			ID_ = 1009,
+			ID_DIMENSIONLABEL = 1008,
+			ID_DIMENSIONEDIT = 1007,
+			ID_PARAMCHOICE = 1006,
+			ID_NODEPATHLABEL = 1004,
+			ID_NODEPATH = 1003,
+			ID_PARAMETERLABEL = 1001,
+			ID_DUMMY_VALUE_
 		};
 	
 	private:
