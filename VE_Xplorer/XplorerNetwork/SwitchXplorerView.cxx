a70dd965512b0ffba58f69b501315e72b5e35889
Doug McCorkle <doug.mccorkle@gmail.com>
2006-12-14 04:32:33 +0000

    new eh to handle switching between network views

---
+++ VE_Xplorer/XplorerNetwork/SwitchXplorerView.cxx
@@ -0,0 +1,132 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2006-07-08 22:57:46 -0500 (Sat, 08 Jul 2006) $
+ * Version:       $Rev: 4907 $
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#include "VE_Xplorer/XplorerNetwork/SwitchXplorerViewEventHandler.h"
+#include "VE_Xplorer/XplorerHandlers/cfdModel.h"
+#include "VE_Xplorer/XplorerHandlers/cfdModelHandler.h"
+#include "VE_Xplorer/XplorerNetwork/cfdExecutive.h"
+#include "VE_Xplorer/GraphicalPlugin/cfdVEBaseClass.h"
+
+
+#include "VE_Open/XML/XMLObject.h"
+#include "VE_Open/XML/Command.h"
+#include "VE_Open/XML/FloatArray.h"
+#include "VE_Open/XML/Transform.h"
+#include "VE_Open/XML/DataValuePair.h"
+#include "VE_Open/XML/ParameterBlock.h"
+#include "VE_Open/XML/Model/Model.h"
+
+#include "VE_Xplorer/XplorerHandlers/cfdDebug.h"
+
+#include <iostream>
+
+using namespace VE_EVENTS;
+using namespace VE_Xplorer;
+
+////////////////////////////////////////////////////////////////////////////
+//Constructor                                                             //
+////////////////////////////////////////////////////////////////////////////
+SwitchXplorerViewEventHandler::SwitchXplorerViewEventHandler()
+:VE_EVENTS::EventHandler()
+{
+   ;
+}
+////////////////////////////////////////////////////////////////////////////////
+SwitchXplorerViewEventHandler::SwitchXplorerViewEventHandler(const SwitchXplorerViewEventHandler& rhs)
+:VE_EVENTS::EventHandler(rhs)
+{
+   ;
+}
+////////////////////////////////////////////////////////////////////////////////
+///Destructor                                      //
+////////////////////////////////////////////////////////////////////////////////
+SwitchXplorerViewEventHandler::~SwitchXplorerViewEventHandler()
+{
+   ;
+}
+////////////////////////////////////////////////////////////////////////////////
+///Equal operator
+////////////////////////////////////////////////////////////////////////////////
+SwitchXplorerViewEventHandler& SwitchXplorerViewEventHandler::operator=(const SwitchXplorerViewEventHandler& rhs)
+{
+   if(this != &rhs)
+   {
+      VE_EVENTS::SwitchXplorerViewEventHandler::operator=(rhs);
+   }
+   return *this;
+}
+////////////////////////////////////////////////////////////////////////////////
+void SwitchXplorerViewEventHandler::SetGlobalBaseObject(VE_Xplorer::cfdGlobalBase* model)
+{
+   ;
+}
+//////////////////////////////////////////////////////////////////////////
+void SwitchXplorerViewEventHandler::Execute( VE_XML::XMLObject* xmlObject )
+{
+   /*// Get the active object
+   VE_XML::Command* command = dynamic_cast< VE_XML::Command* >( xmlObject );
+   VE_XML::DataValuePair* activeModelDVP = command->GetDataValuePair( "Object ID" );   
+   unsigned int id = 0;
+   activeModelDVP->GetData( id );
+
+   _plugins = cfdExecutive::instance()->GetTheCurrentPlugins();
+
+   // Remove any plugins that aren't present in the current network
+   std::map< int, cfdVEBaseClass* >::iterator foundPlugin;
+   foundPlugin = _plugins->find( id );
+
+   if ( foundPlugin != _plugins->end() )
+   {
+      vprDEBUG(vesDBG,1) << "|\t\tPlugin [ " << foundPlugin->first 
+                              << " ]-> " << foundPlugin->second 
+                              << " is being deleted."
+                              << std::endl << vprDEBUG_FLUSH;
+      // if a module is on the plugins map then remove it
+      foundPlugin->second->RemoveSelfFromSG();
+      cfdModelHandler::instance()->RemoveModel( foundPlugin->second->GetCFDModel() );
+      // Must delete current instance of vebaseclass object
+      delete foundPlugin->second;
+      _plugins->erase( foundPlugin );
+   }
+   else
+   {
+      vprDEBUG(vesDBG,1) << "|\t\tPlugin [ " << foundPlugin->first 
+                              << " ]-> " << foundPlugin->second 
+                              << " not present."
+                              << std::endl << vprDEBUG_FLUSH;
+   }
+   //Set active model to null so that if the previous active model is deleted
+   //that we don't get errors in our code other places.
+   cfdModelHandler::instance()->SetActiveModel( 0 );
+   vprDEBUG(vesDBG,1) << "|\t\tPlugin is deleted if present."
+                           << std::endl << vprDEBUG_FLUSH;*/
+}
---
+++ VE_Xplorer/XplorerNetwork/SwitchXplorerView.h
@@ -0,0 +1,88 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2006-09-29 21:36:52 -0500 (Fri, 29 Sep 2006) $
+ * Version:       $Rev: 5599 $
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef SWITCH_XPLORER_VIEW_EVENT_HANDLER_H
+#define SWITCH_XPLORER_VIEW_EVENT_HANDLER_H
+/*!\file SwitchXplorerViewEventHandler.h
+  SwitchXplorerViewEventHandler API
+  */
+/*!\class SwitchXplorerViewEventHandler
+ * Class for swithcing the view in xplorer.
+ */
+//#include <string>
+//#include <vector>
+#include <map>
+#include "VE_Installer/include/VEConfig.h"
+//#include <utility>
+
+namespace VE_XML
+{
+   class XMLObject;
+}
+namespace VE_Xplorer
+{
+   class cfdModel;
+   class cfdGlobalBase;
+   class cfdVEBaseClass;
+}
+#include "VE_Xplorer/XplorerHandlers/EventHandler.h"
+namespace VE_EVENTS
+{
+class VE_XPLORER_NETWORK_EXPORTS SwitchXplorerViewEventHandler: public EventHandler
+{
+public:
+   ///Constructor
+   SwitchXplorerViewEventHandler();
+
+   ///Copy Constructor
+   SwitchXplorerViewEventHandler(const SwitchXplorerViewEventHandler& rhs);
+
+   ///Destructor
+   virtual ~DeleteObjectFromNetworkEventHandler();
+
+   ///Equal operator
+   SwitchXplorerViewEventHandler& operator=(const SwitchXplorerViewEventHandler& rhs);
+
+   ///Set the cfdModel.
+   ///\param model The cfdModel to execute the Command on\n.
+   ///Default uses the active cfdModel from cfdModelHandler\n
+   ///Otherwise, the cfdModel passed in is used.
+   void SetGlobalBaseObject(VE_Xplorer::cfdGlobalBase* model=0);
+   
+   ///Exectute the event
+   ///\param xmlObject The current xmlObject event.
+   void Execute( VE_XML::XMLObject* command); 
+
+private:
+};
+}
+#endif// SWITCH_XPLORER_VIEW_EVENT_HANDLER_H
