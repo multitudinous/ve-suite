5cc74553c4205271588a5b8ae86ed307999344a4
Doug McCorkle <doug.mccorkle@gmail.com>
2007-01-03 04:11:15 +0000

    removed old vesuite home code.

--- VE_Xplorer/TextureBased/cfdOSGShaderManager.h
+++ VE_Xplorer/TextureBased/cfdOSGShaderManager.h
@@ -52,47 +52,46 @@ namespace osg
 #include <string>
 namespace VE_TextureBased
 {
-   class VE_TEXTURE_BASED_EXPORTS cfdOSGShaderManager{
-   public:
-      cfdOSGShaderManager();
-      cfdOSGShaderManager(const cfdOSGShaderManager& sm);
-      virtual ~cfdOSGShaderManager();
+class VE_TEXTURE_BASED_EXPORTS cfdOSGShaderManager
+{
+public:
+   cfdOSGShaderManager();
+   cfdOSGShaderManager(const cfdOSGShaderManager& sm);
+   virtual ~cfdOSGShaderManager();
 
-      virtual void Init() = 0;
+   virtual void Init() = 0;
 
-      void SetShaderDirectory(std::string dir);
-      void SetBounds(float* bounds);
-      void UseCG(bool useCG = false);
+   void SetShaderDirectory(std::string dir);
+   void SetBounds(float* bounds);
+   void UseCG(bool useCG = false);
 
-      osg::StateSet* GetShaderStateSet();
-      unsigned int GetAutoGenTextureUnit(){return _tUnit;}
+   osg::StateSet* GetShaderStateSet();
+   unsigned int GetAutoGenTextureUnit(){return _tUnit;}
 
-   protected:
-      virtual cfdOSGShaderManager& operator=(const cfdOSGShaderManager& sm);
+protected:
+   virtual cfdOSGShaderManager& operator=(const cfdOSGShaderManager& sm);
+   //////////////////
+   //GLSL interface//
+   //////////////////
+   virtual void _setupGLSLShaderProgram(osg::StateSet* ss,
+                                     osg::Program* glslProgram,
+                                     const std::string pgName,bool override = false);
+   virtual osg::Shader* _createGLSLShaderFromFile(const std::string filename,
+                                                        bool isFrag);
+   virtual osg::Shader*  _createGLSLShaderFromInline(const std::string inlineSource,
+                                                        bool isFrag);
+  
+   virtual void _setupStateSetForGLSL()=0;
 
-      virtual char* _createShaderPathForFile(std::string shaderFile);
-      //////////////////
-      //GLSL interface//
-      //////////////////
-      virtual void _setupGLSLShaderProgram(osg::StateSet* ss,
-                                        osg::Program* glslProgram,
-                                        const std::string pgName,bool override = false);
-      virtual osg::Shader* _createGLSLShaderFromFile(const std::string filename,
-                                                           bool isFrag);
-      virtual osg::Shader*  _createGLSLShaderFromInline(const std::string inlineSource,
-                                                           bool isFrag);
-     
-      virtual void _setupStateSetForGLSL()=0;
+   osg::ref_ptr<osg::Shader> _vshader;
+   osg::ref_ptr<osg::Shader> _fshader;
 
-      osg::ref_ptr<osg::Shader> _vshader;
-      osg::ref_ptr<osg::Shader> _fshader;
-
-      osg::ref_ptr<osg::StateSet> _ss;
-      std::string _shaderDirectory;
-      unsigned int _tUnit;
-      float* _bounds;
-      bool _useGLSL;
-   };
+   osg::ref_ptr<osg::StateSet> _ss;
+   std::string _shaderDirectory;
+   unsigned int _tUnit;
+   float* _bounds;
+   bool _useGLSL;
+};
 }
 #endif //_OSG
 #endif
