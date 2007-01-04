190672d628047c2431e7056aa662b141f54c9cc9
S. Gent <sgent@unknown.nil>
2007-01-04 02:31:37 +0000

    cleaned up redundant code for starReader.cxx for cell type 11

--- VE_Builder/Translator/DataLoader/starReader.cxx
+++ VE_Builder/Translator/DataLoader/starReader.cxx
@@ -378,21 +378,13 @@ vtkUnstructuredGrid * starReader::GetUnsGrid( void )
          else if ( cellType == 11 )
          {
             cellFile >> tempData;  //first entry on the second line
-            currentPos = 1;
 
             for ( size_t i = 0; i<8; ++i)
             {
                cellFile >> tempData;
                vertList->InsertUniqueId(tempData-vShift);
-
-               if(currentPos%8 ==0)
-               {
-                  currentPos = 0;
-                  //get the end of line and go to the next one
-                  cellFile.getline( tempLine, charSize );
-                  //ignore the cell number
-               }
             }
+
             cellFile.getline( tempLine, charSize );
             // vertList->InsertUniqueId( tempData - vShift );
             uGrid->InsertNextCell( VTK_CONVEX_POINT_SET, vertList );
