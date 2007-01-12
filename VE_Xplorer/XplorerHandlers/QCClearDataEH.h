c95ef6095d5e07180b5b770f676715279d6bcb94
Biv <biv@unknown.nil>
2007-01-12 00:30:41 +0000

    forgot to add these in

---
+++ VE_Xplorer/XplorerHandlers/QCClearDataEH.h
@@ -0,0 +1,79 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2006-09-29 21:31:32 -0500 (Fri, 29 Sep 2006) $
+ * Version:       $Rev: 5598 $
+ * Author:        $Author: mccdo $
+ * Id:            $Id: QCClearDataEH.h 5598 2006-09-30 02:31:32Z mccdo $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef QUAT_CAM_CLEAR_DATA_H
+#define QUAT_CAM_CLear_DATA_H
+/*!\file QCClearDataEH.h
+  QuatCamClearDataEventHandler API
+  */
+/*!\class QuatCamClearDataEventHandler
+ * Activate the texture based visualization.
+ */
+
+namespace VE_XML
+{
+   class XMLObject;
+}
+namespace VE_Xplorer
+{
+   class cfdGlobalBase;
+   class cfdModel;
+}
+
+#include "VE_Xplorer/XplorerHandlers/EventHandler.h"
+
+#include "VE_Installer/include/VEConfig.h"
+
+namespace VE_EVENTS{
+   class VE_XPLORER_EXPORTS QuatCamClearDataEventHandler : public VE_EVENTS::EventHandler{
+public:
+   ///Constructor
+   QuatCamClearDataEventHandler();
+
+   ///Copy Constructor
+   QuatCamClearDataEventHandler(const QuatCamClearDataEventHandler& ceh);
+   ///Destructor
+   virtual ~QuatCamClearDataEventHandler();
+
+   ///Equal operator
+   QuatCamClearDataEventHandler& operator=(const QuatCamClearDataEventHandler& rhs);
+   ///\param baseObject The cfdGlobalBase object to apply the command to.
+   virtual void SetGlobalBaseObject(VE_Xplorer::cfdGlobalBase* baseObject=0); 
+   
+   ///The internal operation on the CADNode.
+   ///\param veXMLObject The veXMLObject to execute.
+   virtual void Execute(VE_XML::XMLObject* veXMLObject);
+protected:
+
+};
+}
+#endif// QUAT_CAM_LOAD_FILE_H
