8f001bdff256ede727900bce46226526a0154e1d
Joshua Koch <jbkoch@agsolver.com>
2007-01-16 20:30:16 +0000

    Added bullet stuff to PhysicsSimulator so Doug can test on Mac

--- VE_Xplorer/SceneGraph/PhysicsSimulator.cxx
+++ VE_Xplorer/SceneGraph/PhysicsSimulator.cxx
@@ -1,28 +1,59 @@
 #include "VE_Xplorer/SceneGraph/PhysicsSimulator.h"
-/*#include <ode/config.h>
-#if defined(dDOUBLE)
-#define OPAL_USE_DOUBLE 1
-#endif
-#include <opal.h>*/
+
+#include "btBulletDynamicsCommon.h"
 
 using namespace VE_SceneGraph;
+
+const int maxProxies=32766;
 
 vprSingletonImp(PhysicsSimulator);
 
 ////////////////////////////////////////////////////////////////////////////////
 PhysicsSimulator::PhysicsSimulator()
 {
-   //simulator=opal::createSimulator();
-	//simulator->setGravity(opal::Vec3r(0.0, (opal::real)-9.81, 0.0));
+   
 }
 ////////////////////////////////////////////////////////////////////////////////
 void PhysicsSimulator::CleanUp()
 {
-   //simulator->destroy();
+   
 }
 ////////////////////////////////////////////////////////////////////////////////
-/*opal::Simulator* PhysicsSimulator::GetSimulator()
+void PhysicsSimulator::InitPhysics()
 {
-   return this->simulator;
-}*/
+   btCollisionDispatcher* dispatcher=new btCollisionDispatcher();
+
+   #ifdef USE_CUSTOM_NEAR_CALLBACK
+	   //This is optional
+	   dispatcher->setNearCallback(customNearCallback);
+   #endif
+
+   btVector3 worldAabbMin(-10000,-10000,-10000);
+	btVector3 worldAabbMax(10000,10000,10000);
+
+	btOverlappingPairCache* broadphase=new btAxisSweep3(worldAabbMin,worldAabbMax,maxProxies);
+   //btOverlappingPairCache* broadphase=new btSimpleBroadphase;
+	
+   #ifdef REGISTER_CUSTOM_COLLISION_ALGORITHM
+      //This is optional
+   #endif
+
+   #ifdef COMPARE_WITH_QUICKSTEP
+      //This is optional
+	   btConstraintSolver* solver=new OdeConstraintSolver();
+   #else
+	   //default constraint solver
+	   btSequentialImpulseConstraintSolver* solver=new btSequentialImpulseConstraintSolver;
+   #endif
+		
+   dynamics_world=new btDiscreteDynamicsWorld(dispatcher,broadphase,solver);
+	dynamics_world->setGravity(btVector3(0,-10,0));
+
+	//dynamics_world->setDebugDrawer(&debugDrawer);
+}
+////////////////////////////////////////////////////////////////////////////////
+btDynamicsWorld* PhysicsSimulator::GetDynamicsWorld()
+{
+   return dynamics_world;
+}
 ////////////////////////////////////////////////////////////////////////////////
--- VE_Xplorer/SceneGraph/PhysicsSimulator.h
+++ VE_Xplorer/SceneGraph/PhysicsSimulator.h
@@ -1,39 +1,41 @@
+
+//Bullet Continuous Collision Detection and Physics Library
+//Copyright (c) 2003-2006 Erwin Coumans  http://continuousphysics.com/Bullet/
+
 #ifndef PHYSICS_SIMULATOR_H
 #define PHYSICS_SIMULATOR_H
-
 /*!\file PhysicsSimulator.h
 PhysicsSimulator API
 */
 /*!\class VE_SceneGraph::PhysicsSimulator
 * 
 */
-
 #include <vpr/Util/Singleton.h>
 #include "VE_Installer/include/VEConfig.h"
 
-/*namespace opal
-{
-   class Simulator;
-}*/
+class	btDynamicsWorld;
 
 namespace VE_SceneGraph
 {
    class VE_SCENEGRAPH_EXPORTS PhysicsSimulator                //: public vpr::Singleton< PhysicsSimulator >
    {
       public:
-         void CleanUp();                                       //Functions like a destructor
+         void CleanUp();                                       //Functions as the destructor
 
-         //opal::Simulator* GetSimulator();
+         btDynamicsWorld* GetDynamicsWorld();
 
       private:
          PhysicsSimulator();
          ~PhysicsSimulator(){;}                                //Never gets called, don't implement
          vprSingletonHeader(PhysicsSimulator);
 
+         void InitPhysics();
 
-         //opal::Simulator* simulator;
+         //Manages physics objects and constraints and implements update of all objects each frame
+	      btDynamicsWorld* dynamics_world;
+
    };
 }
 
-#endif
+#endif //PHYSICS_SIMULATOR_H
 
