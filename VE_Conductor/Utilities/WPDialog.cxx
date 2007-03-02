5594c959e15da8fb391158dbaf05e5e34f25de87
J. Misra <jmisra@unknown.nil>
2007-03-02 19:11:00 +0000

    added new dialog for streamline seed points

---
+++ VE_Conductor/Utilities/WPDialog.cxx
@@ -0,0 +1,235 @@
+#include "VE_Conductor/Utilities/WPDialog.h"
+//#include "VE_Conductor/Utilities/basic.h"
+
+#include "VE_Open/XML/Command.h"
+#include "VE_Open/XML/DataValuePair.h"
+#include "VE_Open/XML/XMLReaderWriter.h"
+
+#include <wx/statbox.h>
+
+using namespace VE_Conductor::GUI_Utilities;
+
+WPDialog::WPDialog(wxWindow* parent, int id, std::string title):
+BaseDialog(parent, id, title)
+{
+   _xBounds = 0;
+   _yBounds = 0;
+   _zBounds = 0;
+   _buildGUI();
+   /*
+   wxSize displaySize = ::wxGetDisplaySize();
+    wxRect dialogPosition( displaySize.GetWidth()-427, 440, 427, displaySize.GetHeight()-480 );
+   this->SetSize( dialogPosition );
+   */
+   wxSize displaySize = ::wxGetDisplaySize();
+   wxRect dialogPosition( displaySize.GetWidth()-427, 440, 427, displaySize.GetHeight()-480 );
+   this->SetSize( dialogPosition ); 
+}
+////////////////////////////////////////////////////////////////////////////////////
+WPDialog::~WPDialog()
+{
+
+}
+////////////////////////////////////////////////////////////////////////////////////
+void WPDialog::_buildGUI()
+{
+   wxStaticBox* dualSliderGroup = new wxStaticBox(this, -1, wxT("Bounding Box Controls"));
+   wxStaticBoxSizer* mainSizer = new wxStaticBoxSizer(dualSliderGroup,wxVERTICAL);
+   
+   _createDualSliders();
+   wxBoxSizer* xdualSizer = new wxBoxSizer (wxHORIZONTAL);
+   xdualSizer->Add(_xBounds,1,wxALIGN_CENTER|wxEXPAND);
+   
+   wxBoxSizer* ydualSizer = new wxBoxSizer (wxHORIZONTAL);
+   ydualSizer->Add(_yBounds,1,wxALIGN_CENTER|wxEXPAND);
+   
+   wxBoxSizer* zdualSizer = new wxBoxSizer (wxHORIZONTAL);
+   zdualSizer->Add(_zBounds,1,wxALIGN_CENTER|wxEXPAND); 
+   
+     wxBoxSizer* buttonRowSizer = new wxBoxSizer(wxHORIZONTAL);
+     _addOKButton(buttonRowSizer);
+   
+   
+   mainSizer->Add(xdualSizer,1,wxALIGN_CENTER|wxEXPAND);  
+   mainSizer->Add(ydualSizer,1,wxALIGN_CENTER|wxEXPAND);   
+   mainSizer->Add(zdualSizer,1,wxALIGN_CENTER|wxEXPAND);   
+   
+   mainSizer->Add(buttonRowSizer,1,wxALIGN_CENTER|wxEXPAND);
+   
+   _xBounds->Raise();
+   _yBounds->Raise();
+   _zBounds->Raise();
+   
+   SetAutoLayout(true);
+   
+   SetSizer(mainSizer);
+   mainSizer->Fit(dynamic_cast<BaseDialog*>(this));
+}
+////////////////////////////////////////////////////////////////////////////////////
+void WPDialog::_createDualSliders()
+{
+//X Slider
+  _xBounds = new DualSlider(this, -1, 1,0,100, 0, 100, wxDefaultPosition,
+	  wxDefaultSize,wxSL_HORIZONTAL|wxSL_AUTOTICKS|wxSL_LABELS,
+	  wxString("X Bounds"));
+   WPMinSliderCallback* minX = new WPMinSliderCallback(this,"X");
+   WPMaxSliderCallback* maxX = new WPMaxSliderCallback(this,"X");
+   WPBothMoveCallback* bothX = new WPBothMoveCallback(this,"X");
+   
+   _xBounds->SetMinSliderCallback(minX);
+   _xBounds->SetMaxSliderCallback(maxX);
+   _xBounds->SetBothSliderUpdateCallback(bothX);
+   
+//Y slider   
+     _yBounds = new DualSlider(this, -1, 1,0,100, 0, 100, wxDefaultPosition,
+	  wxDefaultSize,wxSL_HORIZONTAL|wxSL_AUTOTICKS|wxSL_LABELS,
+	  wxString("Y Bounds"));
+   WPMinSliderCallback* minY = new WPMinSliderCallback(this,"Y");
+   WPMaxSliderCallback* maxY = new WPMaxSliderCallback(this,"Y");
+   WPBothMoveCallback* bothY = new WPBothMoveCallback(this,"Y");
+   
+   _yBounds->SetMinSliderCallback(minY);
+   _yBounds->SetMaxSliderCallback(maxY);
+   _yBounds->SetBothSliderUpdateCallback(bothY);
+   
+//Z slider
+
+  _zBounds = new DualSlider(this, -1, 1,0,100, 0, 100, wxDefaultPosition,
+	  wxDefaultSize,wxSL_HORIZONTAL|wxSL_AUTOTICKS|wxSL_LABELS,
+	  wxString("Z Bounds"));
+   WPMinSliderCallback* minZ = new WPMinSliderCallback(this,"Z");
+   WPMaxSliderCallback* maxZ = new WPMaxSliderCallback(this,"Z");
+   WPBothMoveCallback* bothZ = new WPBothMoveCallback(this,"Z");
+   
+   _zBounds->SetMinSliderCallback(minZ);
+   _zBounds->SetMaxSliderCallback(maxZ);
+   _zBounds->SetBothSliderUpdateCallback(bothZ);
+
+}     
+////////////////////////////////////////////////////////////////////////////////////
+void WPDialog::WPMinSliderCallback::SliderOperation()
+{
+//what does TP stand for?
+   _wpdlg->SetCommandName("Min WP Update");
+
+   VE_XML::DataValuePair* coordinate = new VE_XML::DataValuePair();
+   coordinate->SetDataType("STRING");
+   coordinate->SetDataName(std::string("Coordinate"));
+   coordinate->SetDataString(_direction);
+   _wpdlg->AddInstruction(coordinate);
+   
+   VE_XML::DataValuePair* direction = new VE_XML::DataValuePair();
+   direction->SetDataType("STRING");
+   direction->SetDataName(std::string("Direction"));
+   direction->SetDataString("Positive");
+   _wpdlg->AddInstruction(direction);
+   
+   VE_XML::DataValuePair* value = new VE_XML::DataValuePair;
+   value->SetData("WP Value",
+	   static_cast<double>(_dualSlider->GetMinSliderValue())/100.0);
+   _wpdlg->AddInstruction(value);
+   
+   _wpdlg->SendCommands();
+   _wpdlg->ClearInstructions();
+}
+////////////////////////////////////////////////////////////////////////////////////
+void WPDialog::WPMaxSliderCallback::SliderOperation()
+{
+//what does TP stand for?
+   _wpdlg->SetCommand("Max WP Update");
+
+   VE_XML::DataValuePair* coordinate = new VE_XML::DataValuePair();
+   coordinate->SetDataType("STRING");
+   coordinate->SetDataName(std::string("Coordinate"));
+   coordinate->SetDataString(_direction);
+   _wpdlg->AddInstruction(coordinate);
+   
+   VE_XML::DataValuePair* direction = new VE_XML::DataValuePair();
+   direction->SetDataType("STRING");
+   direction->SetDataName(std::string("Direction"));
+   direction->SetDataString("Negative");
+   _wpdlg->AddInstruction(direction);
+   
+   VE_XML::DataValuePair* value = new VE_XML::DataValuePair;
+   value->SetData("WP Value",
+	   static_cast<double>(_dualSlider->GetMaxSliderValue())/100.0);
+   _wpdlg->AddInstruction(value);
+   
+   _wpdlg->SendCommands();
+   _wpdlg->ClearInstructions();
+}
+////////////////////////////////////////////////////////////////////////////////////
+void WPDialog::WPBothMoveCallback::SliderOperation()
+{
+//what does TP stand for?
+   _wpdlg->SetCommand("Both WP Update");
+
+   VE_XML::DataValuePair* coordinate = new VE_XML::DataValuePair();
+   coordinate->SetDataType("STRING");
+   coordinate->SetDataName(std::string("Coordinate"));
+   coordinate->SetDataString(_direction);
+   _wpdlg->AddInstruction(coordinate);
+   
+   VE_XML::DataValuePair* direction = new VE_XML::DataValuePair();
+   direction->SetDataType("STRING");
+   direction->SetDataName(std::string("Direction"));
+   direction->SetDataString("Both");
+   _wpdlg->AddInstruction(direction);
+   
+   VE_XML::DataValuePair* minvalue = new VE_XML::DataValuePair;
+   minvalue->SetData("WP Value",
+	   static_cast<double>(_dualSlider->GetMinSliderValue())/100.0); 
+ 
+ 
+   VE_XML::DataValuePair* maxvalue = new VE_XML::DataValuePair;
+   maxvalue->SetData("WP Value",
+	   static_cast<double>(_dualSlider->GetMaxSliderValue())/100.0);
+   
+   
+   
+   _wpdlg->AddInstruction(minvalue);
+   _wpdlg->AddInstruction(maxvalue);
+   
+   _wpdlg->SendCommands();
+   _wpdlg->ClearInstructions();
+}
+//////////////////////////////////////////////////
+void WPDialog::SetCommandName(std::string newName)
+{
+   _commandName = newName;
+}
+////////////////////////////////////////////////////////////////////
+void WPDialog::AddInstruction(VE_XML::DataValuePair* newInstruction)
+{
+   _instructions.push_back(newInstruction);
+}
+////////////////////////////////////////////////////////////////////
+void WPDialog::SendCommands()
+{
+   _sendCommandsToXplorer();
+}
+////////////////////////////////////////////////////////////////////
+wxSizer* WPDialog::_buildSpecificWidgets()
+{
+   wxStaticBox* dualSliderGroup = new wxStaticBox(this, -1, wxT("Volume Clipping Planes"));
+   wxStaticBoxSizer* mainSizer = new wxStaticBoxSizer(dualSliderGroup,wxVERTICAL);
+
+   _createDualSliders();
+   wxBoxSizer* xdualSizer = new wxBoxSizer(wxHORIZONTAL);
+   xdualSizer->Add(_xBounds,1,wxALIGN_CENTER|wxEXPAND);
+
+   wxBoxSizer* ydualSizer = new wxBoxSizer(wxHORIZONTAL);
+   ydualSizer->Add(_yBounds,1,wxALIGN_CENTER|wxEXPAND);
+
+   wxBoxSizer* zdualSizer = new wxBoxSizer(wxHORIZONTAL);
+   zdualSizer->Add(_zBounds,1,wxALIGN_CENTER|wxEXPAND);
+
+   mainSizer->Add(xdualSizer,1,wxALIGN_CENTER|wxEXPAND);
+   mainSizer->Add(ydualSizer,1,wxALIGN_CENTER|wxEXPAND);
+   mainSizer->Add(zdualSizer,1,wxALIGN_CENTER|wxEXPAND);
+
+   return mainSizer;
+}
+
+
+   
---
+++ VE_Conductor/Utilities/WPDialog.h
@@ -0,0 +1,131 @@
+#ifndef WP_DIALOG_H
+#define WP_DIALOG_H
+
+
+#include <string> 
+#include "VE_Installer/include/VEConfig.h"
+#include "VE_Conductor/Utilities/BaseDialog.h"
+#include "VE_Conductor/Utilities/DualSlider.h"
+namespace VE_Conductor
+{
+
+namespace GUI_Utilities
+{
+class VE_CONDUCTOR_UTILS_EXPORTS WPDialog : public BaseDialog
+
+{
+   public:
+   //Constructor
+      WPDialog(wxWindow* parent, int id, std::string title);
+   
+   //Destructor
+   virtual ~WPDialog();
+   
+   
+   ///Set the name of the command
+   void SetCommandName(std::string name);
+  
+   ///Send the commands to Xplorer;
+   void SendCommands();
+
+   ///Add an instruction to send. This is for access in the callbacks.
+   void AddInstruction(VE_XML::DataValuePair* newInstruct);
+   
+   
+   void SetCommand(std::string name){;}
+   
+   protected:
+   
+   class WPMinSliderCallback: public
+   	VE_Conductor::GUI_Utilities::DualSlider::SliderCallback
+   {
+      public:
+      
+      WPMinSliderCallback(WPDialog* parent, std::string direction = "X")
+      
+      {
+         _direction = direction;
+	 _wpdlg = parent;
+      }
+      
+      virtual ~WPMinSliderCallback()
+      {
+         _direction.clear();
+      }
+      
+      virtual void SliderOperation();
+      
+      protected:
+      std::string _direction;
+      WPDialog* _wpdlg;
+    };
+
+
+   class WPBothMoveCallback: public
+   	VE_Conductor::GUI_Utilities::DualSlider::SliderCallback
+   {
+      public:
+      
+      WPBothMoveCallback(WPDialog* parent, std::string direction = "X")
+      
+      {
+         _direction = direction;
+	 _wpdlg = parent;
+      }
+      
+      virtual ~WPBothMoveCallback()
+      {
+         _direction.clear();
+      }
+      
+      virtual void SliderOperation();
+      
+      protected:
+      std::string _direction;
+      WPDialog* _wpdlg;
+    };
+
+
+
+   class WPMaxSliderCallback: public
+   	VE_Conductor::GUI_Utilities::DualSlider::SliderCallback
+   {
+      public:
+      
+      WPMaxSliderCallback(WPDialog* parent, std::string direction = "X")
+      
+      {
+         _direction = direction;
+	 _wpdlg = parent;
+      }
+      
+      virtual ~WPMaxSliderCallback()
+      {
+         _direction.clear();
+      }
+      
+      virtual void SliderOperation();
+      
+      protected:
+      std::string _direction;
+      WPDialog* _wpdlg;
+    };
+
+//Build the DualSliders for the dialog
+void _createDualSliders();
+
+//Add controls for dialog
+virtual void _buildGUI();
+virtual wxSizer* _buildSpecificWidgets();
+
+
+//
+VE_Conductor::GUI_Utilities::DualSlider* _xBounds;
+VE_Conductor::GUI_Utilities::DualSlider* _yBounds;
+VE_Conductor::GUI_Utilities::DualSlider* _zBounds;
+};
+
+}
+
+}
+#endif  //_WP_DIALOG_H
