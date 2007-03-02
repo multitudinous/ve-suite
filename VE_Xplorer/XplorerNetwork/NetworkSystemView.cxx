266b17efb63878efd9db9abb89961901b0d90a95
Terry Jordan <Terry.Jordan@netl.doe.gov>
2007-03-02 16:39:28 +0000

    improved the location and scale of the network view

--- VE_Xplorer/XplorerNetwork/NetworkSystemView.cxx
+++ VE_Xplorer/XplorerNetwork/NetworkSystemView.cxx
@@ -84,151 +84,6 @@ NetworkSystemView& NetworkSystemView::operator=( const NetworkSystemView& input 
    }
    return *this;
 }
-////////////////////////////////////////////////////////////////////////////////
-/*osg::ref_ptr< osg::Group > NetworkSystemView::DrawNetwork( void )
-{
-	osg::ref_ptr<osg::Group> loadedModels = new osg::Group();
-	
-	//instatiate the parser
-	BKPParser bkpp;
-//	bkpp.openFile(argv[1]);
-	bkpp.openFile("Hyper.bkp");
-	//bkpp.openFile("igcc.bkp");
-	// read the scene from the list of file specified commandline args.
-	//read blocks
-	int count = 0;
-
-	//if(bkpp.getNumComponents()<1)
-	//	std::cout<<"No Blocks available."<<std::endl;
-	
-	//add blocks and id to node
-	while(count<bkpp.getNumComponents())
-	{
-		//Add id text
-		//osg::ref_ptr<osg::Geode> textGeode = new osg::Geode();
-		//osg::ref_ptr<osgText::Text> text = new  osgText::Text;
-		//textGeode.get()->addDrawable(text.get());
-		//text.get()->setColor(osg::Vec4(1.0, 1.0, 0.0, 1.0));
-		//text.get()->setCharacterSize(6.0);
-		//text.get()->setAlignment(osgText::Text::CENTER_CENTER);
-		//text.get()->setText(bkpp.getBlockID(count));
-		//textGeode.get()->setName(bkpp.getBlockID(count));
-
-		osg::ref_ptr<osg::AutoTransform> at = new osg::AutoTransform;
-		at.get()->setPosition(osg::Vec3(bkpp.getXCoord(count)*30, bkpp.getYCoord(count)*30, 15.0f));
-		at.get()->setAutoRotateMode(osg::AutoTransform::ROTATE_TO_SCREEN);
-		//at.get()->addChild(textGeode.get());
-		loadedModels.get()->addChild(at.get());
-
-		//add 3d blocks
-		//osg::ref_ptr<osg::Node> loadedModel = osgDB::readNodeFile("osg/"+bkpp.getBlockType(count)+".ive");
-		osg::ref_ptr<osg::Node> loadedModel = osgDB::readNodeFile("3DIcons/"+bkpp.getBlockType(count)+"/"+bkpp.getBlockType(count)+"."+bkpp.getBlockIcon(count)+".obj");
-		
-		//add red block id if block .ive file is not found
-		if(loadedModel.get() == NULL)
-		{
-			//std::cout<<"Unsupported Component: "<<count<<" : "<<bkpp.getBlockType(count)<<std::endl;
-			loadedModel = osgDB::readNodeFile("3DIcons/UnsupportedComponent.ive");
-			//std::cout<<"Rendering Aborted!"<<std::endl;
-			//return 1;
-		}
-		
-		//set the blocks name
-		loadedModel.get()->setName(bkpp.getBlockID(count));
-		
-		//Scale up 3D comps
-		osg::ref_ptr<osg::AutoTransform> scale = new osg::AutoTransform;
-		scale.get()->addChild(loadedModel.get());
-		scale.get()->setScale(5.0f);
-
-		//move the block to its correct location
-		osg::ref_ptr<osg::MatrixTransform> mModelTrans = new osg::MatrixTransform();
-		//mModelTrans.get()->addChild(loadedModel.get());
-		mModelTrans.get()->addChild(scale.get());
-		mModelTrans.get()->preMult(osg::Matrix::translate(bkpp.getXCoord(count)*30, bkpp.getYCoord(count)*30, 0.0));
-		mModelTrans.get()->setName(bkpp.getBlockID(count));
-		loadedModels.get()->addChild(mModelTrans.get());
-		
-		count++;
-	}
-
-	//add streams to node
-	int streamCount=0;
-	count=0;
-	//create colors for each type of stream
-	osg::ref_ptr<osg::Vec4Array> colorBlack = new osg::Vec4Array;
-	colorBlack->push_back(osg::Vec4(0.0f,0.0f,0.0f,1.0f));
-	osg::ref_ptr<osg::Vec4Array> colorRed = new osg::Vec4Array;
-	colorRed->push_back(osg::Vec4(1.0f,0.0f,0.0f,1.0f));
-	osg::ref_ptr<osg::Vec4Array> colorGreen = new osg::Vec4Array;
-	colorGreen->push_back(osg::Vec4(0.0f,1.0f,0.0f,1.0f));
-
-	// same trick for shared normal.
-	osg::ref_ptr<osg::Vec3Array> shared_normals = new osg::Vec3Array;
-	shared_normals->push_back(osg::Vec3(0.0f,-1.0f,0.0f));
-	
-	osg::Geode* geode = new osg::Geode();
-
-	//if there are no streams
-	//if(bkpp.getNumStream()<1)
-	//	std::cout<<"No Streams available."<<std::endl;
-	
-	
-	while(streamCount < bkpp.getNumStream())
-	{
-		osg::Vec3Array* vertices = new osg::Vec3Array(bkpp.getStreamSize(streamCount));
-        osg::Geometry* linesGeom = new osg::Geometry();
-		while(count < bkpp.getStreamSize(streamCount))
-		{	
-        	(*vertices)[count].set(bkpp.getStreamXCoord(streamCount, count)*30.0, bkpp.getStreamYCoord(streamCount, count)*30.0, 0.0);
-			count++;
-		}
-		linesGeom->setVertexArray(vertices);
-		
-		// apply correct color to line
-		// Material - Type 0 - black
-		// Heat - Type 1 - Red
-		// Work - Type 2 - Green
-		if(bkpp.getStreamType(streamCount)==0)
-			linesGeom->setColorArray(colorBlack.get());
-		else if(bkpp.getStreamType(streamCount)==1)
-			linesGeom->setColorArray(colorRed.get());
-		else if(bkpp.getStreamType(streamCount)==2)
-			linesGeom->setColorArray(colorGreen.get());
-		//else
-		//	std::cout<<"Stream Type Error: Stream Type - "<<bkpp.getStreamType(streamCount)<<std::endl;
-		
-		linesGeom->setColorBinding(osg::Geometry::BIND_OVERALL);
-		// use the shared normal array.
-		linesGeom->setNormalArray(shared_normals.get());
-		linesGeom->setNormalBinding(osg::Geometry::BIND_OVERALL);
-
-	        linesGeom->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINE_STRIP,0,bkpp.getStreamSize(streamCount)));
-		
-		geode->addDrawable(linesGeom);
-
-		//Add id text
-		//osg::Geode* textGeode = new osg::Geode();
-		//osg::ref_ptr<osgText::Text> text = new  osgText::Text;
-		//textGeode->addDrawable(text.get());
-		//text.get()->setColor(osg::Vec4(1.0, 1.0, 0.0, 1.0));
-		//text.get()->setCharacterSize(6.0);
-		//text.get()->setAlignment(osgText::Text::CENTER_CENTER);
-		//text.get()->setText(bkpp.getStreamId(streamCount));
-		//textGeode->setName(bkpp.getStreamId(streamCount));
-
-		osg::AutoTransform* at = new osg::AutoTransform;
-		at->setPosition(osg::Vec3(bkpp.getStreamXCoord(streamCount, 0)*30.0, bkpp.getStreamYCoord(streamCount, 0)*30.0, 3.0f));
-		at->setAutoRotateMode(osg::AutoTransform::ROTATE_TO_SCREEN);
-		//at->addChild(textGeode);
-		loadedModels.get()->addChild(at);
-
-		streamCount++;
-		count = 0;
-	}
-	loadedModels.get()->addChild(geode);
-	return loadedModels.get();
-}*/
 
 ////////////////////////////////////////////////////////
 osg::ref_ptr< osg::Group > NetworkSystemView::DrawNetwork( void )
@@ -252,16 +107,25 @@ osg::ref_ptr< osg::Group > NetworkSystemView::DrawNetwork( void )
 	{
 		//_fileProgress->Update( 75 + (i*timeCalc), _("Loading data") );
 		VE_XML::VE_Model::Model* model = dynamic_cast< VE_XML::VE_Model::Model* >( objectVector.at( i ) );
-		std::cout<<"FILENAME: "<< model->GetIconFilename() <<std::endl;
-		VE_XML::VE_Model::Point * iconLocation = model->GetIconLocation();
-		std::pair<unsigned int, unsigned int> xyPair = iconLocation->GetPoint();
-		std::cout<<"X: "<< xyPair.first <<" Y: " << xyPair.second <<std::endl;
 
 		//add 3d blocks
 		osg::ref_ptr<osg::Node> loadedModel = osgDB::readNodeFile("3DIcons/"+model->GetIconFilename()+".obj");
 		//osg::ref_ptr<VE_SceneGraph::TextTexture> text = new VE_SceneGraph::TextTexture();
 		//text->UpdateText(model->GetModelName());
 		
+		//add red block id if block .ive file is not found
+		if(loadedModel.get() == NULL)
+			loadedModel = osgDB::readNodeFile("3DIcons/UnsupportedComponent.ive");
+
+		//set the blocks name
+		loadedModel->setName(model->GetModelName());
+
+		//Put origin in the center of the model
+		//osg::ref_ptr<osg::AutoTransform> loadedModelNormalized = new osg::AutoTransform();
+		//loadedModelNormalized->addChild(loadedModel.get());
+		//float radius = loadedModel.get()->getBound().radius();
+		//loadedModelNormalized->setPosition(osg::Vec3(0, 0, 0-radius));
+
 		//Result Pane
 		osg::ref_ptr<osg::Group> resultPane = new osg::Group();
 
@@ -270,6 +134,7 @@ osg::ref_ptr< osg::Group > NetworkSystemView::DrawNetwork( void )
         text->setColor(layoutColor);
 		text->setText(model->GetModelName());
 		//text->setAutoRotateToScreen(true);
+		text->setRotation(osg::Quat(osg::DegreesToRadians(90.0), osg::Vec3d(1.0, 0.0, 0.0)));
 		text->setCharacterSize(0.5);
 
 		osg::ref_ptr<osg::Geode> textGeode = new osg::Geode();
@@ -277,14 +142,14 @@ osg::ref_ptr< osg::Group > NetworkSystemView::DrawNetwork( void )
 
 		osg::ref_ptr<osg::AutoTransform> transTextGeode = new osg::AutoTransform();
 		transTextGeode->addChild(textGeode.get());
-		transTextGeode->setPosition(osg::Vec3d(-1.0, 0.0, 0.1));
+		transTextGeode->setPosition(osg::Vec3d(-1.0, -0.1, 1.0));
 
 		osg::ref_ptr<osg::Geometry> pane = new osg::Geometry;
 		osg::ref_ptr<osg::Vec3Array> coords = new osg::Vec3Array(4);
-		(*coords)[0] = osg::Vec3f(-1.5, -2.0, 0);//original png size is 300X400 => 3X4
-		(*coords)[1] = osg::Vec3f(1.5, -2.0, 0);
-		(*coords)[2] = osg::Vec3f(1.5, 2.0, 0);
-		(*coords)[3] = osg::Vec3f(-1.5, 2.0, 0);
+		(*coords)[0] = osg::Vec3f(-1.5, 0.0, -2.0);//original png size is 300X400 => 3X4
+		(*coords)[1] = osg::Vec3f(1.5, 0.0, -2.0);
+		(*coords)[2] = osg::Vec3f(1.5, 0.0, 2.0);
+		(*coords)[3] = osg::Vec3f(-1.5, 0.0, 2.0);
 
 		pane->setVertexArray(coords.get());
 		pane->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::QUADS,0,4));
@@ -297,7 +162,7 @@ osg::ref_ptr< osg::Group > NetworkSystemView::DrawNetwork( void )
 		pane->setTexCoordArray(0,texcoords);
 
 		osg::Vec3Array* normals = new osg::Vec3Array(1);
-		(*normals)[0].set(0.0f,0.0f,1.0f);
+		(*normals)[0].set(1.0f,0.0f,0.0f);
 		pane->setNormalArray(normals);
 		pane->setNormalBinding(osg::Geometry::BIND_OVERALL);
 		
@@ -320,73 +185,62 @@ osg::ref_ptr< osg::Group > NetworkSystemView::DrawNetwork( void )
 		resultPane->addChild(transTextGeode.get());
 		resultPane->addChild(paneGeode.get());
 
-		//add red block id if block .ive file is not found
-		if(loadedModel.get() == NULL)
-			loadedModel = osgDB::readNodeFile("3DIcons/UnsupportedComponent.ive");
-
-		//set the blocks name
-		loadedModel.get()->setName(model->GetModelName());
-
-		//Rotate the 3d comps 90 degrees around X axis
+		//Rotate the 3d comps 180 degrees around X axis
 		//corrects issue with initial model location
 		osg::ref_ptr<osg::AutoTransform> rotatedComp = new osg::AutoTransform();
-		rotatedComp.get()->addChild(loadedModel.get());
-		rotatedComp.get()->setRotation(osg::Quat(osg::DegreesToRadians(90.0), osg::Vec3d(1.0, 0.0, 0.0)));
+		rotatedComp->addChild(loadedModel.get());
+		rotatedComp->setRotation(osg::Quat(osg::DegreesToRadians(180.0), osg::Vec3d(1.0, 0.0, 0.0)));
 
 		//rotate/scale/mirror component
-		osg::ref_ptr<osg::AutoTransform> mirrorComp = new osg::AutoTransform();
-		mirrorComp.get()->addChild(rotatedComp.get());
-	
 		int mirror = model->GetIconMirror();
 		float rotation = model->GetIconRotation();
 		float iconScale = model->GetIconScale();
+
+		//rotate according to iconMirror value
+		osg::ref_ptr<osg::AutoTransform> mirrorComp = new osg::AutoTransform();
+		mirrorComp->addChild(rotatedComp.get());
 		if(mirror > 0 && mirror < 3)
 		{
 			//horizontally
 			if(mirror == 1)
-				mirrorComp.get()->setRotation(osg::Quat(osg::DegreesToRadians(180.0), osg::Vec3d(0.0, 1.0, 0.0)));
+				mirrorComp->setRotation(osg::Quat(osg::DegreesToRadians(180.0), osg::Vec3d(0.0, 0.0, 1.0)));
 			//vertically
 			else
-				mirrorComp.get()->setRotation(osg::Quat(osg::DegreesToRadians(180.0), osg::Vec3d(1.0, 0.0, 0.0)));
+				mirrorComp->setRotation(osg::Quat(osg::DegreesToRadians(180.0), osg::Vec3d(1.0, 0.0, 0.0)));
 		}
 
-		//rotate
+		//rotate according to iconRotation value
 		osg::ref_ptr<osg::AutoTransform> reRotatedComp = new osg::AutoTransform();
-		reRotatedComp.get()->addChild(mirrorComp.get());
-		reRotatedComp.get()->setRotation(osg::Quat(osg::DegreesToRadians(rotation), osg::Vec3d(0.0, 1.0, 0.0)));
-
-		//move the text to the -y
-		//osg::ref_ptr<osg::MatrixTransform> textTrans = new osg::MatrixTransform();
-		//textTrans.get()->addChild(text.get());
-		//textTrans->preMult(osg::Matrix::translate(0.0, -loadedModel.get()->getBound().radius(), 0.0));
+		reRotatedComp->addChild(mirrorComp.get());
+		reRotatedComp->setRotation(osg::Quat(osg::DegreesToRadians(rotation), osg::Vec3d(0.0, 1.0, 0.0)));
 
 		//move the text to the -y
 		osg::ref_ptr<osg::AutoTransform> textTrans = new osg::AutoTransform();
-		textTrans.get()->addChild(resultPane.get());
-		textTrans.get()->setPosition(osg::Vec3d(0.0, loadedModel.get()->getBound().radius(), 0.0));
-		//textTrans.get()->setAutoRotateMode(osg::AutoTransform::ROTATE_TO_SCREEN);
-		textTrans.get()->setRotation(osg::Quat(osg::DegreesToRadians(180.0), osg::Vec3d(1.0, 0.0, 0.0)));
+		textTrans->addChild(resultPane.get());
+		textTrans->setPosition(osg::Vec3d(0.0, 0.0, loadedModel.get()->getBound().radius()));
+		textTrans->setRotation(osg::Quat(osg::DegreesToRadians(180.0), osg::Vec3d(1.0, 0.0, 0.0)));
 
 		//Scale up 3D comps & text
 		osg::ref_ptr<osg::AutoTransform> scale = new osg::AutoTransform;
-		scale.get()->addChild(reRotatedComp.get());
-		scale.get()->addChild(textTrans.get());
-		scale.get()->setScale(5.0f * iconScale);
+		scale->addChild(reRotatedComp.get());
+		//scale.get()->addChild(rotatedComp.get());
+		scale->addChild(textTrans.get());
+		scale->setScale(6.0f * iconScale);
 
 		//translate to comp with name to correct location
-		//osg::ref_ptr<osg::MatrixTransform> mModelTrans = new osg::MatrixTransform();
-		//mModelTrans.get()->addChild(scale.get());
-		////mModelTrans.get()->addChild(loadedModel.get());
-		//mModelTrans.get()->preMult(osg::Matrix::translate(xyPair.first*1.02, xyPair.second*1.05, 0.0));
-		//mModelTrans.get()->setName(model->GetModelName());
-		//loadedModels.get()->addChild(mModelTrans.get());
-		
-		//translate to comp with name to correct location
+		VE_XML::VE_Model::Point * iconLocation = model->GetIconLocation();
+		std::pair<unsigned int, unsigned int> xyPair = iconLocation->GetPoint();
+		std::cout<<"X: "<< xyPair.first <<" Y: " << xyPair.second <<std::endl;
 		osg::ref_ptr<osg::AutoTransform> mModelTrans = new osg::AutoTransform();
-		mModelTrans.get()->addChild(scale.get());
-		mModelTrans.get()->setPosition(osg::Vec3d(xyPair.first*1.02, xyPair.second*1.05, 0.0));
-		mModelTrans.get()->setName(model->GetModelName());
-		loadedModels.get()->addChild(mModelTrans.get());
+		osg::Vec3 center = mModelTrans.get()->getBound().center();
+		//20 and 28 should be replaced with conductor icon width and height respectively
+		//osg::Vec3 centerTrans = osg::Vec3((xyPair.first + 20) - center.x(), (xyPair.second + 28) - center.y(), 0 - center.z());
+		osg::Vec3 centerTrans = osg::Vec3((xyPair.first + 20) - center.x(), 0 - center.z(), (xyPair.second + 22) - center.y());
+		//osg::Vec3 centerTrans = osg::Vec3(xyPair.first + 20, xyPair.second + 25, 0 );
+		mModelTrans->addChild(scale.get());
+		mModelTrans->setPosition(centerTrans);
+		mModelTrans->setName(model->GetModelName());
+		loadedModels->addChild(mModelTrans.get());		
 	}	
 	
 	//Streams	
@@ -405,7 +259,8 @@ osg::ref_ptr< osg::Group > NetworkSystemView::DrawNetwork( void )
 		{
 			std::pair< unsigned int, unsigned int > rawPoint = veNetwork->GetLink( i )->GetLinkPoint( j )->GetPoint();
 			std::cout << "X: " << rawPoint.first << " Y: " << rawPoint.second << std::endl;
-			(*vertices)[j].set(rawPoint.first, rawPoint.second, 0.0);
+			//(*vertices)[j].set(rawPoint.first, rawPoint.second, 0.0);
+			(*vertices)[j].set(rawPoint.first, 0.0, rawPoint.second);
 		}
 		linesGeom->setVertexArray(vertices);
 		linesGeom->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINE_STRIP, 0, numberOfPoints));		linesGeom->setColorBinding(osg::Geometry::BIND_OVERALL);
@@ -415,5 +270,19 @@ osg::ref_ptr< osg::Group > NetworkSystemView::DrawNetwork( void )
 		geode->addDrawable(linesGeom);
 	}
 	loadedModels->addChild(geode);
-	return loadedModels;
+	
+	//rotate the world about the X to normalize the flowsheet
+	osg::ref_ptr<osg::AutoTransform> worldRotate = new osg::AutoTransform();
+	worldRotate->addChild(loadedModels.get());
+	worldRotate->setRotation(osg::Quat(osg::DegreesToRadians(180.0), osg::Vec3d(1.0, 0.0, 0.0)));
+
+	//center the world
+	osg::ref_ptr<osg::AutoTransform> worldTranslate = new osg::AutoTransform();
+	worldTranslate->addChild(worldRotate.get());
+	osg::Vec3 worldCenter = worldRotate.get()->getBound().center();
+	//osg::Vec3 worldTrans = osg::Vec3(0 - worldCenter.x(), 0 - worldCenter.y(), 0 - worldCenter.z());
+	osg::Vec3 worldTrans = osg::Vec3(0 - worldCenter.x(), 0, 0 - worldCenter.z());
+	worldTranslate->setPosition(worldTrans);
+
+	return worldTranslate.get();
 }
