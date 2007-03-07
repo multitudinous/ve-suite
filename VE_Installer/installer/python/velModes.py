79244659a87d731946bf472e5ffc9b81fd2c6078
Doug McCorkle <doug.mccorkle@gmail.com>
2007-03-07 12:19:03 +0000

    correct launcher

--- VE_Installer/installer/python/velModes.py
+++ VE_Installer/installer/python/velModes.py
@@ -11,7 +11,7 @@ JCONF_BASE_PATH = VELAUNCHER_DIR
 if os.path.basename(JCONF_BASE_PATH) == "bin":
     JCONF_BASE_PATH = os.path.dirname(JCONF_BASE_PATH)
 ##Set default Jconf files.
-DEFAULT_JCONF = join(JCONF_BASE_PATH, "share", "vesuite", "stereo_desktop", "desktop.jconf")
+DEFAULT_JCONF = join(JCONF_BASE_PATH, "share", "vesuite","vrj_configs", "stereo_desktop", "desktop.jconf")
 DEFAULT_DEV_JCONF = join(JCONF_BASE_PATH, "..", "..", "share",
                          "stereo_desktop", "desktop.jconf")
 MODE_LIST = ["Desktop", "Tablet", "Computation", "Visualization",
