dda64544e9df5b3e7d420f2d0829b1eccc8173b9
Mike Lem <mikelem@unknown.nil>
2007-03-09 18:40:52 +0000

    Launcher: Added linuxLauncherBuild.sh script, which uses pyinstaller-1.3 to make a Linux velauncher executable.

---
+++ VE_Installer/installer/linuxLauncherBuild.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#This script builds the linux executable for velauncher.py.
+#Run it, then grab the velauncher executable from ${Output_Dir}.
+
+export PyInstaller_Location=/home/vr/Applications/TSVEG/Build_Apps/pyinstaller-1.3
+export Output_Dir=dist
+
+#python ${PyInstaller_Location}/Configure.py
+python ${PyInstaller_Location}/Makespec.py --onefile --out=${Output_Dir} velauncher.py
+python ${PyInstaller_Location}/Build.py ${Output_Dir}/velauncher.spec
