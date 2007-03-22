3ae8e957930619e67b6d24101db06ad5a81f7ed0
Mike Lem <mikelem@unknown.nil>
2007-03-22 18:39:37 +0000

    LinuxConfig: Added desktop files, finetuned configuration scripts.

--- VE_Installer/installer/vesuiteGnomeConfig.sh
+++ VE_Installer/installer/vesuiteGnomeConfig.sh
@@ -6,6 +6,12 @@
 #3. Inserting VE-Suite into the application menu.
 
 #Run as an executable ( ./vesuiteGnomeConfig.sh ), not in a "source" command.
+#Valid Arguments:
+# --user: User-only configuration
+# --global: Global configuration
+# (Nothing): Default configuration
+#           (Global if write-access to /usr/share/, else user-only.)
+
 #To work properly, a path to velauncher.py must be
 #set in PATH in your ~/.cshrc file.
 #After you're finished setting it up, re-login to put the changes into effect.
@@ -13,33 +19,86 @@
 #NOTE: This script only works with Gnome 2.8+ desktop.
 
 #Files
-export Mime_Type_Package_File=VE-Suite.xml
+export Mimetype_Package_File=VE-Suite.xml
 export Desktop_File=vesuite.desktop
-export Icon_File=gnome-mime-application-x-vesuite.png
+export Reboot_File=vesuiteReboot.desktop
+export Shutdown_File=vesuiteShutdown.desktop
+export Wake_File=vesuiteWake.desktop
+export Mimetype_Icon_File=gnome-mime-application-x-vesuite.png
+export Menu_Icon_Source=ve_icon32x32.png
+export Menu_Icon_Destination=ve-suite.png
 #Location of Files
-export Mime_Type_Package_File_Location=$Mime_Type_Package_File
+export Mimetype_Package_File_Location=$Mimetype_Package_File
 export Desktop_File_Location=$Desktop_File
-export Icon_File_Location=installerImages/$Icon_File
-#Directories to Install Files To
-export Mime_Directory=$HOME/.local/share/mime
-export Applications_Directory=$HOME/.local/share/applications
-export Icon_Directory=$HOME/.icons
+export Reboot_File_Location=$Reboot_File
+export Shutdown_File_Location=$Shutdown_File
+export Wake_File_Location=$Wake_File
+export Mimetype_Icon_File_Location=installerImages/$Mimetype_Icon_File
+export Menu_Icon_File_Location=installerImages/$Menu_Icon_Source
+#Set GlobalConfig
+#GlobalConfig determines whether a user-only or global config is made.
+case $1 in
+   --user)
+      #User-only config
+      export GlobalConfig=False
+      ;;
+   --global)
+      #Global config
+      export GlobalConfig=True
+      ;;
+   "")
+      #Default config:
+      #Global if user can write to /usr/share/
+      #otherwise user-only.
+      if [ -w "/usr/share/" ]
+      then
+         export GlobalConfig=True
+      else
+         export GlobalConfig=False
+      fi
+      ;;
+   *)
+      #Invalid arg. Spit out error message and quit.
+      echo "Invalid argument passed."
+      echo "Try '--user' or '--global' to choose the configuration's scope."
+      exit 0
+      ;;
+esac
+
+#Set Install Directories
+if [ $GlobalConfig == "True" ]
+then
+   echo "Setting up global configuration."
+   export Mime_Directory=/usr/share/mime
+   export Applications_Directory=/usr/share/applications
+   export Icon_Directory=/usr/share/icons
+else
+   echo "Setting up user-only configuration."
+   export Mime_Directory=$HOME/.local/share/mime
+   export Applications_Directory=$HOME/.local/share/applications
+   export Icon_Directory=$HOME/.icons
+fi
 
 ##Copy MimeType package & update Mime database
 echo "Installing MIME-type package for VE-Suite..."
 mkdir -p $Mime_Directory/packages
-cp $Mime_Type_Package_File_Location $Mime_Directory/packages/$Mime_Type_Package_File
+cp $Mimetype_Package_File_Location $Mime_Directory/packages/$Mimetype_Package_File
 update-mime-database $Mime_Directory
 
 ##Install Desktop package
 echo "Installing .desktop file for VE-Suite..."
 mkdir -p $Applications_Directory
-desktop-file-install --vendor=VRAC --dir=$Applications_Directory --rebuild-mime-info-cache $Desktop_File_Location
+desktop-file-install --vendor=vrac --dir=$Applications_Directory --rebuild-mime-info-cache $Desktop_File_Location
+desktop-file-install --vendor=vrac --dir=$Applications_Directory --rebuild-mime-info-cache $Reboot_File_Location
+desktop-file-install --vendor=vrac --dir=$Applications_Directory --rebuild-mime-info-cache $Shutdown_File_Location
+desktop-file-install --vendor=vrac --dir=$Applications_Directory --rebuild-mime-info-cache $Wake_File_Location
 
 ##Install icon file
 echo "Installing icon for .ves files..."
-mkdir -p $Icon_Directory
-cp $Icon_File_Location $Icon_Directory/$Icon_File
+mkdir -p $Icon_Directory/hicolor/48x48/mimetypes
+mkdir -p $Icon_Directory/hicolor/32x32/apps
+cp $Mimetype_Icon_File_Location $Icon_Directory/hicolor/48x48/mimetypes/$Mimetype_Icon_File
+cp $Menu_Icon_File_Location $Icon_Directory/hicolor/32x32/apps/$Menu_Icon_Destination
 
 echo "Done."
 echo "Re-login for settings to take effect."
--- VE_Installer/installer/vesuiteKdeConfig.sh
+++ VE_Installer/installer/vesuiteKdeConfig.sh
@@ -5,25 +5,79 @@
 #2. Giving .ves files the VE-Suite icon.
 #3. Inserting VE-Suite into the application menu.
 
-#Run as an executable ( ./vesuiteKdeConfig.sh ), not in a "source" command.
+#Run as an executable ( ./vesuiteGnomeConfig.sh ), not in a "source" command.
+#Valid Arguments:
+# --user: User-only configuration
+# --global: Global configuration
+# (Nothing): Default configuration
+#           (Global if write-access to /usr/share/, else user-only.)
+
 #To work properly, a path to velauncher.py must be
 #set in PATH in your ~/.cshrc file.
 #After you're finished setting it up, re-login to put the changes into effect.
 
-#NOTE: This script only works with KDE desktop.
+#NOTE: This script only works with Gnome 2.8+ desktop.
 
 #Files
 export Mime_Type_Package_File=ves.desktop
 export Desktop_File=vesuite.desktop
-export Icon_File=gnome-mime-application-x-vesuite.png
+export Reboot_File=vesuiteReboot.desktop
+export Shutdown_File=vesuiteShutdown.desktop
+export Wake_File=vesuiteWake.desktop
+export Mimetype_Icon_File=gnome-mime-application-x-vesuite.png
+export Menu_Icon_Source=ve_icon32x32.png
+export Menu_Icon_Destination=ve-suite.png
 #Location of Files
 export Mime_Type_Package_File_Location=$Mime_Type_Package_File
 export Desktop_File_Location=$Desktop_File
-export Icon_File_Location=installerImages/$Icon_File
-#Directories to Install Files To
-export Mime_Directory=$HOME/.kde/share/mimelnk
-export Applications_Directory=$HOME/.kde/share/applnk
-export Icon_Directory=$HOME/.kde/share/icons
+export Reboot_File_Location=$Reboot_File
+export Shutdown_File_Location=$Shutdown_File
+export Wake_File_Location=$Wake_File
+export Mimetype_Icon_File_Location=installerImages/$Mimetype_Icon_File
+export Menu_Icon_File_Location=installerImages/$Menu_Icon_Source
+#Set GlobalConfig
+#GlobalConfig determines whether a user-only or global config is made.
+case $1 in
+   --user)
+      #User-only config
+      export GlobalConfig=False
+      ;;
+   --global)
+      #Global config
+      export GlobalConfig=True
+      ;;
+   "")
+      #Default config:
+      #Global if user can write to /usr/share/
+      #otherwise user-only.
+      if [ -w "/usr/share/" ]
+      then
+         export GlobalConfig=True
+      else
+         export GlobalConfig=False
+      fi
+      ;;
+   *)
+      #Invalid arg. Spit out error message and quit.
+      echo "Invalid argument passed."
+      echo "Try '--user' or '--global' to choose the configuration's scope."
+      exit 0
+      ;;
+esac
+
+#Set Install Directories
+if [ $GlobalConfig == "True" ]
+then
+   echo "Setting up global configuration."
+   export Mime_Directory=/usr/share/mimelnk
+   export Applications_Directory=/usr/share/applnk
+   export Icon_Directory=/usr/share/icons
+else
+   echo "Setting up user-only configuration."
+   export Mime_Directory=$HOME/.kde/share/mimelnk
+   export Applications_Directory=$HOME/.kde/share/applnk
+   export Icon_Directory=$HOME/.kde/share/icons
+fi
 
 ##Copy MimeType package & update Mime database
 echo "Installing MIME-type package for VE-Suite..."
@@ -34,12 +88,17 @@ cp $Mime_Type_Package_File_Location $Mime_Directory/application/$Mime_Type_Packa
 ##Install Desktop package
 echo "Installing .desktop file for VE-Suite..."
 mkdir -p $Applications_Directory
-#desktop-file-install --vendor=VRAC --dir=$Applications_Directory --rebuild-mime-info-cache $Desktop_File_Location
+desktop-file-install --vendor=vrac --dir=$Applications_Directory --rebuild-mime-info-cache $Desktop_File_Location
+desktop-file-install --vendor=vrac --dir=$Applications_Directory --rebuild-mime-info-cache $Reboot_File_Location
+desktop-file-install --vendor=vrac --dir=$Applications_Directory --rebuild-mime-info-cache $Shutdown_File_Location
+desktop-file-install --vendor=vrac --dir=$Applications_Directory --rebuild-mime-info-cache $Wake_File_Location
 
 ##Install icon file
 echo "Installing icon for .ves files..."
-mkdir -p $Icon_Directory
-cp $Icon_File_Location $Icon_Directory/vesuite.png
+mkdir -p $Icon_Directory/hicolor/48x48/mimetypes
+mkdir -p $Icon_Directory/hicolor/32x32/apps
+cp $Mimetype_Icon_File_Location $Icon_Directory/hicolor/48x48/mimetypes/$Mimetype_Icon_File
+cp $Menu_Icon_File_Location $Icon_Directory/hicolor/32x32/apps/$Menu_Icon_Destination
 
 echo "Done."
 echo "Re-login for settings to take effect."
