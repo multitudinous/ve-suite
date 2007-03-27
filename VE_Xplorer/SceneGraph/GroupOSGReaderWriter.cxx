04bacba8e95e14fa6aef4fe17ef25703c055a0dd
Doug McCorkle <doug.mccorkle@gmail.com>
2007-03-27 14:32:41 +0000

    added separate reader writer classes for the osg nodes

---
+++ VE_Xplorer/SceneGraph/GroupOSGReaderWriter.cxx
@@ -0,0 +1,98 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2007-03-26 17:31:03 -0500 (Mon, 26 Mar 2007) $
+ * Version:       $Rev: 7216 $
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#include "VE_Xplorer/SceneGraph/Group.h"
+
+#include <osg/Group>
+#include <osg/Node>
+#include <osgDB/Registry>
+#include <osgDB/Input>
+#include <osgDB/Output>
+
+//C/C++ Libraries
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace VE_SceneGraph;
+// forward declare functions to use later.
+bool VEGroup_readLocalData(osg::Object& obj, osgDB::Input& fr);
+bool VEGroup_writeLocalData(const osg::Object& obj, osgDB::Output& fw);
+
+// register the read and write functions with the osgDB::Registry.
+osgDB::RegisterDotOsgWrapperProxy ve_GroupProxy
+(
+    new VE_SceneGraph::Group,
+    "Group",
+    "Object Node VE_SceneGraph::Group",
+    &VEGroup_readLocalData,
+    &VEGroup_writeLocalData
+);
+//////////////////////////////////////////////////////////////
+bool VEGroup_readLocalData(osg::Object& obj, osgDB::Input& fr)
+{
+    bool iteratorAdvanced = false;
+
+    VE_SceneGraph::Group& group = static_cast<VE_SceneGraph::Group&>(obj);
+
+    int num_children;
+    if (fr[0].matchWord("num_children") &&
+        fr[1].getInt(num_children))
+    {
+        // could allocate space for children here...
+        fr+=2;
+        iteratorAdvanced = true;
+    }
+
+    osg::Node* node = NULL;
+    while((node=fr.readNode())!=NULL)
+    {
+        group.addChild(node);
+        iteratorAdvanced = true;
+    }
+
+    return iteratorAdvanced;
+}
+
+///////////////////////////////////////////////////////////
+bool VEGroup_writeLocalData(const osg::Object& obj, osgDB::Output& fw)
+{
+   ///call the base class writer
+   const VE_SceneGraph::Group& group = static_cast<const VE_SceneGraph::Group&>(obj);
+    //fw.writeObject(group);
+    fw.indent() << "num_children " << group.getNumChildren() << std::endl;
+    for(unsigned int i=0;i<group.getNumChildren();++i)
+    {
+        fw.writeObject(*group.getChild(i));
+    }
+    return true;
+}
---
+++ VE_Xplorer/SceneGraph/SwitchOSGReaderWriter.cxx
@@ -0,0 +1,161 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+*
+* VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+*
+* Original Development Team:
+*   - ISU's Thermal Systems Virtual Engineering Group,
+*     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+*   - Reaction Engineering International, www.reaction-eng.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+* Boston, MA 02111-1307, USA.
+*
+* -----------------------------------------------------------------
+* Date modified: $Date: 2007-03-26 17:31:03 -0500 (Mon, 26 Mar 2007) $
+* Version:       $Rev: 7216 $
+* Author:        $Author$
+* Id:            $Id$
+* -----------------------------------------------------------------
+*
+*************** <auto-copyright.pl END do not edit this line> ***************/
+#include "VE_Xplorer/SceneGraph/Switch.h"
+
+//C/C++ Libraries
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+#include <osgDB/Registry>
+#include <osgDB/Input>
+#include <osgDB/Output>
+
+using namespace VE_SceneGraph;
+// forward declare functions to use later.
+bool VESwitch_readLocalData(osg::Object& obj, osgDB::Input& fr);
+bool VESwitch_writeLocalData(const osg::Object& obj, osgDB::Output& fw);
+
+// register the read and write functions with the osgDB::Registry.
+osgDB::RegisterDotOsgWrapperProxy g_SwitchProxy
+(
+    new VE_SceneGraph::Switch,
+    "Switch",
+    "Object Node VE_SceneGraph::Switch Group ",
+    &VESwitch_readLocalData,
+    &VESwitch_writeLocalData
+);
+///////////////////////////////////////////////////////////////
+bool VESwitch_readLocalData(osg::Object& obj, osgDB::Input& fr)
+{
+    bool iteratorAdvanced = false;
+
+    VE_SceneGraph::Switch& sw = static_cast<VE_SceneGraph::Switch&>(obj);
+
+    if (fr.matchSequence("value"))
+    {
+        if (fr[1].matchWord("ALL_CHILDREN_ON"))
+        {
+            sw.setAllChildrenOn();
+            iteratorAdvanced = true;
+            fr+=2;
+        }
+        else if (fr[1].matchWord("ALL_CHILDREN_OFF"))
+        {
+            sw.setAllChildrenOff();
+            iteratorAdvanced = true;
+            fr+=2;
+        }
+        else if (fr[1].isInt())
+        {
+            unsigned int value;
+            fr[1].getUInt(value);
+            sw.setSingleChildOn(value);
+            iteratorAdvanced = true;
+            fr+=2;
+        }
+    }
+
+    if (fr[0].matchWord("NewChildDefaultValue"))
+    {
+        if (fr[1].matchWord("TRUE")) 
+        {
+            sw.setNewChildDefaultValue(true);
+            iteratorAdvanced = true;
+            fr += 2;
+        }
+        else if (fr[1].matchWord("FALSE"))
+        {
+            sw.setNewChildDefaultValue(false);
+            iteratorAdvanced = true;
+            fr += 2;
+        }
+        else if (fr[1].isInt())
+        {
+            int value;
+            fr[1].getInt(value);
+            sw.setNewChildDefaultValue(value!=0);
+            iteratorAdvanced = true;
+            fr += 2;
+        }
+    }
+
+    if (fr.matchSequence("ValueList {"))
+    {
+        int entry = fr[0].getNoNestedBrackets();
+
+        // move inside the brakets.
+        fr += 2;
+
+        unsigned int pos=0;
+        while (!fr.eof() && fr[0].getNoNestedBrackets()>entry)
+        {
+            int value;
+            if (fr[0].getInt(value))
+            {
+                sw.setValue(pos,value!=0);
+                ++pos;
+            }
+            ++fr;
+        }
+
+        ++fr;
+        
+        iteratorAdvanced = true;
+        
+    }
+
+    return iteratorAdvanced;
+}
+///////////////////////////////////////////////////////////
+bool VESwitch_writeLocalData(const osg::Object& obj, osgDB::Output& fw)
+{
+   const VE_SceneGraph::Switch& sw = static_cast<const VE_SceneGraph::Switch&>(obj);
+   //fw.writeObject(sw);
+
+    fw.indent()<<"NewChildDefaultValue "<<sw.getNewChildDefaultValue()<<std::endl;
+
+    fw.indent()<<"ValueList {"<< std::endl;
+    fw.moveIn();
+    const Switch::ValueList& values = sw.getValueList();
+    for(Switch::ValueList::const_iterator itr=values.begin();
+        itr!=values.end();
+        ++itr)
+    {
+        fw.indent()<<*itr<<std::endl;
+    }
+    fw.moveOut();
+    fw.indent()<<"}"<< std::endl;
+
+    return true;
+}
