48b80783e99f25801aa9bf6e1214be7414f722f3
Biv <biv@unknown.nil>
2007-03-27 16:15:17 +0000

    fixing windows build

--- VE_Installer/setup.bat
+++ VE_Installer/setup.bat
@@ -3,16 +3,17 @@ REM Edit this file to reflect your local environment for VE_Suite
 set VE_SUITE_HOME=C:\devEnv\VE_Suite_1.0
 set VE_INSTALL_DIR=C:\devEnv\VE_Suite_1.0
 
-set VJ_BASE_DIR=C:\devEnv\VES.1.0-Deps\VRJuggler2.0.3-1
+REM set VJ_BASE_DIR=C:\devEnv\VES.1.0-Deps\vrjuggler-2.0-svn\vrjuggler-2.0.3-win32-vc80
 set VJ_DEPS_DIR=C:\devEnv\VES.1.0-Deps\VRJuggler2.0.3-1
+set VJ_BASE_DIR=C:\devEnv\VES.1.0-Deps\VRJuggler2.0.3-1
 REM set VJ_BASE_DIR=C:\devEnv\VES.1.0-Deps\vrjuggler-2.0.1-win32-vc80
 REM set VJ_DEPS_DIR=C:\devEnv\VES.1.0-Deps\vrjuggler-2.0.1-win32-vc80-deps
 REM set VTK_HOME=C:\devEnv\VES.1.0-Deps\vtk-5.0.0_Install_vc8
 set VTK_HOME=C:\devEnv\VES.1.0-Deps\vtk_cvs\vtk_vc80_install
 REM set WX_HOME=C:\WxWidgets_2.8.0_Pre-Compile_vc8.0
 REM set WX_HOME=C:\WxWidgets_2.8.0_Pre-Compile_vc8.0
-set WX_HOME=C:\devEnv\VES.1.0-deps\wxWidgets-2.8.0
-set WX_ROOT=C:\devEnv\VES.1.0-Deps\wxWidgets-2.8.0
+set WX_HOME=C:\devEnv\VES.1.0-deps\wxWidgets-2.8.3
+set WX_ROOT=C:\devEnv\VES.1.0-Deps\wxWidgets-2.8.3
 set JAVA_HOME=C:\j2sdk1.4.2_06
 set ACE_ROOT=C:\devEnv\VES.1.0-Deps\ACE_wrappers
 REM set ACE_ROOT=C:\ACETAO_5.5.1_Pre-Compile_vc8.0
@@ -71,7 +72,7 @@ set OMNINAMES_LOGDIR=%VE_SUITE_HOME%\VE_Installer
 REM Python build environment variables
 set PYTHONPATH=%VJ_DEPS_DIR%\lib\python
 
-set Path=%VJ_DEPS_DIR%\bin;%VJ_DEPS_DIR%\lib;%VJ_BASE_DIR%\lib;%Path%
+set Path=%VJ_BASE_DIR%\lib;%VJ_DEPS_DIR%\bin;%VJ_DEPS_DIR%\lib;%Path%
 set Path=%VE_SUITE_HOME%\bin;%VTK_HOME%\bin;%WX_HOME%\lib\vc_dll;%Path%
 set Path=%ACE_ROOT%\bin;%ACE_ROOT%\lib;%XERCESCROOT%\Build\Win32\VC7.1\Debug;%Path%
 set Path=%TAO_ROOT%\orbsvcs\Naming_Service;%Path%
