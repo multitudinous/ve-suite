a085fa8ce46514fff1a74d9f141a6c8fbae52a37
Doug McCorkle <doug.mccorkle@gmail.com>
2007-04-20 20:56:10 +0000

    actually put the code in the right spot...

--- VE_Xplorer/SceneGraph/DCS.cxx
+++ VE_Xplorer/SceneGraph/DCS.cxx
@@ -183,18 +183,6 @@ void DCS::SetTranslationArray( std::vector<double> array )
    this->_dcs->setTrans( this->_translation[0], this->_translation[1], this->_translation[2] );
 #elif _OSG           
    this->setPosition( osg::Vec3d( array[0], array[1], array[2]) );
-   
-   ///Do this so that the normals will not be affected by the scaling applied 
-   ///by the user - see osg post one April 19, 2007
-   if ( array[0] != 1 )
-   {
-      this->getOrCreateStateSet()->setMode( GL_NORMALIZE, osg::StateAttribute::ON );
-   }
-   else
-   {
-      this->getOrCreateStateSet()->setMode( GL_NORMALIZE, osg::StateAttribute::OFF );
-   }
-   
 #elif _OPENSG
 #endif
    UpdatePhysicsTransform();
@@ -229,6 +217,18 @@ void DCS::SetScaleArray( std::vector<double> array )
    this->_dcs->setScale( array[0], array[1], array[2] );
 #elif _OSG
    this->setScale( osg::Vec3d( array[0], array[1], array[2]) );
+   
+   ///Do this so that the normals will not be affected by the scaling applied 
+   ///by the user - see osg post one April 19, 2007
+   if ( array[0] != 1 )
+   {
+      this->getOrCreateStateSet()->setMode( GL_NORMALIZE, osg::StateAttribute::ON );
+   }
+   else
+   {
+      this->getOrCreateStateSet()->setMode( GL_NORMALIZE, osg::StateAttribute::OFF );
+   }
+   
 #elif _OPENSG
 #endif
    UpdatePhysicsTransform();
