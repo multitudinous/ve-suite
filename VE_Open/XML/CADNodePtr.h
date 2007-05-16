260834dedcd0f8164b842d501dacdb7be87a4570
Biv <biv@unknown.nil>
2007-05-16 14:29:26 +0000

    smart ptr headers

---
+++ VE_Open/XML/CADNodePtr.h
@@ -0,0 +1,57 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+*
+* VE-Suite is (C) Copyright 1998-2006 by Iowa State University
+*
+* Original Development Team:
+*   - ISU's Thermal Systems Virtual Engineering Group,
+*     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+*   - Reaction Engineering International, www.reaction-eng.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+* Boston, MA 02111-1307, USA.
+*
+* -----------------------------------------------------------------
+* Date modified: $Date: 2007-02-10 20:08:51 -0600 (Sat, 10 Feb 2007) $
+* Version:       $Rev: 6862 $
+* Author:        $Author: mccdo $
+* Id:            $Id: ProgramPtr.h 7419 2007-04-21 13:06:22Z mccdo $
+* -----------------------------------------------------------------
+*
+*************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef _VE_CAD_PTR_H_
+#define _VE_CAD_PTR_H_
+
+#include <loki/SmartPtr.h>
+
+/**
+ * \file
+ *
+ * Include this file to get a forward declaration of the pointer type
+ * VE_XML::VE_CAD::CADNode.  To get the full 
+ * declaration of VE_XML::VE_CAD::CADNode
+ * VE_Open/XML/CAD/CADNode.h must be included, too.
+ */
+
+namespace VE_XML
+{
+namespace VE_CAD
+{
+   class CADNode;
+   /// Typedef for a SmartPtr type for the CADNode.
+   typedef Loki::SmartPtrDef<CADNode>::type CADNodePtr;
+}
+}
+#endif
+
