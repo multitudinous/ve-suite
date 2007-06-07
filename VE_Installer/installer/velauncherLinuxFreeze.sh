4bc05b68ccf729037ad1289286af85ea07d2c363
Micro Z80 <microz80@unknown.nil>
2007-06-07 06:57:56 +0000

    commit test

--- VE_Installer/installer/velauncherLinuxFreeze.sh
+++ VE_Installer/installer/velauncherLinuxFreeze.sh
@@ -5,12 +5,14 @@
 #OUTPUT: A velauncher executable in ${Output_Dir}.
 
 #Location of PyInstaller
-export PyInstaller_Location=pyinstaller-1.3
+export PyInstaller_Location=/home/vr/Applications/TSVEG/Build_Apps/pyinstaller-1.3
 #Directory the executable will be written to.
 export Output_Dir=dist
 
+#Configures PyInstaller
+python ${PyInstaller_Location}/Configure.py
 #Makes velauncher's spec file.
-python ${PyInstaller_Location}/Makespec.py -p ${VE_SUITE_HOME}/VE_Installer/installer/python --onefile --out=${Output_Dir} velauncher.py
+python ${PyInstaller_Location}/Makespec.py --onefile --out=${Output_Dir} velauncher.py
 #Builds the executable
 python ${PyInstaller_Location}/Build.py ${Output_Dir}/velauncher.spec
 #Afterwards, grab the executable from ${Output_Dir} and place it in VE_Suite's bin directory.
