0fd7d5532676d9428ce1132b7303545c09d1b610
Micro Z80 <microz80@unknown.nil>
2007-06-13 14:25:16 +0000

    Restored Shutdown window

--- VE_Installer/installer/python/velServerKillWindow.py
+++ VE_Installer/installer/python/velServerKillWindow.py
@@ -1,5 +1,5 @@
 """VE-Launcher's Name Server Shutdown window"""
-#import wx
+import wx
 import os, sys, string, thread
 from velBase import *
 from sys import exc_info
@@ -8,8 +8,7 @@ from time import sleep
 if windows:
     import win32api
 
-class ServerKillWindow:
-#class ServerKillWindow(wx.Frame):
+class ServerKillWindow(wx.Frame):
     """A window to shutdown the Nameserver after launch.
     Functions:
         __init__(pids, [parent, title])
@@ -17,30 +16,29 @@ class ServerKillWindow:
         OnClose(event)"""
     def __init__(self, pids, conduct_Pid, parent = None, title = "Shutdown Name Server"):
         """Creates the Server Shutdown Window"""
-        #wx.Frame.__init__(self, parent, wx.ID_ANY, title, wx.Point(0, 0),
-        #                  style = wx.DEFAULT_FRAME_STYLE &
-        #                  ~(wx.RESIZE_BORDER | wx.CLOSE_BOX | wx.MAXIMIZE_BOX))
+        wx.Frame.__init__(self, parent, wx.ID_ANY, title, wx.Point(0, 0),
+                          style = wx.DEFAULT_FRAME_STYLE &
+                          ~(wx.RESIZE_BORDER | wx.CLOSE_BOX | wx.MAXIMIZE_BOX))
         self.pids = pids
         self.c_Pid = str(conduct_Pid[0])
-        #lblMsg = wx.StaticText(self, -1, "After you're done with VE-Suite,\n"+\
-        #                                 "press the button below to shutdown\n"+\
-        #                                 "the Name Server.")
-        #bDone = wx.Button(self, -1, "Shutdown Name Server")
-        #self.Bind(wx.EVT_BUTTON, self.KillNameserver, bDone)
-        #rowSizer = wx.BoxSizer(wx.VERTICAL)
-        #border = 10
-        #rowSizer.Add(lblMsg, 2, wx.ALL, border)
-        #rowSizer.Add(bDone, 1, wx.EXPAND)
-        #rowSizer.SetMinSize(KILL_WINDOW_SIZE)
-        #rowSizer.SetSizeHints(self)
-        #Style(self)
-        #self.SetSizer(rowSizer)
-        #self.SetSize(KILL_WINDOW_SIZE)
-        #self.CentreOnScreen()
-        #self.Show()
-        self.runWhile()
-        #thread.start_new_thread(self.test, ())
-	
+        lblMsg = wx.StaticText(self, -1, "After you're done with VE-Suite,\n"+\
+                                         "press the button below to shutdown\n"+\
+                                         "the Name Server.")
+        bDone = wx.Button(self, -1, "Shutdown Name Server")
+        self.Bind(wx.EVT_BUTTON, self.KillNameserver, bDone)
+        rowSizer = wx.BoxSizer(wx.VERTICAL)
+        border = 10
+        rowSizer.Add(lblMsg, 2, wx.ALL, border)
+        rowSizer.Add(bDone, 1, wx.EXPAND)
+        rowSizer.SetMinSize(KILL_WINDOW_SIZE)
+        rowSizer.SetSizeHints(self)
+        Style(self)
+        self.SetSizer(rowSizer)
+        self.SetSize(KILL_WINDOW_SIZE)
+        self.CentreOnScreen()
+        self.Show()
+        #self.runWhile()
+    """	
     def runWhile(self):
 	if windows:
 	   #pslist is precompiled dos systemtool (pslist.exe) which needs to be distributed as a dependency
@@ -48,14 +46,7 @@ class ServerKillWindow:
 	   while (len(ps) >= 20):
 	       ps = os.popen("pslist " + self.c_Pid).read().split()
 	       sleep(2)
-           """
-           getWinPids = velSearchWinPid.pyperf()
-           winPids = getWinPids.procids()
 
-           while self.hasPid(winPids):
-              winPids = getWinPids.procids()
-              sleep(2)
-           """  
            PROCESS_TERMINATE = 1
            for pid in self.pids:
                try:
@@ -75,8 +66,7 @@ class ServerKillWindow:
            for pid in self.pids:
                killArray.append(str(pid))
            Popen(killArray)
-         
-    """	
+    """     
     def KillNameserver(self, event):
         #Shutdown any Nameservers running on this computer.
         if windows:
@@ -100,4 +90,3 @@ class ServerKillWindow:
         #Closes Server Shutdown Window
         self.Hide()
         self.Destroy()
-    """		
