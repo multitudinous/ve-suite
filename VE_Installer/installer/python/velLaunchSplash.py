f4b495e4a50b72f864eae2d8805b46bd549a9c9c
Micro Z80 <microz80@unknown.nil>
2007-06-13 19:43:55 +0000

    Back to normal

--- VE_Installer/installer/python/velLaunchSplash.py
+++ VE_Installer/installer/python/velLaunchSplash.py
@@ -5,12 +5,17 @@ from velBase import *
 
 class LaunchSplash(wx.SplashScreen):
     def __init__(self):
-        image = wx.Bitmap(SPLASH_IMAGE, wx.BITMAP_TYPE_XPM)
-        wx.SplashScreen.__init__(self, image,
-                                 wx.SPLASH_CENTRE_ON_SCREEN | wx.SPLASH_TIMEOUT,
-                                 SPLASH_TIME, None, -1)
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
+        try:
+            image = wx.Bitmap(SPLASH_IMAGE, wx.BITMAP_TYPE_XPM)
+            wx.SplashScreen.__init__(self, image,
+                                     wx.SPLASH_CENTRE_ON_SCREEN | wx.SPLASH_TIMEOUT,
+                                     SPLASH_TIME, None, -1)
+            self.Bind(wx.EVT_CLOSE, self.OnClose)
+	except:
+	    pass
 
+        #wx.Yield() 
+	
     def OnClose(self, event):
         self.Hide()
         self.Destroy()
