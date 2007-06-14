acf02ec6be81eb57aad0a4d3c10dc0c226796cf5
Mike Lem <mikelem@unknown.nil>
2007-06-14 14:39:26 +0000

    Launcher: Copyrights added, directory dialog problem fixed.

--- VE_Installer/installer/python/velDependencies.py
+++ VE_Installer/installer/python/velDependencies.py
@@ -1,3 +1,35 @@
+# /*************** <auto-copyright.pl BEGIN do not edit this line> *************
+# *
+# * VE-Suite is (C) Copyright 1998-2007 by Iowa State University
+# *
+# * Original Development Team:
+# *   - ISU's Thermal Systems Virtual Engineering Group,
+# *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+# *   - Reaction Engineering International, www.reaction-eng.com
+# *
+# * This library is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU Library General Public
+# * License as published by the Free Software Foundation; either
+# * version 2 of the License, or (at your option) any later version.
+# *
+# * This library is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# * Library General Public License for more details.
+# *
+# * You should have received a copy of the GNU Library General Public
+# * License along with this library; if not, write to the
+# * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# * Boston, MA 02111-1307, USA.
+# *
+# * -----------------------------------------------------------------
+# * Date modified: $Date$
+# * Version:       $Rev$
+# * Author:        $Author$
+# * Id:            $Id$
+# * -----------------------------------------------------------------
+# *
+# *************** <auto-copyright.pl END do not edit this line> **************
 """Handles checking and choosing the Dependencies folder for VE-Launcher.
 
 Used only for Windows now. See velDepsWindow for Linux side.
@@ -113,7 +145,7 @@ def Get(parent):
     dlg = wx.DirDialog(None,
                        "Choose the VE Dependencies directory:",
                        searchDir,
-                       style=wx.DD_DEFAULT_STYLE)
+                       style=wx.DD_DEFAULT_STYLE | ~wx.DD_NEW_DIR_BUTTON)
     if dlg.ShowModal() == wx.ID_OK:
         ##If a directory's chosen, exit the loop and return it.
         searchDir = dlg.GetPath()
--- VE_Installer/installer/python/velDepsWindow.py
+++ VE_Installer/installer/python/velDepsWindow.py
@@ -1,3 +1,35 @@
+# /*************** <auto-copyright.pl BEGIN do not edit this line> *************
+# *
+# * VE-Suite is (C) Copyright 1998-2007 by Iowa State University
+# *
+# * Original Development Team:
+# *   - ISU's Thermal Systems Virtual Engineering Group,
+# *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+# *   - Reaction Engineering International, www.reaction-eng.com
+# *
+# * This library is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU Library General Public
+# * License as published by the Free Software Foundation; either
+# * version 2 of the License, or (at your option) any later version.
+# *
+# * This library is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# * Library General Public License for more details.
+# *
+# * You should have received a copy of the GNU Library General Public
+# * License along with this library; if not, write to the
+# * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# * Boston, MA 02111-1307, USA.
+# *
+# * -----------------------------------------------------------------
+# * Date modified: $Date$
+# * Version:       $Rev$
+# * Author:        $Author$
+# * Id:            $Id$
+# * -----------------------------------------------------------------
+# *
+# *************** <auto-copyright.pl END do not edit this line> **************
 """VE-Launcher's Linux Dependencies window.
 
 Non-Windows only. See velDependencies for Windows dependency code."""
@@ -149,7 +181,7 @@ class DepsWindow(wx.Dialog):
         dlg = wx.DirDialog(None,
                            "Choose the dependency's directory:",
                            defaultDirectory,
-                           style=wx.DD_DEFAULT_STYLE)
+                           style=wx.DD_DEFAULT_STYLE | ~wx.DD_NEW_DIR_BUTTON)
         if dlg.ShowModal() == wx.ID_OK:
             location = dlg.GetPath()
             dlg.Destroy()
@@ -181,7 +213,7 @@ class DepsWindow(wx.Dialog):
         dlg = wx.DirDialog(None,
                            "Choose VR-Juggler's directory:",
                            jugglerDirStart,
-                           style=wx.DD_DEFAULT_STYLE)
+                           style=wx.DD_DEFAULT_STYLE | ~wx.DD_NEW_DIR_BUTTON)
         if dlg.ShowModal() == wx.ID_OK:
             location = dlg.GetPath()
             dlg.Destroy()
