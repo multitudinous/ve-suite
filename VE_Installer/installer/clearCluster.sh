0cfe8ae1f5cf790aa51aea62e28d74c61bab46d2
Doug McCorkle <doug.mccorkle@gmail.com>
2007-08-23 17:20:36 +0000

    added clearcluster script

---
+++ VE_Installer/installer/clearCluster.sh
@@ -0,0 +1,8 @@
+ssh rockarwkviz4 "killall project_tao_osg_vep"
+ssh rockarwkviz3 "killall project_tao_osg_vep"
+ssh rockarwkviz2 "killall project_tao_osg_vep"
+ssh rockarwkviz1 "killall project_tao_osg_vep"
+killall project_tao_osg_vep
+killall Exe_server
+killall WinClient
+killall Naming_Service
