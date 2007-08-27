188fc35e43193b40efae4ae1cd80cda5237fb0c0
Biv <biv@unknown.nil>
2007-08-27 14:33:35 +0000

    Adding DataObjectHandler class for vtkdataobjects

---
+++ VE_Xplorer/Utilities/DataObjectHandler.cxx
@@ -0,0 +1,163 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2007 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2007-06-15 11:06:13 -0500 (Fri, 15 Jun 2007) $
+ * Version:       $Rev: 8206 $
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#include "VE_Xplorer/Utilities/DataObjectHandler.h"
+
+#include <vtkDataSet.h>
+#include <vtkDataObject.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkMultiGroupDataSet.h>
+#include <vtkMultiGroupDataIterator.h>
+#include <vtkCellDataToPointData.h>
+#include <vtkCellData.h>
+#include <vtkPointData.h>
+#include <iostream>
+using namespace VE_Util;
+
+//////////////////////////////////////
+DataObjectHandler::DataObjectHandler()
+{
+    m_datasetOperator = 0;
+}
+///////////////////////////////////////
+DataObjectHandler::~DataObjectHandler()
+{
+    if(m_datasetOperator)
+    {
+        delete m_datasetOperator;
+	    m_datasetOperator = 0;
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+void DataObjectHandler::OperateOnAllDatasetsInObject(vtkDataObject* dataObject)
+{
+    /*if(m_datasetOperator)
+    {
+        std::cout<<"No operator defined for datasets!!!"<<std::endl;
+        std::cout<<"DataObjectHandler::operateOnAllDatasetsInObject."<<std::endl;
+	    return;
+    }*/
+    unsigned int numDatasets = 0;
+    vtkDataSet* currentDataset = 0;
+    if(dataObject->IsA("vtkMultiGroupDataSet"))
+    {
+        try
+	    {
+            vtkMultiGroupDataSet* mgd = dynamic_cast<vtkMultiGroupDataSet*>(dataObject);
+            unsigned int nGroups = mgd->GetNumberOfGroups();
+            unsigned int nDatasetsInGroup = 0;
+            vtkMultiGroupDataIterator* mgdIterator = vtkMultiGroupDataIterator::New();
+            mgdIterator->SetDataSet(mgd);
+            ///For traversal of nested multigroupdatasets
+            mgdIterator->VisitOnlyLeavesOn();
+            mgdIterator->GoToFirstItem();
+
+            while(!mgdIterator->IsDoneWithTraversal())
+            {
+                currentDataset = dynamic_cast<vtkDataSet*>(mgdIterator->GetCurrentDataObject());
+                _convertCellDataToPointData(currentDataset);
+				if(m_datasetOperator)
+				{
+                    m_datasetOperator->OperateOnDataset(currentDataset);
+				}
+				mgdIterator->GoToNextItem();   
+            }
+            if(mgdIterator)
+            {
+                mgdIterator->Delete();
+                mgdIterator = 0;
+            }
+        }
+        catch(...)
+        {
+            std::cout<<"Invalid Dataset: "<<dataObject->GetClassName()<<std::endl;
+        }
+    }
+    else //Assume this is a regular vtkdataset
+    {
+        currentDataset = dynamic_cast<vtkDataSet*>(dataObject);
+        _convertCellDataToPointData(currentDataset);
+		if(m_datasetOperator)
+		{
+            m_datasetOperator->OperateOnDataset(currentDataset);
+		}
+    }
+
+}
+////////////////////////////////////////////////////////////////////////////////////////////
+void DataObjectHandler::_convertCellDataToPointData(vtkDataSet* dataSet)
+{
+    ///either cast it to a dataset or iterate over all datasets
+    unsigned int numPtDataArrays = dataSet->GetPointData()
+                                        ->GetNumberOfArrays();
+    /*vprDEBUG(vesDBG,1) << "|\tnumPtDataArrays = " << numPtDataArrays
+                          << std::endl << vprDEBUG_FLUSH;
+*/
+    unsigned int numCellArrays = dataSet->GetCellData()->GetNumberOfArrays();
+  /*  vprDEBUG(vesDBG,1) << "|\tnumCellArrays = " << numCellArrays
+                          << std::endl << vprDEBUG_FLUSH;
+*/
+    if ( numCellArrays > 0 && numPtDataArrays == 0 )
+    {
+        std::cout <<"|\tThe dataset has no point data -- "
+                  << "will try to convert cell data to point data" << std::endl;
+
+        vtkCellDataToPointData * converter = vtkCellDataToPointData::New();
+        converter->SetInput( dataSet );
+        converter->PassCellDataOff();
+        converter->Update();
+
+	///Why do we need to do this only for unstructured grids?
+        if ( dataSet->GetDataObjectType() == VTK_UNSTRUCTURED_GRID )
+        {
+            vtkDataSet * newdataset = vtkUnstructuredGrid::New();
+            newdataset->DeepCopy( converter->GetOutput() );
+            converter->Delete();
+
+            dataSet->Delete();
+            dataSet = newdataset;
+        }
+        else
+        {
+            converter->Delete();
+            std::cout <<"\nAttempt failed: can not currently handle "
+                      << "this type of data\n" << std::endl;
+            exit(1);
+        }
+    }
+    return;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+void DataObjectHandler::SetDatasetOperatorCallback(DatasetOperatorCallback* dsoCbk)
+{
+    m_datasetOperator = dsoCbk;
+}
---
+++ VE_Xplorer/Utilities/DataObjectHandler.h
@@ -0,0 +1,90 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2007 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2007-06-15 11:06:13 -0500 (Fri, 15 Jun 2007) $
+ * Version:       $Rev: 8206 $
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef DATA_OBJECT_HANDLER_H
+#define DATA_OBJECT_HANDLER_H
+/*!\file DataObjectHandler.h
+DataObjectHandler API. At a minimum, converts cell data to point data in each dataset of
+the data object.
+*/
+
+/*!\class VE_Util::DataObjectHandler
+*
+*/
+class vtkDataObject;
+class vtkDataSet;
+
+#include "VE_Installer/include/VEConfig.h"
+
+
+namespace VE_Util
+{
+class VE_UTIL_EXPORTS DataObjectHandler
+{
+public:
+    ///Constructor
+    DataObjectHandler();
+    ///Destructor
+    virtual ~DataObjectHandler();
+
+    /*!\class VE_Util::DataObjectHandler::DatasetOperatorCallback
+    *
+    */
+    class DatasetOperatorCallback
+    {
+    public:    
+        ///Constructor
+        DatasetOperatorCallback(){};
+	    ///Destructor
+        virtual ~DatasetOperatorCallback(){};
+	    ///The operation to do on each vtkDataSet in the vtkDataObject
+	    ///\param dataset The vtkDataSet to operate on
+	    virtual void OperateOnDataset(vtkDataSet* dataset) = 0;
+    protected:
+    };
+    ///Do the operation specified in the DataObjectHandlerCallback\n
+    ///on a vtkDataObject
+    void OperateOnAllDatasetsInObject(vtkDataObject* dataObject);
+    ///Set the operation to perform on each dataset in the vtkDataObject
+    ///\param dsoCbk The DataSetOperatorCallback
+    void SetDatasetOperatorCallback(DatasetOperatorCallback* dsoCbk);
+protected:
+    ///Convert the cell data to point data if it exists
+    ///\param dataset The vtkDataSet
+    void _convertCellDataToPointData(vtkDataSet* dataset);
+
+    DatasetOperatorCallback* m_datasetOperator;///<The DatasetOperatorCallback.
+};
+}
+#endif
