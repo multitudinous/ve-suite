a263f9811cdee6a0fa5d2fcbb2a56b8e279406de
Biv <biv@unknown.nil>
2007-08-29 21:30:29 +0000

    ConcreteLoader -- includes perlin noise texture (3d) and Concrete shader...not very flexible (ie uniforms are hard coded)...

---
+++ VE_Xplorer/SceneGraph/Utilities/ConcreteLoader.cxx
@@ -0,0 +1,178 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2005 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include "VE_Xplorer/SceneGraph/Utilities/ConcreteLoader.h"
+#include "VE_Xplorer/SceneGraph/Utilities/PerlinNoiseTexture.h"
+#include <iostream>
+#include <sstream>
+#include "VE_Open/XML/Shader/Shader.h"
+#include "VE_Open/XML/Shader/Program.h"
+#include "VE_Open/XML/Shader/Uniform.h"
+using namespace VE_XML::VE_Shader;
+using namespace VE_SceneGraph::Utilities;
+//////////////////////////////////////
+//Constructors                      //
+//////////////////////////////////////
+ConcreteLoader::ConcreteLoader()
+:ShaderHelper()
+
+{
+	m_noise = new PerlinNoiseTexture(64,64,64);
+}
+///////////////////////////////////////////////////
+ConcreteLoader::ConcreteLoader(const ConcreteLoader& rhs)
+:ShaderHelper(rhs)
+{
+}
+//////////////////////////////////////////////////////////////
+ConcreteLoader& ConcreteLoader::operator=(const ConcreteLoader& rhs)
+{
+   if(this != &rhs)
+   {
+      ShaderHelper::operator=(rhs);
+   }
+   return *this;
+}
+///////////////////////////////////////////
+//Destructor                             //
+///////////////////////////////////////////
+ConcreteLoader::~ConcreteLoader()
+{
+    if( m_noise )
+	{
+        delete m_noise;
+		m_noise = 0;
+	}
+}
+/////////////////////////////////////////////////////////////////////////////////
+void ConcreteLoader::_loadShader(std::string vertexSource,std::string fragmentSource)
+{
+#ifdef _OSG
+   //std::cout<<"Loading shader!!"<<std::endl;
+   //std::cout<<"vertex shader!!"<<std::endl<<vertexSource<<std::endl;
+   //std::cout<<"frag shader!!"<<std::endl<<fragmentSource<<std::endl;
+   VE_XML::VE_Shader::ShaderPtr vertShader = new VE_XML::VE_Shader::Shader();
+   vertShader->SetShaderType("Vertex");
+   vertShader->SetShaderSource(vertexSource);
+   
+   VE_XML::VE_Shader::ShaderPtr fragShader = new VE_XML::VE_Shader::Shader();
+   fragShader->SetShaderType("Fragment");
+   fragShader->SetShaderSource(fragmentSource);
+
+   VE_XML::VE_Shader::Uniform lightPosition;
+   lightPosition.SetType("Float");
+   lightPosition.SetName("LightPos");
+   lightPosition.SetSize(3);
+   std::vector<float> lightPos;
+   lightPos.push_back(10.0);
+   lightPos.push_back(10.0);
+   lightPos.push_back(10.0);
+   lightPosition.SetValues(lightPos);
+
+   VE_XML::VE_Shader::Uniform scale;
+   scale.SetType("Float");
+   scale.SetName("Scale");
+   scale.SetSize(1);
+   std::vector<float> value;
+   value.push_back(1.0);
+   scale.SetValues(value);
+
+   VE_XML::VE_Shader::Uniform noiseScale;
+   noiseScale.SetType("Float");
+   noiseScale.SetName("NoiseScale");
+   noiseScale.SetSize(1);
+   std::vector<float> ns;
+   ns.push_back(1.0);
+   noiseScale.SetValues(ns);
+
+   VE_XML::VE_Shader::Uniform specularValues;
+   specularValues.SetType("Float");
+   specularValues.SetName("specularPower");
+   specularValues.SetSize(1);
+   std::vector<float> specularPower;
+   specularPower.push_back(20.0);
+   specularValues.SetValues(specularPower);
+   
+   fragShader->AddUniform(noiseScale); 
+   //fragShader->AddUniform(dmaterial); 
+   vertShader->AddUniform(scale); 
+   vertShader->AddUniform(lightPosition); 
+   VE_XML::VE_Shader::Program glslProgram;// = new VE_XML::VE_Shader::Program();
+   glslProgram.SetProgramName("Phong Shader");
+   glslProgram.SetVertexShader(vertShader);
+   glslProgram.SetFragmentShader(fragShader);
+
+   LoadGLSLProgram(&glslProgram);
+   _ss->addUniform(new osg::Uniform("Noise",0));
+   _ss->setTextureAttributeAndModes(0,m_noise->GetNoiseTexture());
+#endif
+}
+/////////////////////////////////////////
+void ConcreteLoader::SyncShaderAndStateSet()
+{
+   std::string vSource(
+		"varying float LightIntensity;\n"
+		"varying vec3  MCposition;\n"
+
+		"uniform vec3  LightPos;\n"
+		"uniform float Scale;\n"
+
+		"void main()\n"
+		"{\n"
+			"vec3 ECposition = vec3(gl_ModelViewMatrix * gl_Vertex);\n"
+			"MCposition      = vec3(gl_Vertex) * Scale;\n"
+			"vec3 tnorm      = normalize(vec3(gl_NormalMatrix * gl_Normal));\n"
+			"LightIntensity  = dot(normalize(LightPos - ECposition), tnorm);\n"
+			"LightIntensity *= 1.5;\n"
+			"gl_Position     = ftransform();\n"
+		"}\n"
+    );
+   std::string fSource(
+	"varying float LightIntensity; \n"
+	"varying vec3  MCposition;\n"
+
+	"uniform sampler3D Noise;\n"
+	"uniform float NoiseScale;\n"
+
+	"void main()\n"
+	"{\n"
+		"vec4  noisevec  = texture3D(Noise, NoiseScale * MCposition);\n"
+		"float intensity = min(1.0, noisevec[3] * 18.0);\n"
+		"vec3  color     = vec3(intensity * LightIntensity);\n"
+	    
+		"gl_FragColor    = vec4(color, 1.0);\n"
+	"}\n"
+
+	);
+   _loadShader(vSource,fSource);
+}
---
+++ VE_Xplorer/SceneGraph/Utilities/ConcreteLoader.h
@@ -0,0 +1,88 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2005 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef CONCRETE_LOADER_H
+#define CONCRETE_LOADER_H
+/*!\file ConcreteLoader.h
+  ConcreteLoader API
+  */
+/*!\class VE_SceneGraph::Utilities::ConcreteLoader
+ * Class that creates an OSG StateSet representing
+ * a procedrual concrete shading glsl program.
+ */
+#include "VE_Installer/include/VEConfig.h"
+#include "VE_Xplorer/SceneGraph/Utilities/ShaderHelper.h"
+#ifdef _OSG
+
+#include <string>
+#include <vector>
+///////////////////////////////////////////////////////////////////////
+//this class is used to create a stateset representing a phong shader// 
+///////////////////////////////////////////////////////////////////////
+namespace VE_SceneGraph
+{
+namespace Utilities
+{
+class PerlinNoiseTexture;
+class VE_SCENEGRAPH_UTILS_EXPORTS ConcreteLoader : 
+	public VE_SceneGraph::Utilities::ShaderHelper
+{
+public:
+   ///Constructor
+   ConcreteLoader();
+
+   ///Copy Constructor
+   ConcreteLoader(const ConcreteLoader& rhs);
+  
+   ///Destructor
+   virtual ~ConcreteLoader();
+  
+   ///Sync up the active stateset with the current shader
+   virtual void SyncShaderAndStateSet(); 
+
+   ///Equal operator
+   ///\param rhs Right hand side.
+   ConcreteLoader& operator=(const ConcreteLoader& rhs);
+protected:
+   ///Load and create the stateset for phong shader
+   ///\param vertexSource The vertex shader source code
+   ///\param fragSource The fragment shader source code
+   virtual void _loadShader(std::string vertexSource, std::string fragSource);
+
+   PerlinNoiseTexture* m_noise;///<The noise texture
+};
+}
+}
+#endif
+#endif// PHONG_LOADER_H
