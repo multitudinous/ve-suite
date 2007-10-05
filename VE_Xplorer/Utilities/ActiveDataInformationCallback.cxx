4fa8e2fddcd83d81d33b68b0408f48d07f886e1d
Biv <biv@unknown.nil>
2007-10-05 13:27:10 +0000

    more dataobject operators

---
+++ VE_Xplorer/Utilities/ActiveDataInformationCallback.cxx
@@ -0,0 +1,73 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2007 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2007-06-15 11:06:13 -0500 (Fri, 15 Jun 2007) $
+ * Version:       $Rev: 8206 $
+ * Author:        $Author: biv $
+ * Id:            $Id: ComputeDataObjectBoundsCallback.h 9200 2007-10-03 19:17:07Z biv $
+ * -----------------------------------------------------------------
+ *
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include "VE_Xplorer/Utilities/ActiveDataInformationCallback.h"
+#include <vtkDataSet.h>
+#include <vtkPointData.h>
+
+using namespace VE_Util;
+//////////////////////////////////////////////////////////////
+ActiveDataInformationCallback::ActiveDataInformationCallback()
+{
+
+}
+///////////////////////////////////////////////////////////////
+ActiveDataInformationCallback::~ActiveDataInformationCallback()
+{
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+void ActiveDataInformationCallback::OperateOnDataset(vtkDataSet* dataset)
+{
+    if(!m_activeScalar.empty())
+    {
+        dataset->GetPointData()->SetActiveScalars(m_activeScalar.c_str());   
+    }
+    if(!m_activeVector.empty())
+    {
+        dataset->GetPointData()->SetActiveVectors(m_activeVector.c_str());   
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void ActiveDataInformationCallback::SetActiveDataName(std::string name,
+		                                                               bool isVector)
+{
+    (isVector)?m_activeVector = name:m_activeScalar = name;   
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+std::string ActiveDataInformationCallback::GetActiveDataName(bool isVector)
+{
+    return (isVector)?m_activeVector:m_activeScalar;   
+}
+
---
+++ VE_Xplorer/Utilities/ActiveDataInformationCallback.h
@@ -0,0 +1,83 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2007 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2007-06-15 11:06:13 -0500 (Fri, 15 Jun 2007) $
+ * Version:       $Rev: 8206 $
+ * Author:        $Author: biv $
+ * Id:            $Id: ComputeDataObjectBoundsCallback.h 9200 2007-10-03 19:17:07Z biv $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+#ifndef ACTIVE_DATA_INFORMATION_CALLBACK 
+#define  ACTIVE_DATA_INFORMATION_CALLBACK
+/*!\file ActiveDataInformationCallback.h
+ActiveDataInformationCallback API.
+*/
+
+/*!\class VE_Util::ActiveDataInformationCallback
+*
+*/
+class vtkDataSet;
+
+#include "VE_Installer/include/VEConfig.h"
+#include "VE_Xplorer/Utilities/DataObjectHandler.h"
+
+#include <vector>
+#include <string>
+#include <map>
+namespace VE_Util
+{
+class VE_UTIL_EXPORTS ActiveDataInformationCallback:
+        public DataObjectHandler::DatasetOperatorCallback
+{
+public:    
+    ///Constructor
+    ActiveDataInformationCallback();
+    ///Destructor
+    virtual ~ActiveDataInformationCallback();
+    ///The operation to do on each vtkDataSet in the vtkDataObject
+    ///\param dataset The vtkDataSet to operate on
+    virtual void OperateOnDataset(vtkDataSet* dataset);
+    
+    ///Set the active vector or scalar
+    ///\param name The name of the scalar or vector to make active
+    ///\param isVector Flag determining vector or scalar data 
+    void SetActiveDataName(std::string name,bool isVector=false);
+
+    ///Get the name of the active scalar or vector
+    ///\param isVector Flag determining vector or scalar data 
+    std::string GetActiveDataName(bool isVector=false);
+
+    ///Get active scalar or vector 
+    ///\param  isVector Flag determining vector or scalar data to retrieve 
+    std::string GetActiveData(bool isVector=false);
+
+protected:
+    std::string m_activeScalar;///<The active scalar 	
+    std::string m_activeVector;///<The active vector	
+};
+}
+#endif
+
