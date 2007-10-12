28236e1d1664436b3d0c441aa96031d047b4848e
Daniel Shipton <dan@bitmethod.com>
2007-10-12 03:23:34 +0000

    environment builds.

--- src/ves/xplorer/environment/cfdTeacher.cxx
+++ src/ves/xplorer/environment/cfdTeacher.cxx
@@ -38,7 +38,7 @@
 #include <ves/xplorer/scenegraph/Group.h>
 
 #include <ves/xplorer/cfdCommandArray.h>
-#include <ves/xplorer/event/viz/cfdScalarBarActor.h>
+//#include <ves/xplorer/event/viz/cfdScalarBarActor.h>
 #include <ves/xplorer/ModelHandler.h>
 #include <ves/xplorer/cfdDebug.h>
 
