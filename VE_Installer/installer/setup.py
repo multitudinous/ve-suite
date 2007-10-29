4a2fbc41591a698efa4013985624c09e35a27eb1
Doug McCorkle <doug.mccorkle@gmail.com>
2007-10-29 16:22:57 +0000

    more VE_Installer updates

--- VE_Installer/installer/setup.py
+++
@@ -1,7 +0,0 @@
-# setup.py
-from distutils.core import setup
-import py2exe
-
-setup(windows=[{"script": "velauncher.py",
-                "icon_resources": [(1, "installerImages/Ve_icon.ico")]}],
-      zipfile=None)
