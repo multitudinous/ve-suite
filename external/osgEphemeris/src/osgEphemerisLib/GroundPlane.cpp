74d5bef6637d407db4c9a44d4e2bdb2783fe84c3
Biv <biv@unknown.nil>
2007-11-15 23:27:35 +0000

    These are the mods to add a default textured ground plane. It is slightly modified code from osgterrain example. This will need cleanup but checking in so others can start to use it.

--- external/osgEphemeris/src/osgEphemerisLib/GroundPlane.cpp
+++ external/osgEphemeris/src/osgEphemerisLib/GroundPlane.cpp
@@ -11,42 +11,49 @@
  * OpenSceneGraph Public License for more details.
 */
 #include <osgEphemeris/GroundPlane>
-
+#include "./osgshaderterrain.h"
 using namespace osgEphemeris;
 
 GroundPlane::GroundPlane( double radius )
 {
-    osg::ref_ptr<osg::Vec3Array> coords = new osg::Vec3Array;
+    osg::ref_ptr<osg::Geometry> terrain = CreateTerrain(radius);
+    if(terrain.valid())
+    {
+        addDrawable( terrain.get() );
+    }
+    else
+    {
+        osg::ref_ptr<osg::Vec3Array> coords = new osg::Vec3Array;
 
-    coords->push_back( osg::Vec3( -radius, -radius, -1000.0 ));
-    coords->push_back( osg::Vec3(  radius, -radius, -1000.0 ));
-    coords->push_back( osg::Vec3(  radius,  radius, -1000.0 ));
-    coords->push_back( osg::Vec3( -radius,  radius, -1000.0 ));
+        coords->push_back( osg::Vec3( -radius, -radius, -1000.0 ));
+        coords->push_back( osg::Vec3(  radius, -radius, -1000.0 ));
+        coords->push_back( osg::Vec3(  radius,  radius, -1000.0 ));
+        coords->push_back( osg::Vec3( -radius,  radius, -1000.0 ));
 
-    osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
-    normals->push_back( osg::Vec3(0,0,1));
+        osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
+        normals->push_back( osg::Vec3(0,0,1));
 
-    osg::ref_ptr<osg::Vec4Array> colors  = new osg::Vec4Array;
-    colors->push_back( osg::Vec4( 0.0, 0.0, 0.0, 1.0 ));
+        osg::ref_ptr<osg::Vec4Array> colors  = new osg::Vec4Array;
+        colors->push_back( osg::Vec4( 0.0, 0.0, 0.0, 1.0 ));
 
-    osg::ref_ptr<osg::Vec2Array> tcoords = new osg::Vec2Array;
-    tcoords->push_back( osg::Vec2( 0.0, 0.0 ));
-    tcoords->push_back( osg::Vec2( 1.0, 0.0 ));
-    tcoords->push_back( osg::Vec2( 1.0, 1.0 ));
-    tcoords->push_back( osg::Vec2( 0.0, 1.0 ));
+        osg::ref_ptr<osg::Vec2Array> tcoords = new osg::Vec2Array;
+        tcoords->push_back( osg::Vec2( 0.0, 0.0 ));
+        tcoords->push_back( osg::Vec2( 1.0, 0.0 ));
+        tcoords->push_back( osg::Vec2( 1.0, 1.0 ));
+        tcoords->push_back( osg::Vec2( 0.0, 1.0 ));
 
+        osg::ref_ptr<osg::Geometry> geometry = new osg::Geometry;
+        geometry->setVertexArray( coords.get() );
+        geometry->setTexCoordArray( 0, tcoords.get() );
 
-    osg::ref_ptr<osg::Geometry> geometry = new osg::Geometry;
-    geometry->setVertexArray( coords.get() );
-    geometry->setTexCoordArray( 0, tcoords.get() );
+        geometry->setNormalArray( normals.get() );
+        geometry->setNormalBinding( osg::Geometry::BIND_OVERALL );
 
-    geometry->setNormalArray( normals.get() );
-    geometry->setNormalBinding( osg::Geometry::BIND_OVERALL );
+        geometry->setColorArray( colors.get() );
+        geometry->setColorBinding( osg::Geometry::BIND_OVERALL );
 
-    geometry->setColorArray( colors.get() );
-    geometry->setColorBinding( osg::Geometry::BIND_OVERALL );
+        geometry->addPrimitiveSet( new osg::DrawArrays( osg::PrimitiveSet::QUADS, 0, coords->size() ));
 
-    geometry->addPrimitiveSet( new osg::DrawArrays( osg::PrimitiveSet::QUADS, 0, coords->size() ));
-
-    addDrawable( geometry.get() );
+        addDrawable( geometry.get() );
+    }
 }
