139fc7a73299819164f8b96a2f15715b928928af
Doug McCorkle <doug.mccorkle@gmail.com>
2007-12-12 23:35:46 +0000

    added new osgOQ in

---
+++ external/osgOQ/Test/oqTest00/oqTest00.cpp
@@ -0,0 +1,329 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2006 Robert Osfield 
+ *
+ * This application is open source and may be redistributed and/or modified   
+ * freely and without restriction, both in commericial and non commericial applications,
+ * as long as this copyright notice is maintained.
+ * 
+ * This application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+// oqTest00
+//
+// This test code demonstrates inserting OcclusionQueryNodes manually into
+//   a scene graph.
+
+#include <osgDB/ReadFile>
+#include <osgDB/WriteFile>
+#include <osgUtil/SmoothingVisitor>
+#include <osgUtil/Optimizer>
+#include <osg/BoundingSphere>
+#include <osg/ShapeDrawable>
+
+#include <osg/MatrixTransform>
+#include <osg/Switch>
+
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+
+#include <osgGA/TrackballManipulator>
+#include <osgGA/FlightManipulator>
+#include <osgGA/DriveManipulator>
+#include <osgGA/KeySwitchMatrixManipulator>
+#include <osgGA/StateSetManipulator>
+#include <osgGA/AnimationPathManipulator>
+#include <osgGA/TerrainManipulator>
+
+#include "osgOQ/OcclusionQueryVisitor.h"
+#include "osgOQ/OcclusionQueryNode.h"
+
+#include <list>
+#include <iostream>
+#include <sstream>
+
+
+// Ptr to geom that we'll modify.
+static osg::ref_ptr<osg::Geometry> g_geom;
+static osg::Vec3 g_center( -1.5f, .5f, 1.f );
+static float g_radius( 1. );
+
+void setOctohedronVertices( osg::Geometry& geom, const osg::Vec3& center, float radius );
+
+class KeyHandler : public osgGA::GUIEventHandler 
+{
+public: 
+    KeyHandler( osg::Node& node )
+      : _node( node ),
+        _enable( true ),
+        _debug( false )
+    {}
+
+    bool handle( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& )
+    {
+        switch( ea.getEventType() )
+        {
+            case(osgGA::GUIEventAdapter::KEYUP):
+            {
+                if (ea.getKey()==osgGA::GUIEventAdapter::KEY_F4)
+                {
+                    if (g_radius == 1.f )
+                        g_radius = 1.5f;
+                    else
+                        g_radius = 1.f;
+                    setOctohedronVertices( *g_geom, g_center, g_radius );
+
+                    return true;
+                }
+                else if (ea.getKey()==osgGA::GUIEventAdapter::KEY_F6)
+                {
+                    // F6 -- Toggle osgOQ testing.
+                    _enable = !_enable;
+                    osgOQ::EnableQueryVisitor eqv( _enable );
+                    _node.accept( eqv );
+                    return true;
+                }
+                else if (ea.getKey()==osgGA::GUIEventAdapter::KEY_F7)
+                {
+                    // F7 -- Toggle display of OQ test bounding volumes
+                    _debug = !_debug;
+                    osgOQ::DebugDisplayVisitor ddv( _debug );
+                    _node.accept( ddv );
+                    return true;
+                }
+                else if (ea.getKey()==osgGA::GUIEventAdapter::KEY_F8)
+                {
+                    // F8 -- Gether stats and display
+                    osgOQ::StatisticsVisitor sv;
+                    _node.accept( sv );
+                    std::cout << "osgOQ: Stats: numOQNs " << sv.getNumOQNs() << ", numPased " << sv.getNumPassed() << std::endl;
+                    return true;
+                }
+                else if (ea.getKey()=='o')
+                {
+                    if (osgDB::writeNodeFile( _node, "saved_model.osg" ))
+                        osg::notify( osg::ALWAYS ) << "osgOQ: Wrote scene graph to \"saved_model.osg\"" << std::endl;
+                    else
+                        osg::notify( osg::ALWAYS ) << "osgOQ: Wrote failed for \"saved_model.osg\"" << std::endl;
+                    return true;
+                }
+                return false;
+            }
+            default:
+                break;
+        }
+        return false;
+    }
+
+    osg::Node& _node;
+
+    bool _enable, _debug;
+};
+
+void setOctohedronVertices( osg::Geometry& geom, const osg::Vec3& center, float radius )
+{
+    osg::ref_ptr<osg::Vec3Array> v = new osg::Vec3Array;
+    v->push_back( osg::Vec3( center[0], center[1]-radius, center[2] ) );
+    v->push_back( osg::Vec3( center[0]+radius, center[1], center[2] ) );
+    v->push_back( osg::Vec3( center[0], center[1]+radius, center[2] ) );
+    v->push_back( osg::Vec3( center[0]-radius, center[1], center[2] ) );
+    v->push_back( osg::Vec3( center[0], center[1], center[2]+radius ) );
+    v->push_back( osg::Vec3( center[0], center[1], center[2]-radius ) );
+
+    geom.setVertexArray( v.get() );
+}
+
+osg::ref_ptr<osg::Geometry>
+createOctohedron( const osg::Vec3& center, float radius, const osg::Vec4& color )
+{
+    osg::ref_ptr<osg::Vec4Array> c = new osg::Vec4Array;
+    c->push_back( color );
+
+    osg::ref_ptr<osg::Vec3Array> n = new osg::Vec3Array;
+    n->push_back( osg::Vec3( 1., -1., 1. ) / 1.732 );
+    n->push_back( osg::Vec3( 1., -1., -1. ) / 1.732 );
+    n->push_back( osg::Vec3( 1., 1., -1. ) / 1.732 );
+    n->push_back( osg::Vec3( 1., 1., 1. ) / 1.732 );
+    n->push_back( osg::Vec3( -1., -1., 1. ) / 1.732 );
+    n->push_back( osg::Vec3( -1., -1., -1. ) / 1.732 );
+    n->push_back( osg::Vec3( -1., 1., -1. ) / 1.732 );
+    n->push_back( osg::Vec3( -1., 1., 1. ) / 1.732 );
+
+    unsigned short idx[] = {
+        0, 1, 4,
+        1, 0, 5,
+        2, 1, 5,
+        1, 2, 4,
+        3, 0, 4,
+        0, 3, 5,
+        3, 2, 5,
+        2, 3, 4 };
+
+    osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
+    setOctohedronVertices( *geom, center, radius );
+    geom->setColorArray( c.get() );
+    geom->setColorBinding( osg::Geometry::BIND_OVERALL );
+    geom->setNormalArray( n.get() );
+    geom->setNormalBinding( osg::Geometry::BIND_PER_PRIMITIVE );
+
+    geom->addPrimitiveSet( new osg::DrawElementsUShort( GL_TRIANGLES, 24, idx ) );
+
+    return geom.get();
+}
+
+osg::ref_ptr<osg::Node>
+createScene()
+{
+    osg::ref_ptr<osg::Group> grp = new osg::Group;
+
+    osg::Geode* geode = new osg::Geode;
+
+    g_geom = createOctohedron(
+        g_center, g_radius, osg::Vec4( 1.f, .5f, 1.f, 1.f ) );
+    geode->addDrawable( g_geom.get() );
+
+    osg::ref_ptr<osg::Geometry> draw = createOctohedron(
+        osg::Vec3( 1.5f, -1.f, .5f ), 1.f, osg::Vec4( 1.f, 1.f, 1.f, 1.f ) );
+    geode->addDrawable( draw.get() );
+
+    osgOQ::OcclusionQueryNode* oqn = new osgOQ::OcclusionQueryNode;
+    oqn->addChild( geode );
+    grp->addChild( oqn );
+
+
+
+    osg::ref_ptr<osg::TessellationHints> hints = new osg::TessellationHints;
+    hints->setDetailRatio( 5.f );
+
+    geode = new osg::Geode;
+
+    osg::ref_ptr<osg::ShapeDrawable> shape = new osg::ShapeDrawable(
+    new osg::Box( osg::Vec3( 0.f, 0.f, -1.f ), 10.f, 10.f, .1f ), hints.get() );
+    shape->setColor( osg::Vec4( 1.f, 1.f, 1.f, 1.f ) );
+    geode->addDrawable( shape.get() );
+
+    grp->addChild( geode );
+
+    return (osg::Node*)grp.get();
+}
+
+
+
+int main(int argc, char** argv)
+{
+    // use an ArgumentParser object to manage the program arguments.
+    osg::ArgumentParser arguments(&argc,argv);
+
+    arguments.getApplicationUsage()->setApplicationName(arguments.getApplicationName());
+    arguments.getApplicationUsage()->setDescription(arguments.getApplicationName()+" is the standard OpenSceneGraph example which loads and visualises 3d models.");
+    arguments.getApplicationUsage()->setCommandLineUsage(arguments.getApplicationName()+" [options] filename ...");
+    arguments.getApplicationUsage()->addCommandLineOption("--image <filename>","Load an image and render it on a quad");
+    arguments.getApplicationUsage()->addCommandLineOption("--dem <filename>","Load an image/DEM and render it on a HeightField");
+    arguments.getApplicationUsage()->addCommandLineOption("-h or --help","Display command line parameters");
+    arguments.getApplicationUsage()->addCommandLineOption("--help-env","Display environmental variables available");
+    arguments.getApplicationUsage()->addCommandLineOption("--help-keys","Display keyboard & mouse bindings available");
+    arguments.getApplicationUsage()->addCommandLineOption("--help-all","Display all command line, env vars and keyboard & mouse bindings.");
+    arguments.getApplicationUsage()->addCommandLineOption("--SingleThreaded","Select SingleThreaded threading model for viewer.");
+    arguments.getApplicationUsage()->addCommandLineOption("--CullDrawThreadPerContext","Select CullDrawThreadPerContext threading model for viewer.");
+    arguments.getApplicationUsage()->addCommandLineOption("--DrawThreadPerContext","Select DrawThreadPerContext threading model for viewer.");
+    arguments.getApplicationUsage()->addCommandLineOption("--CullThreadPerCameraDrawThreadPerContext","Select CullThreadPerCameraDrawThreadPerContext threading model for viewer.");
+
+    arguments.getApplicationUsage()->addCommandLineOption("--stock","Display the stock scene.");
+    arguments.getApplicationUsage()->addCommandLineOption("--opt","Run the osgUtil Optimizer.");
+
+    // if user request help write it out to cout.
+    bool helpAll = arguments.read("--help-all");
+    unsigned int helpType = ((helpAll || arguments.read("-h") || arguments.read("--help"))? osg::ApplicationUsage::COMMAND_LINE_OPTION : 0 ) |
+                            ((helpAll ||  arguments.read("--help-env"))? osg::ApplicationUsage::ENVIRONMENTAL_VARIABLE : 0 ) |
+                            ((helpAll ||  arguments.read("--help-keys"))? osg::ApplicationUsage::KEYBOARD_MOUSE_BINDING : 0 );
+    if (helpType)
+    {
+        arguments.getApplicationUsage()->write(std::cout, helpType);
+        return 1;
+    }
+
+    // report any errors if they have occurred when parsing the program arguments.
+    if (arguments.errors())
+    {
+        arguments.writeErrorMessages(std::cout);
+        return 1;
+    }
+    
+    osgViewer::Viewer viewer( arguments );
+    
+    // set up the camera manipulators.
+    {
+        osg::ref_ptr<osgGA::KeySwitchMatrixManipulator> keyswitchManipulator = new osgGA::KeySwitchMatrixManipulator;
+
+        keyswitchManipulator->addMatrixManipulator( '1', "Trackball", new osgGA::TrackballManipulator() );
+        keyswitchManipulator->addMatrixManipulator( '2', "Flight", new osgGA::FlightManipulator() );
+        keyswitchManipulator->addMatrixManipulator( '3', "Drive", new osgGA::DriveManipulator() );
+        keyswitchManipulator->addMatrixManipulator( '4', "Terrain", new osgGA::TerrainManipulator() );
+
+        std::string pathfile;
+        char keyForAnimationPath = '5';
+        while (arguments.read("-p",pathfile))
+        {
+            osgGA::AnimationPathManipulator* apm = new osgGA::AnimationPathManipulator(pathfile);
+            if (apm || !apm->valid()) 
+            {
+                unsigned int num = keyswitchManipulator->getNumMatrixManipulators();
+                keyswitchManipulator->addMatrixManipulator( keyForAnimationPath, "Path", apm );
+                keyswitchManipulator->selectMatrixManipulator(num);
+                ++keyForAnimationPath;
+            }
+        }
+
+        viewer.setCameraManipulator( keyswitchManipulator.get() );
+    }
+
+    // add the state manipulator
+    viewer.addEventHandler( new osgGA::StateSetManipulator(viewer.getCamera()->getOrCreateStateSet()) );
+    
+    // add the thread model handler
+    viewer.addEventHandler(new osgViewer::ThreadingHandler);
+
+    // add the window size toggle handler
+    viewer.addEventHandler(new osgViewer::WindowSizeHandler);
+
+    // add the stats handler
+    viewer.addEventHandler(new osgViewer::StatsHandler);
+
+    // add the help handler
+    viewer.addEventHandler(new osgViewer::HelpHandler(arguments.getApplicationUsage()));
+
+    // add the record camera path handler
+    viewer.addEventHandler(new osgViewer::RecordCameraPathHandler);
+
+    osg::ref_ptr<osgDB::ReaderWriter::Options> opts =
+        new osgDB::ReaderWriter::Options();
+    // load the specified model
+    osg::ref_ptr<osg::Node> loadedModel = osgDB::readNodeFiles( arguments, opts.get() );
+    if (!loadedModel) 
+        loadedModel = createScene();
+
+    bool optimize = arguments.read( "--opt" );
+
+    // any option left unread are converted into errors to write out later.
+    arguments.reportRemainingOptionsAsUnrecognized();
+
+    // report any errors if they have occurred when parsing the program arguments.
+    if (arguments.errors())
+    {
+        arguments.writeErrorMessages(std::cout);
+        return 1;
+    }
+
+
+    // optimize the scene graph, remove redundant nodes and state etc.
+    if (optimize)
+    {
+        osgUtil::Optimizer optimizer;
+        optimizer.optimize( loadedModel.get() );
+    }
+    viewer.addEventHandler(new KeyHandler( *loadedModel ));
+
+    viewer.setUpViewOnSingleScreen( 0 );
+    viewer.setSceneData( loadedModel.get() );
+    return viewer.run();
+}
---
+++ external/osgOQ/include/osgOQ/OcclusionQueryVisitor.h
@@ -0,0 +1,229 @@
+//
+// Copyright (C) 2007 Skew Matrix Software LLC (http://www.skew-matrix.com)
+//
+// This library is open source and may be redistributed and/or modified under  
+// the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
+// (at your option) any later version.  The full license is in LICENSE file
+// included with this distribution, and on the openscenegraph.org website.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+// OpenSceneGraph Public License for more details.
+//
+
+#ifndef OSGOQ_OCCLUSION_QUERY_VISITOR_H
+#define OSGOQ_OCCLUSION_QUERY_VISITOR_H 1
+
+#include "osgOQ/QueryExportDeclaration.h"
+#include <osg/Node>
+#include <osg/NodeVisitor>
+#include <osg/Geode>
+#include <osg/Geometry>
+
+namespace osgOQ {
+
+
+// Use this visitor to insert OcclusionQueryNodes (OQNs) in the
+//   visited subgraph. OQNs may be nested, which could introduce
+//   visual anomalies; see also OcclusionQueryFlatVisitor.
+class OSGOQ_EXPORT OcclusionQueryNonFlatVisitor : public osg::NodeVisitor
+{
+public:
+    OcclusionQueryNonFlatVisitor();
+    virtual ~OcclusionQueryNonFlatVisitor();
+
+    // Specify the vertex count threshold for performing occlusion
+    //   query tests. Nodes in the scene graph whose total child geometry
+    //   contains fewer vertices than the specified threshold will
+    //   never be tested, just drawn. (In fact, they will br treated as
+    //   potential occluders and rendered first in front-to-back order.)
+    void setOccluderThreshold( int vertices );
+    int getOccluderThreshold() const;
+
+    virtual void apply( osg::Node& node );
+
+protected:
+    // When an OQR creates all OQNs and each OQN shares the same OQC,
+    //   these methods are used to uniquely name all OQNs. Handy
+    //   for debugging.
+    std::string getNextOQNName();
+    int getNameIdx() const { return _nameIdx; }
+
+    osg::ref_ptr<osg::StateSet> _state;
+    osg::ref_ptr<osg::StateSet> _debugState;
+
+    unsigned int _nameIdx;
+
+    int _occluderThreshold;
+};
+
+// Use this visitor to insert OcclusionQueryNodes (OQNs) in the
+//   visited subgraph. Only one OQN will test any particular node
+//   (no nesting). See also OcclusionQueryNonFlatVisitor.
+class OSGOQ_EXPORT OcclusionQueryFlatVisitor : public osg::NodeVisitor
+{
+public:
+    OcclusionQueryFlatVisitor();
+    virtual ~OcclusionQueryFlatVisitor();
+
+    // Specify the vertex count threshold for performing occlusion
+    //   query tests. Nodes in the scene graph whose total child geometry
+    //   contains fewer vertices than the specified threshold will
+    //   never be tested, just drawn. (In fact, they will br treated as
+    //   potential occluders and rendered first in front-to-back order.)
+    void setOccluderThreshold( int vertices );
+    int getOccluderThreshold() const;
+
+    virtual void apply( osg::Group& group );
+    virtual void apply( osg::Geode& geode );
+
+protected:
+    void addOQN( osg::Node& node );
+
+    // When an OQR creates all OQNs and each OQN shares the same OQC,
+    //   these methods are used to uniquely name all OQNs. Handy
+    //   for debugging.
+    std::string getNextOQNName();
+    int getNameIdx() const { return _nameIdx; }
+
+    osg::ref_ptr<osg::StateSet> _state;
+    osg::ref_ptr<osg::StateSet> _debugState;
+
+    unsigned int _nameIdx;
+
+    int _occluderThreshold;
+};
+
+// OQNs derive from Group, and therefore can only perform tests on
+//   subgraphs, not individual Drawables. To perform a query on a single
+//   Drawable, the scene graph must be rearranged so that the target
+//   Drawable has no sibling Drawables within its owning Geode(s); then
+//   an OQN can be placed above the owning Geode(s) to effectively perform
+//   a query only on the target Drawable. This NodeVisitor rearranges the
+//   visited scene graph to allow query testing on large Drawables.
+class OSGOQ_EXPORT PerDrawableQueryVisitor : public osg::NodeVisitor
+{
+public:
+    PerDrawableQueryVisitor();
+    virtual ~PerDrawableQueryVisitor();
+
+    // Specify the vertex count threshold for performing occlusion
+    //   query tests. Nodes in the scene graph whose total child geometry
+    //   contains fewer vertices than the specified threshold will
+    //   never be tested, just drawn. (In fact, they will br treated as
+    //   potential occluders and rendered first in front-to-back order.)
+    void setOccluderThreshold( int vertices );
+    int getOccluderThreshold() const;
+
+    virtual void apply( osg::Group& group );
+    virtual void apply( osg::Geode& geode );
+
+protected:
+    osg::Geode* copyGeodeNoChildren( osg::Geode* src );
+
+    unsigned int _occluderThreshold;
+    unsigned int _count;
+
+    typedef std::vector< osg::ref_ptr<osg::Geode> > GeodeList;
+    GeodeList _newChildren;
+};
+
+
+// Find all OQNs in the visited scene graph and set their visibility threshold.
+class OSGOQ_EXPORT VisibilityThresholdVisitor : public osg::NodeVisitor
+{
+public:
+    VisibilityThresholdVisitor( unsigned int threshold=500 )
+      : osg::NodeVisitor( osg::NodeVisitor::TRAVERSE_ALL_CHILDREN ),
+        _visThreshold( threshold ) {}
+    virtual ~VisibilityThresholdVisitor() {}
+
+    virtual void apply( osg::Node& node );
+
+protected:
+    unsigned int _visThreshold;
+};
+
+// Find all OQNs in the visited scene graph and set the number of frames
+//   between queries.
+class OSGOQ_EXPORT QueryFrameCountVisitor : public osg::NodeVisitor
+{
+public:
+    QueryFrameCountVisitor( int count=5 )
+      : osg::NodeVisitor( osg::NodeVisitor::TRAVERSE_ALL_CHILDREN ),
+        _count( count ) {}
+    virtual ~QueryFrameCountVisitor() {}
+
+    virtual void apply( osg::Node& node );
+
+protected:
+    unsigned int _count;
+};
+
+// Find all OQNs in the visited scene graph and enable or disable queries..
+class OSGOQ_EXPORT EnableQueryVisitor : public osg::NodeVisitor
+{
+public:
+    EnableQueryVisitor( bool enable=true )
+      : osg::NodeVisitor( osg::NodeVisitor::TRAVERSE_ALL_CHILDREN ),
+        _enabled( enable ) {}
+    virtual ~EnableQueryVisitor() {}
+
+    virtual void apply( osg::Node& node );
+
+protected:
+    bool _enabled;
+};
+
+// Find all OQNs in the visited scene graph and enable or disable the
+//   debug bounding volume display.
+class OSGOQ_EXPORT DebugDisplayVisitor : public osg::NodeVisitor
+{
+public:
+    DebugDisplayVisitor( bool debug=true )
+      : osg::NodeVisitor( osg::NodeVisitor::TRAVERSE_ALL_CHILDREN ),
+        _debug( debug ) {}
+    virtual ~DebugDisplayVisitor() {}
+
+    virtual void apply( osg::Node& node );
+
+protected:
+    bool _debug;
+};
+
+// Remove all OQNs from the visited scene graph.
+class OSGOQ_EXPORT RemoveOcclusionQueryVisitor : public osg::NodeVisitor
+{
+public:
+    RemoveOcclusionQueryVisitor();
+    virtual ~RemoveOcclusionQueryVisitor();
+
+    virtual void apply( osg::Node& node );
+
+protected:
+};
+
+// Gather statistics about OQN performance in the visited scene graph.
+class OSGOQ_EXPORT StatisticsVisitor : public osg::NodeVisitor
+{
+public:
+    StatisticsVisitor( osg::NodeVisitor::TraversalMode mode=osg::NodeVisitor::TRAVERSE_ACTIVE_CHILDREN );
+    virtual ~StatisticsVisitor();
+
+    virtual void apply( osg::Node& node );
+
+    void reset();
+    unsigned int getNumOQNs() const;
+    unsigned int getNumPassed() const;
+
+protected:
+    unsigned int _numOQNs;
+    unsigned int _numPassed;
+};
+
+
+}
+
+
+#endif
