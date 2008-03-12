b13dc5d88494938d5049cde104e245c6fff1dbc8
Joshua Koch <jbkoch@agsolver.com>
2008-03-12 00:46:15 +0000

    CameraPlacementTool demo: Corrected the drawing of the viewing frustum to compensate for osg to juggler coordinates and osg::Matrix to gmtl::Matrix
    Former-commit-id: 7eb234e8925a051fae126ab2f619672fac5ac5ee

--- share/examples/CameraPlacementTool/GraphicalPlugin/Camera.cxx
+++ share/examples/CameraPlacementTool/GraphicalPlugin/Camera.cxx
@@ -41,52 +41,60 @@ void Camera::Initialize()
 void Camera::DrawViewFrustum()
 {
     //Get near and far from the Projection matrix.
-    const double near = m_projectionMatrix[ 3 ][ 2 ] / ( m_projectionMatrix[ 2 ][ 2 ] - 1.0 );
-    const double far = m_projectionMatrix[ 3 ][ 2 ] / ( 1.0 + m_projectionMatrix[ 2 ][ 2 ] );
+    const double near = m_projectionMatrix.mData[ 11 ] / ( m_projectionMatrix.mData[ 10 ] - 1.0 );
+    const double far = m_projectionMatrix.mData[ 11 ] / ( 1.0 + m_projectionMatrix.mData[ 10 ] );
 
     //Get the sides of the near plane.
-    const double nLeft = near * ( m_projectionMatrix[ 2 ][ 0 ] - 1.0 ) / m_projectionMatrix[ 0 ][ 0 ];
-    const double nRight = near * ( 1.0 + m_projectionMatrix[ 2 ][ 0 ] ) / m_projectionMatrix[ 0 ][ 0 ];
-    const double nTop = near * ( 1.0 + m_projectionMatrix[ 2 ][ 1 ] ) / m_projectionMatrix[ 1 ][ 1 ];
-    const double nBottom = near * ( m_projectionMatrix[ 2 ][ 1 ] - 1.0 ) / m_projectionMatrix[ 1 ][ 1 ];
+    const double nLeft =   near * ( m_projectionMatrix.mData[ 2 ] - 1.0 ) /
+                                    m_projectionMatrix.mData[ 0 ];
+    const double nRight =  near * ( m_projectionMatrix.mData[ 2 ] + 1.0 ) /
+                                    m_projectionMatrix.mData[ 0 ];
+    const double nTop =    near * ( m_projectionMatrix.mData[ 6 ] + 1.0 ) /
+                                    m_projectionMatrix.mData[ 5 ];
+    const double nBottom = near * ( m_projectionMatrix.mData[ 6 ] - 1.0 ) /
+                                    m_projectionMatrix.mData[ 5 ];
 
     //Get the sides of the far plane.
-    const double fLeft = far * ( m_projectionMatrix[ 2 ][ 0 ] - 1.0 ) / m_projectionMatrix[ 0 ][ 0 ];
-    const double fRight = far * ( 1.0 + m_projectionMatrix[ 2 ][ 0 ] ) / m_projectionMatrix[ 0 ][ 0 ];
-    const double fTop = far * ( 1.0 + m_projectionMatrix[ 2 ][ 1 ] ) / m_projectionMatrix[ 1 ][ 1 ];
-    const double fBottom = far * ( m_projectionMatrix[ 2 ][ 1 ] - 1.0 ) / m_projectionMatrix[ 1 ][ 1 ];
+    const double fLeft =   far * ( m_projectionMatrix.mData[ 2 ] - 1.0 ) /
+                                   m_projectionMatrix.mData[ 0 ];
+    const double fRight =  far * ( m_projectionMatrix.mData[ 2 ] + 1.0 ) /
+                                   m_projectionMatrix.mData[ 0 ];
+    const double fTop =    far * ( m_projectionMatrix.mData[ 6 ] + 1.0 ) /
+                                   m_projectionMatrix.mData[ 5 ];
+    const double fBottom = far * ( m_projectionMatrix.mData[ 6 ] - 1.0 ) /
+                                   m_projectionMatrix.mData[ 5 ];
 
     //Our vertex array needs only 9 vertices:
     //The origin, and the eight corners of the near and far planes.
-    osg::ref_ptr< osg::Vec3Array > v = new osg::Vec3Array();
-    v->resize( 9 );
-    (*v)[ 0 ].set( 0.0, 0.0, 0.0 );
-    (*v)[ 1 ].set( nLeft, nBottom, -near );
-    (*v)[ 2 ].set( nRight, nBottom, -near );
-    (*v)[ 3 ].set( nRight, nTop, -near );
-    (*v)[ 4 ].set( nLeft, nTop, -near );
-    (*v)[ 5 ].set( fLeft, fBottom, -far );
-    (*v)[ 6 ].set( fRight, fBottom, -far );
-    (*v)[ 7 ].set( fRight, fTop, -far );
-    (*v)[ 8 ].set( fLeft, fTop, -far );
+    osg::ref_ptr< osg::Vec3Array > vertices = new osg::Vec3Array();
+    vertices->resize( 9 );
+    (*vertices)[ 0 ].set( 0.0, 0.0, 0.0 );
+    (*vertices)[ 1 ].set( nLeft, near, nBottom );
+    (*vertices)[ 2 ].set( nRight, near, nBottom );
+    (*vertices)[ 3 ].set( nRight, near, nTop );
+    (*vertices)[ 4 ].set( nLeft, near, nTop );
+    (*vertices)[ 5 ].set( fLeft, far, fBottom );
+    (*vertices)[ 6 ].set( fRight, far, fBottom );
+    (*vertices)[ 7 ].set( fRight, far, fTop );
+    (*vertices)[ 8 ].set( fLeft, far, fTop );
 
-    osg::ref_ptr< osg::Geometry > geom = new osg::Geometry();
-    geom->setVertexArray( v.get() );
+    osg::ref_ptr< osg::Geometry > geometry = new osg::Geometry();
+    geometry->setVertexArray( vertices.get() );
 
-    osg::ref_ptr< osg::Vec4Array > c = new osg::Vec4Array();
-    c->push_back( osg::Vec4( 1.0, 1.0, 1.0, 1.0 ) );
-    geom->setColorArray( c.get() );
-    geom->setColorBinding( osg::Geometry::BIND_OVERALL );
+    osg::ref_ptr< osg::Vec4Array > color = new osg::Vec4Array();
+    color->push_back( osg::Vec4( 1.0, 1.0, 1.0, 1.0 ) );
+    geometry->setColorArray( color.get() );
+    geometry->setColorBinding( osg::Geometry::BIND_OVERALL );
 
     GLushort idxLines[ 8 ]  = { 0, 5, 0, 6, 0, 7, 0, 8 };
     GLushort idxLoops0[ 4 ] = { 1, 2, 3, 4 };
     GLushort idxLoops1[ 4 ] = { 5, 6, 7, 8 };
-    geom->addPrimitiveSet( new osg::DrawElementsUShort( osg::PrimitiveSet::LINES, 8, idxLines ) );
-    geom->addPrimitiveSet( new osg::DrawElementsUShort( osg::PrimitiveSet::LINE_LOOP, 4, idxLoops0 ) );
-    geom->addPrimitiveSet( new osg::DrawElementsUShort( osg::PrimitiveSet::LINE_LOOP, 4, idxLoops1 ) );
+    geometry->addPrimitiveSet( new osg::DrawElementsUShort( osg::PrimitiveSet::LINES, 8, idxLines ) );
+    geometry->addPrimitiveSet( new osg::DrawElementsUShort( osg::PrimitiveSet::LINE_LOOP, 4, idxLoops0 ) );
+    geometry->addPrimitiveSet( new osg::DrawElementsUShort( osg::PrimitiveSet::LINE_LOOP, 4, idxLoops1 ) );
 
     osg::ref_ptr< osg::Geode > geode = new osg::Geode();
-    geode->addDrawable( geom.get() );
+    geode->addDrawable( geometry.get() );
 
     geode->getOrCreateStateSet()->setMode( GL_LIGHTING, osg::StateAttribute::OFF | osg::StateAttribute::PROTECTED );
 
