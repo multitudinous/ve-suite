48fa115539d46b295638a2976950420ada7fbf5b
Joshua Koch <jbkoch@agsolver.com>
2008-03-27 05:09:11 +0000

    setting svn properties
    Former-commit-id: 9b961ab80d3abcecabf7d0a8f25e90689eace35e

--- share/examples/CameraPlacementTool/GraphicalPlugin/CameraPlacementToolShadersPtr.h
+++ share/examples/CameraPlacementTool/GraphicalPlugin/CameraPlacementToolShadersPtr.h
@@ -23,10 +23,10 @@
  * Boston, MA 02111-1307, USA.
  *
  * -----------------------------------------------------------------
- * Date modified: $Date: 2008-03-13 21:07:47 -0500 (Thu, 13 Mar 2008) $
- * Version:       $Rev: 10811 $
- * Author:        $Author: dshipton $
- * Id:            $Id: Command.h 10811 2008-03-14 02:07:47Z dshipton $
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
  * -----------------------------------------------------------------
  *
  *************** <auto-copyright.rb END do not edit this line> ***************/
