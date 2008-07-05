38bf64eb35064738fb80a2de6821c22e1a50bb20
Hgx <hgx@unknown.nil>
2008-07-05 16:25:21 +0000

    - Added initial network client test.
    Former-commit-id: b9b2f2ea840740eb7ada9e3b06589e20fe49d917

---
+++ test/JuliusNetworkClientTest.h
@@ -0,0 +1,32 @@
+
+#ifndef VE_JULIUS_NETWORK_CLIENT_TEST_H_
+#define VE_JULIUS_NETWORK_CLIENT_TEST_H_
+
+#include "apps/voice/JuliusNetworkClient.h"
+
+#include <cxxtest/TestSuite.h>
+
+class JuliusNetworkClientTest : public CxxTest::TestSuite
+{
+public:
+
+   void testConnect()
+   {
+      JuliusNetworkClient* jnc = new JuliusNetworkClient();
+      TS_ASSERT(!jnc->isConnected());
+      TS_ASSERT(jnc->connect(std::string("localhost")));
+      TS_ASSERT(jnc->isConnected());
+      delete jnc;
+   }
+
+   void testStartDataLoop()
+   {
+      JuliusNetworkClient* jnc = new JuliusNetworkClient();
+      TS_ASSERT(jnc->connect(std::string("localhost")));
+      TS_ASSERT(jnc->startDataLoop());
+      delete jnc;
+   }
+
+};
+
+#endif
