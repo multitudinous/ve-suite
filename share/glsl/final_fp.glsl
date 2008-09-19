f5e9724b5beda24ffe98bf21e65f4bdcef23f56a
Joshua Koch <jbkoch@agsolver.com>
2008-09-19 17:21:06 +0000

    Update to the glow convolution shaders
    Former-commit-id: ed0b2419c11fca197ff7fcd0d142cb4ca8a258ec

--- share/glsl/final_fp.glsl
+++ share/glsl/final_fp.glsl
@@ -2,13 +2,24 @@ uniform float glowStrength;
 uniform vec4 glowColor;
 
 uniform sampler2D baseMap;
+uniform sampler2D stencilGlowMap;
 uniform sampler2D glowMap;
 
 void main()
 {
     vec4 original = texture2D( baseMap, gl_TexCoord[ 0 ].xy );
+    float stencilGlowValue = texture2D( stencilGlowMap, gl_TexCoord[ 0 ].xy ).x;
     float glowValue = texture2D( glowMap, gl_TexCoord[ 0 ].xy ).x;
     vec4 glow = ( glowStrength * glowValue ) * glowColor;
+    glow = ( 1.0 - stencilGlowValue ) * glow;
 
-    gl_FragColor = original + ( 1.0 - original.w ) * glow;
+    //original = dest
+    //glow = src
+    //src * sFactor + dest * dFactor
+    //vec4 srcFactor = vec4( glow.a, glow.a, glow.a, glow.a );
+    //vec4 destFactor = vec4( 1.0, 1.0, 1.0, 1.0 ) - srcFactor;
+    //vec4 srcComponent = glow * srcFactor;
+    //vec4 destComponent = original * destFactor;
+    //gl_FragColor = srcComponent + destComponent;
+    gl_FragColor = original + glow;
 }
