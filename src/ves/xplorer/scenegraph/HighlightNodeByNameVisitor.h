d65df24555d50124d04a89ff06137fb055919390
Doug McCorkle <doug.mccorkle@gmail.com>
2008-09-19 19:57:07 +0000

    added visitor to highlight node by node name
    Former-commit-id: 97996198cca6191fc4fff92a1ce3248dc15de821

---
+++ src/ves/xplorer/scenegraph/HighlightNodeByNameVisitor.h
@@ -0,0 +1,80 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+*
+* VE-Suite is (C) Copyright 1998-2008 by Iowa State University
+*
+* Original Development Team:
+*   - ISU's Thermal Systems Virtual Engineering Group,
+*     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+*   - Reaction Engineering International, www.reaction-eng.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+* Boston, MA 02111-1307, USA.
+*
+* -----------------------------------------------------------------
+* Date modified: $Date$
+* Version:       $Rev$
+* Author:        $Author$
+* Id:            $Id$
+* -----------------------------------------------------------------
+*
+*************** <auto-copyright.rb END do not edit this line> ***************/
+#ifndef HIGHLIGHT_NODE_BY_NAME_VISITOR_H
+#define HIGHLIGHT_NODE_BY_NAME_VISITOR_H
+
+/*!\file HighlightNodeByNameVisitor.h
+*/
+
+/*!\class ves::xplorer::scenegraph::HighlightNodeByNameVisitor
+*
+*/
+
+/*!\namespace ves::xplorer::scenegraph
+*
+*/
+
+// --- VE-Suite Includes --- //
+#include <ves/VEConfig.h>
+
+// --- OSG Includes --- //
+#include <osg/NodeVisitor>
+
+namespace ves
+{
+namespace xplorer
+{
+namespace scenegraph
+{
+class VE_SCENEGRAPH_EXPORTS HighlightNodeByNameVisitor : public osg::NodeVisitor
+{
+public:
+    ///Constructor
+    ///\param node The node to be traversed
+    HighlightNodeByNameVisitor( std::string nodeName );
+
+    ///Destructor
+    virtual ~HighlightNodeByNameVisitor();
+
+    ///Apply function that gets called during the traversal
+    ///\param node A parent node of the node being traversed
+    virtual void apply( osg::Node& node );
+
+private:
+    std::string mNodeName;///<GUID to identify the VE-Open model
+};
+}
+}
+}
+
+#endif //HIGHLIGHT_NODE_BY_NAME_VISITOR_H
