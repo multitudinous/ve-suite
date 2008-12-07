b36fb0aa47d0a0cc3b109019442ab479a1122b1a
Doug McCorkle <doug.mccorkle@gmail.com>
2008-12-07 04:48:34 +0000

    new visitor for physics
    Former-commit-id: d9a719ce287daf48319f8a59f1eddac443a52a6b

---
+++ src/ves/xplorer/scenegraph/LocalToWorldNodePath.cxx
@@ -0,0 +1,90 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2008 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+
+// --- VE-Suite Includes --- //
+#include <ves/xplorer/scenegraph/LocalToWorldNodePath.h>
+
+// --- C/C++ Libraries --- //
+#include <iostream>
+
+using namespace ves::xplorer::scenegraph;
+
+////////////////////////////////////////////////////////////////////////////////
+LocalToWorldNodePath::LocalToWorldNodePath( osg::Node* stopNode,
+                                              osg::Node* startNode )
+    :
+    NodeVisitor( TRAVERSE_ALL_CHILDREN ),
+    mStopNode( stopNode )
+{
+    startNode->accept( *this );
+}
+////////////////////////////////////////////////////////////////////////////////
+LocalToWorldNodePath::~LocalToWorldNodePath()
+{
+    ;
+}
+////////////////////////////////////////////////////////////////////////////////
+void LocalToWorldNodePath::apply( osg::Node& node )
+{
+    if( &node == mStopNode.get() )
+    {
+        osg::NodePath nodePath = getNodePath();
+        NodeAndPath nap( &node, nodePath );
+        mNaplIncludeLocal.push_back( nap );
+        for( size_t i = 0; i < nodePath.size(); ++i )
+        {
+            std::cout << "name " << nodePath.at(i)->getName() << std::endl;
+        }
+        nodePath.erase( nodePath.begin() );
+        NodeAndPath nap2( &node, nodePath );
+        mNaplExcludeLocal.push_back( nap2 );
+        for( size_t i = 0; i < nodePath.size(); ++i )
+        {
+            std::cout << "name " << nodePath.at(i)->getName() << std::endl;
+        }
+        return;
+    }
+    osg::NodeVisitor::traverse( node );
+}
+////////////////////////////////////////////////////////////////////////////////
+LocalToWorldNodePath::NodeAndPathList LocalToWorldNodePath::GetLocalToWorldNodePath(
+    bool includeLocalNode )
+{
+    if( includeLocalNode )
+    {
+        return mNaplIncludeLocal;
+    }
+
+    return mNaplExcludeLocal;
+}
+////////////////////////////////////////////////////////////////////////////////
---
+++ src/ves/xplorer/scenegraph/LocalToWorldNodePath.h
@@ -0,0 +1,96 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2008 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+
+#ifndef LOCAL_TO_WORLD_TRANSFORM_H
+#define LOCAL_TO_WORLD_TRANSFORM_H
+
+// --- VE-Suite Includes --- //
+#include <ves/VEConfig.h>
+
+// --- OSG Includes --- //
+#include <osg/ref_ptr>
+#include <osg/NodeVisitor>
+
+#include <gmtl/Matrix.h>
+
+#include <vector>
+#include <utility>
+
+namespace ves
+{
+namespace xplorer
+{
+namespace scenegraph
+{
+/*!\file LocalToWorldNodePath.h
+ */
+
+/*!\class LocalToWorldNodePath
+ *
+ */
+class VE_SCENEGRAPH_EXPORTS LocalToWorldNodePath : public osg::NodeVisitor
+{
+public:
+    typedef std::pair< osg::Node*, osg::NodePath > NodeAndPath;
+    typedef std::vector< NodeAndPath > NodeAndPathList;
+    
+    ///Constructor
+    ///\param stopNode The node with coordinate system to transform to
+    ///\param startNode The local node to transform
+    LocalToWorldNodePath( osg::Node* stopNode, osg::Node* startNode );
+
+    ///
+    virtual void apply( osg::Node& node );
+
+    ///Return by reference
+    ///\param includeLocalTransform Specifies if the local transform
+    /// should be included in the transformation matrix
+    ///\return Get the desired transformation matrix
+    NodeAndPathList GetLocalToWorldNodePath(
+        bool includeLocalNode = false );
+
+    ///Destructor
+    virtual ~LocalToWorldNodePath();
+
+private:
+    ///
+    osg::ref_ptr< osg::Node > mStopNode;
+
+    NodeAndPathList mNaplIncludeLocal;
+    NodeAndPathList mNaplExcludeLocal;
+};
+} //end scenegraph
+} //end xplorer
+} //end ves
+
+#endif //LOCAL_TO_WORLD_TRANSFORM_H
