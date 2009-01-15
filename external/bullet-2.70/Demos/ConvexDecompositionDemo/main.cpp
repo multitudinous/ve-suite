008bba0ca2a1fbb521025c5292ba6f8cb317b1b4
Doug McCorkle <doug.mccorkle@gmail.com>
2009-01-15 22:38:35 +0000

    new bullet
    Former-commit-id: d224eb8f566941007c2568de709d413e91ce2cb1

---
+++ external/bullet-2.70/Demos/ConvexDecompositionDemo/main.cpp
@@ -0,0 +1,28 @@
+
+
+#include "ConvexDecompositionDemo.h"
+#include "GlutStuff.h"
+#include "GLDebugDrawer.h"
+#include "btBulletDynamicsCommon.h"
+
+GLDebugDrawer	gDebugDrawer;
+
+int main(int argc,char** argv)
+{
+        const char* filename = "file.obj";
+
+
+        ConvexDecompositionDemo* convexDecompDemo = new ConvexDecompositionDemo();
+
+        convexDecompDemo->initPhysics(filename);
+
+		convexDecompDemo->getDynamicsWorld()->setDebugDrawer(&gDebugDrawer);
+
+
+        convexDecompDemo->clientResetScene();
+
+       
+
+        return glutmain(argc, argv,640,480,"Bullet Physics Demo. http://www.continuousphysics.com/Bullet/phpBB2/",convexDecompDemo);
+}
+
