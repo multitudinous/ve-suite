4701d2b43e213a5e9c7cf2851cab3daf24be2917
Joshua Koch <jbkoch@agsolver.com>
2009-08-22 20:06:42 +0000

    Small fixes for glow shaders
    Former-commit-id: 6253a6a7040d977db0ccf80a5fb0b71592617215

--- share/glsl/gauss_convolution_1Dx_fp.glsl
+++ share/glsl/gauss_convolution_1Dx_fp.glsl
@@ -9,15 +9,15 @@ uniform sampler2D glowMap;
 void main()
 {
     float WT9_NORMALIZE = ( WT9_0 + 2.0 * ( WT9_1 + WT9_2 + WT9_3 + WT9_4 ) );
-    vec3 outColor = texture2D( glowMap, gl_TexCoord[ 0 ].xy ).rgb * ( WT9_0 / WT9_NORMALIZE );
-    outColor     += texture2D( glowMap, gl_TexCoord[ 1 ].xy ).rgb * ( WT9_1 / WT9_NORMALIZE );
-    outColor     += texture2D( glowMap, gl_TexCoord[ 1 ].zw ).rgb * ( WT9_1 / WT9_NORMALIZE );
-    outColor     += texture2D( glowMap, gl_TexCoord[ 2 ].xy ).rgb * ( WT9_2 / WT9_NORMALIZE );
-    outColor     += texture2D( glowMap, gl_TexCoord[ 2 ].zw ).rgb * ( WT9_2 / WT9_NORMALIZE );
-    outColor     += texture2D( glowMap, gl_TexCoord[ 3 ].xy ).rgb * ( WT9_3 / WT9_NORMALIZE );
-    outColor     += texture2D( glowMap, gl_TexCoord[ 3 ].zw ).rgb * ( WT9_3 / WT9_NORMALIZE );
-    outColor     += texture2D( glowMap, gl_TexCoord[ 4 ].xy ).rgb * ( WT9_4 / WT9_NORMALIZE );
-    outColor     += texture2D( glowMap, gl_TexCoord[ 4 ].zw ).rgb * ( WT9_4 / WT9_NORMALIZE );
+    vec3 outColor = texture2D( glowMap, gl_TexCoord[ 0 ].st ).rgb * ( WT9_0 / WT9_NORMALIZE );
+    outColor     += texture2D( glowMap, gl_TexCoord[ 1 ].st ).rgb * ( WT9_1 / WT9_NORMALIZE );
+    outColor     += texture2D( glowMap, gl_TexCoord[ 1 ].pq ).rgb * ( WT9_1 / WT9_NORMALIZE );
+    outColor     += texture2D( glowMap, gl_TexCoord[ 2 ].st ).rgb * ( WT9_2 / WT9_NORMALIZE );
+    outColor     += texture2D( glowMap, gl_TexCoord[ 2 ].pq ).rgb * ( WT9_2 / WT9_NORMALIZE );
+    outColor     += texture2D( glowMap, gl_TexCoord[ 3 ].st ).rgb * ( WT9_3 / WT9_NORMALIZE );
+    outColor     += texture2D( glowMap, gl_TexCoord[ 3 ].pq ).rgb * ( WT9_3 / WT9_NORMALIZE );
+    outColor     += texture2D( glowMap, gl_TexCoord[ 4 ].st ).rgb * ( WT9_4 / WT9_NORMALIZE );
+    outColor     += texture2D( glowMap, gl_TexCoord[ 4 ].pq ).rgb * ( WT9_4 / WT9_NORMALIZE );
     
     gl_FragColor = vec4( outColor, 1.0 );
 }
