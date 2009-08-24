79bff2f3106b947a333293efa43634b78904f6c3
Jared Abodeely <jaredabo@gmail.com>
2009-08-24 21:10:49 +0000

    test to help debug issue with multiple units of the same type
    Former-commit-id: bd040223f7a205d17aff933921c18786dd91e255

---
+++ share/examples/SimpleNetwork/TestFourUnits.bat
@@ -0,0 +1,4 @@
+start Plugins\Unit\win32\NetworkOneUnit -ORBInitRef NameService=corbaloc:iiop:localhost:1239/NameService 
+start Plugins\Unit\win32\NetworkTwoUnit -ORBInitRef NameService=corbaloc:iiop:localhost:1239/NameService 
+start Plugins\Unit\win32\NetworkThreeUnit -ORBInitRef NameService=corbaloc:iiop:localhost:1239/NameService 
+start Plugins\Unit\win32\NetworkOneUnit -ORBInitRef NameService=corbaloc:iiop:localhost:1239/NameService 
