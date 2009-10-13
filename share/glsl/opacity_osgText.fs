18f4df874eaf9af02dc4f79b36e526fe89d498ee
Doug McCorkle <doug.mccorkle@gmail.com>
2009-10-13 13:39:49 +0000

    correct how opacity is controlled on the text display geodes.
    Former-commit-id: d4e59251e6796ad3310d5fa4f879a7c4c4761f99

--- share/glsl/opacity_osgText.fs
+++ share/glsl/opacity_osgText.fs
@@ -5,8 +5,11 @@ uniform vec4 glowColor;
 void main()
 {
     vec4 texColor = texture2D(tex, gl_TexCoord[0].st);
-    gl_FragData[ 0 ] = vec4( gl_Color.rgb, texColor.a * opacityVal );
-
+    //This varies opacity via the unifrom passed in
+    //gl_FragData[ 0 ] = vec4( gl_Color.rgb, texColor.a * opacityVal );
+    //This varies opacity via the SetColor interface on theosgText Geode
+    gl_FragData[ 0 ] = vec4( gl_Color.rgb, texColor.a * gl_Color.a );
+    
     //To handle the glow 
     vec4 color = glowColor;
     if( gl_Color.a < 1.0 )
