c89f08aadbf9294ef84cbf3fe9d7edc1ac70e594
Terry Jordan <Terry.Jordan@netl.doe.gov>
2010-02-01 18:44:59 +0000

    move dynsim to ves source tree
    Former-commit-id: 6a0031b7e5b6e9b11a7e4c15d51dd91d2a34a913

---
+++ share/tools/aspen/DynSimUnit/AspenIconData.h
@@ -0,0 +1,9 @@
+#ifndef ASPENICONDATA_H
+#define ASPENICONDATA_H
+
+#include <map>
+#include <string>
+
+std::map< std::string, std::pair< unsigned int, unsigned int > > GetAspenIconData();
+
+#endif
---
+++ share/tools/aspen/DynSimUnit/AspenPlusLUT.h
@@ -0,0 +1,11 @@
+#ifndef ASPENPLUSLUT_H
+#define ASPENPLUSLUT_H
+
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+std::map< std::pair< std::string, std::string >, std::vector< double > > GetAspenPlusLUT();
+
+#endif
