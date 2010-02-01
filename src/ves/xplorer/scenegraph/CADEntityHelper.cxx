79eccadb2578c08de50d4ebe185612fac7663ebc
Terry Jordan <Terry.Jordan@netl.doe.gov>
2010-02-01 20:15:54 +0000

    corrects OSG bug with flatten static transform
    Former-commit-id: 116cd67b4326fb537fb4e23e36b07fc62d9e75f2

--- src/ves/xplorer/scenegraph/CADEntityHelper.cxx
+++ src/ves/xplorer/scenegraph/CADEntityHelper.cxx
@@ -416,7 +416,8 @@ void CADEntityHelper::LoadFile( const std::string& filename,
     {
         osgUtil::Optimizer graphOpti;
         graphOpti.optimize( tempCADNode.get(), 
-                           osgUtil::Optimizer::FLATTEN_STATIC_TRANSFORMS |
+                           //Had to comment out this flag because of a bug in OSG
+						   //osgUtil::Optimizer::FLATTEN_STATIC_TRANSFORMS |
                            //osgUtil::Optimizer::REMOVE_REDUNDANT_NODES |
                            osgUtil::Optimizer::REMOVE_LOADED_PROXY_NODES |
                            osgUtil::Optimizer::COMBINE_ADJACENT_LODS |
