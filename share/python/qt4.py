6235737108c61820d59f39fe1bc3be6840db6a53
Penn Taylor <rpenn3@gmail.com>
2010-02-05 21:14:01 +0000

    Qt4 build tool for SCons
    Former-commit-id: 4c3ebcb574bcce4624fc709f71885a2ee93442d0

---
+++ share/python/qt4.py
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>301 Moved Permanently</title>
+</head><body>
+<h1>Moved Permanently</h1>
+<p>The document has moved <a href="http://clam-project.org/clam/trunk/CLAM/scons/sconstools/qt4.py">here</a>.</p>
+<hr>
+<address>Apache/2.2.3 (Debian) DAV/2 SVN/1.4.2 mod_fastcgi/2.4.2 PHP/4.4.4-8+etch6 mod_ssl/2.2.3 OpenSSL/0.9.8c Server at iua-share.upf.edu Port 80</address>
+</body></html>
