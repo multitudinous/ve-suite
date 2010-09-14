2935dc953fce4f691229d18a2f4e5e1d2fca8a7d
Doug McCorkle <doug.mccorkle@gmail.com>
2010-09-14 04:37:14 +0000

    add unix tools for the examples
    Former-commit-id: e47a29daeb5b0d3a15e2d6bc9b508fa875709126

---
+++ share/examples/SimpleNetwork/RunThreeUnits.sh
@@ -0,0 +1,3 @@
+./Plugins/Unit/macosx-10.5-i386/NetworkOneUnit -ORBInitRef NameService=corbaloc:iiop:localhost:1239/NameService &
+./Plugins/Unit/macosx-10.5-i386/NetworkTwoUnit -ORBInitRef NameService=corbaloc:iiop:localhost:1239/NameService &
+./Plugins/Unit/macosx-10.5-i386/NetworkThreeUnit -ORBInitRef NameService=corbaloc:iiop:localhost:1239/NameService &
