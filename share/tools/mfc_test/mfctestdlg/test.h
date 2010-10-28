bef903cca51782d3d5cf47b3d8fa05aed1bf4607
Terry Jordan <Terry.Jordan@netl.doe.gov>
2010-10-28 19:15:25 +0000

    Former-commit-id: c58b9db43d00cddd47cc4359d3c149a2bf0cdac3

---
+++ share/tools/mfc_test/mfctestdlg/test.h
@@ -0,0 +1,31 @@
+// test.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CtestApp:
+// See test.cpp for the implementation of this class
+//
+
+class CtestApp : public CWinApp
+{
+public:
+	CtestApp();
+
+// Overrides
+	public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CtestApp theApp;
