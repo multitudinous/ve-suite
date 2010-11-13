f35943da2c3906797ce3a663198e47b8d1f7a6fa
Doug McCorkle <doug.mccorkle@gmail.com>
2010-11-13 19:52:58 +0000

    [MFB-2.2.0-15020-15021]
    Former-commit-id: fa27288476a272b27176b50e9dadde9eeef96481

--- share/tools/aspen/VE_AspenUnit/AspenIconData.h
+++
@@ -1,9 +0,0 @@
-#ifndef ASPENICONDATA_H
-#define ASPENICONDATA_H
-
-#include <map>
-#include <string>
-
-std::map< std::string, std::pair< unsigned int, unsigned int > > GetAspenIconData();
-
-#endif
