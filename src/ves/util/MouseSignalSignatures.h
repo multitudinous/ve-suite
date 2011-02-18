52c7b4422cd71f9215ed05122e13f72db5a0dd2d
Doug McCorkle <doug.mccorkle@gmail.com>
2011-02-18 03:19:32 +0000

    added new headers to help simplify working with signals.
    Former-commit-id: 7d3a636857a94d49d614dfdba318e914306e9507

---
+++ src/ves/util/MouseSignalSignatures.h
@@ -0,0 +1,103 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2011 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+#ifndef VES_UTIL_MOUSE_SIGNAL_SIGNATURE_H
+#define VES_UTIL_MOUSE_SIGNAL_SIGNATURE_H
+
+#include <boost/signals2/signal.hpp>
+
+#include <ves/xplorer/eventmanager/BooleanPropagationCombiner.h>
+
+#include <gadget/Type/KeyboardMouse/Keys.h>
+
+namespace osg
+{
+class Vec3d;
+}
+
+namespace ves
+{
+namespace util
+{
+    /// signal for generating the start and end point for selection and other
+    ///interaction tools.
+    /// Params are: start point and end point
+    typedef boost::signals2::signal< void ( osg::Vec3d const&, osg::Vec3d const& ) > StartEndPointSignal_type;
+    
+    /// MouseMove signal
+    /// Params are: x, y, z, state (modifier mask OR'd with button mask)
+    typedef boost::signals2::signal< bool ( int, int, int, int ),
+    ves::xplorer::eventmanager::BooleanPropagationCombiner > MouseMoveSignal_type;
+    
+    /// MouseDoubleClick signal
+    /// Params are: button, x, y, z, state (modifier mask OR'd with button mask)
+    typedef boost::signals2::signal< bool ( gadget::Keys, int, int, int, int ),
+    ves::xplorer::eventmanager::BooleanPropagationCombiner > MouseDoubleClickSignal_type;
+
+    /// Scroll signal type
+    /// Params are: deltaX, deltaY, x, y, state (modifier mask OR'd with button mask)
+    typedef boost::signals2::signal< bool ( int, int, int, int, int ),
+    ves::xplorer::eventmanager::BooleanPropagationCombiner > ScrollSignal_type;
+    
+    /// ButtonPress signal type
+    /// Params are: button, x, y, state (modifier mask OR'd with button mask)
+    typedef boost::signals2::signal< bool ( gadget::Keys, int, int, int ),
+    ves::xplorer::eventmanager::BooleanPropagationCombiner > ButtonPressSignal_type;
+    
+    /// ButtonRelease signal type
+    typedef boost::signals2::signal< bool ( gadget::Keys, int, int, int ),
+    ves::xplorer::eventmanager::BooleanPropagationCombiner > ButtonReleaseSignal_type;
+    
+    /// KeyPress signal type
+    /// First arg is the key that was pressed
+    /// Second arg is the gadget::ModiferMask (modifier mask)
+    /// Third arg is the unicode representation of the key
+    ///NOTE: As soon as VR Juggler supports wide body chars we can change the 
+    ///char argument back to a wchar_t
+    typedef boost::signals2::signal< bool ( gadget::Keys, int, char ),
+    ves::xplorer::eventmanager::BooleanPropagationCombiner > KeyPressSignal_type;
+    
+    typedef boost::signals2::signal< bool ( gadget::Keys, int, char ) > KeyPressSignal_rtype;
+    
+    /// KeyRelease signal type
+    /// First arg is the key that was pressed
+    /// Second arg is the gadget::ModiferMask (modifier mask)
+    /// Third arg is the unicode representation of the key
+    ///NOTE: As soon as VR Juggler supports wide body chars we can change the 
+    ///char argument back to a wchar_t
+    typedef boost::signals2::signal< bool ( gadget::Keys, int, char ),
+    ves::xplorer::eventmanager::BooleanPropagationCombiner > KeyReleaseSignal_type;
+    
+    typedef boost::signals2::signal< bool ( gadget::Keys, int, char ) > KeyReleaseSignal_rtype;
+}
+}
+#endif
---
+++ src/ves/util/SimpleDataTypeSignalSignatures.h
@@ -0,0 +1,60 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2011 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+#ifndef VES_UTIL_SIMPLE_DATA_TYPE_SIGNAL_SIGNATURE_H
+#define VES_UTIL_SIMPLE_DATA_TYPE_SIGNAL_SIGNATURE_H
+
+#include <boost/signals2/signal.hpp>
+
+#include <ves/xplorer/eventmanager/BooleanPropagationCombiner.h>
+
+namespace ves
+{
+namespace util
+{
+    ///Signal for a String
+    typedef boost::signals2::signal< void ( std::string const& ) > StringSignal_type;
+
+    ///Signal for a bool
+    typedef boost::signals2::signal< void ( bool const& ) > BoolSignal_type;
+
+    ///Signal for double
+    typedef boost::signals2::signal< void ( double const& ) > DoubleSignal_type;
+
+    ///Signal for int
+    typedef boost::signals2::signal< void ( int const& ) > IntSignal_type;
+    
+}
+}
+#endif
+
+
