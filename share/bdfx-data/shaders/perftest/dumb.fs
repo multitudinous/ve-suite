26bbf316834ddda9d7a1cdc1b3ad67368dcb4b5b
Joshua Koch <jbkoch@agsolver.com>
2011-02-18 07:55:44 +0000

    removing old bdfx-data files
    Former-commit-id: 8b2af64041b20f9e552b069d398aaba7ef32dbaf

--- share/bdfx-data/shaders/perftest/dumb.fs
+++
@@ -1,7 +0,0 @@
-
-varying vec4 dumb_outColor;
-
-void main()
-{
-    gl_FragData[0] = dumb_outColor;
-}
--- share/bdfx-data/test-main.fs
+++
@@ -1,8 +0,0 @@
-
-varying vec4 oColor;
-
-void main( void )
-{
-    gl_FragColor = oColor;
-    //gl_FragColor = vec4( 1, 0, 0, 1 );
-}
