51476d3e1943e5262292f0bf03f376eadd2dbfb8
Penn Taylor <rpenn3@gmail.com>
2011-02-27 18:23:45 +0000

    Rolls back minor adjustment of min/max values for scalars. I assume this was a debugging test that got left in. Also removes large method that has been commented out since initial checkin.
    Former-commit-id: 752048ce871900b2c66098ca16e42044bbd574a7

--- src/ves/xplorer/data/VizBasePropertySet.cxx
+++ src/ves/xplorer/data/VizBasePropertySet.cxx
@@ -106,121 +106,6 @@ bool VizBasePropertySet::WriteToDatabase( Poco::Data::Session* const session,
     return temp;
 }
 ////////////////////////////////////////////////////////////////////////////////
-/*void VizBasePropertySet::CreateSkeleton()
-{
-    AddProperty( "DataSet", 0, "Data Set" );
-    PSVectorOfStrings enumValues;
-
-    AddProperty( "DataSet_ScalarData", 0, "Scalar Data" );
-    // Dummy value to ensure this gets set up as an enum
-    enumValues.push_back( "Select Scalar Data" );
-    SetPropertyAttribute( "DataSet_ScalarData", "enumValues", enumValues );
-    mPropertyMap["DataSet"]->SignalValueChanged.connect( boost::bind( &VizBasePropertySet::UpdateScalarDataOptions, this, _1 ) );
-
-    AddProperty( "DataSet_ScalarRange", boost::any(), "Scalar Range" );
-    SetPropertyAttribute( "DataSet_ScalarRange", "isUIGroupOnly", true );
-    SetPropertyAttribute( "DataSet_ScalarRange", "setExpanded", true );
-
-    AddProperty( "DataSet_ScalarRange_Min", 0.0, "Min" );
-    mPropertyMap["DataSet_ScalarRange_Min"]->SetDisabled();
-
-    AddProperty( "DataSet_ScalarRange_Max", 1.0, "Max" );
-    mPropertyMap["DataSet_ScalarRange_Max"]->SetDisabled();
-
-    mPropertyMap["DataSet_ScalarData"]->SignalValueChanged.connect( boost::bind( &VizBasePropertySet::UpdateScalarDataRange, this, _1 ) );
-    mPropertyMap["DataSet_ScalarRange_Min"]->SignalRequestValidation.connect( boost::bind( &VizBasePropertySet::ValidateScalarMinMax, this, _1, _2 ) );
-    mPropertyMap["DataSet_ScalarRange_Max"]->SignalRequestValidation.connect( boost::bind( &VizBasePropertySet::ValidateScalarMinMax, this, _1, _2 ) );
-
-    AddProperty( "DataSet_VectorData", 0, "Vector Data" );
-    enumValues.clear();
-    enumValues.push_back( "Select Vector Data" );
-    SetPropertyAttribute( "DataSet_VectorData", "enumValues", enumValues );
-    mPropertyMap["DataSet"]->SignalValueChanged.connect( boost::bind( &VizBasePropertySet::UpdateVectorDataOptions, this, _1 ) );
-    
-    // Now that DataSet subproperties exist, we can initialize the values in
-    // the dataset enum. If we had tried to do this beforehand, none of the
-    // connections between DataSet and its subproperties would have been in
-    // place yet.
-    enumValues.clear();
-    enumValues = ves::xplorer::data::DatabaseManager::instance()->GetStringVector( "Dataset", "Filename" );
-    if( enumValues.empty() )
-    {
-        enumValues.push_back( "No datasets loaded" );
-    }
-
-    SetPropertyAttribute( "DataSet", "enumValues", enumValues );
-    // Now that DataSet has choices loaded, force an update on the available
-    // scalar and vector data
-    PropertyPtr nullPtr;
-    UpdateScalarDataOptions( nullPtr );
-    UpdateVectorDataOptions( nullPtr );
-
-    AddProperty( "Direction", 0, "Direction" );
-    enumValues.clear();
-    enumValues.push_back( "x" );
-    enumValues.push_back( "y" );
-    enumValues.push_back( "z" );
-    enumValues.push_back( "By Wand" );
-    enumValues.push_back( "All" );
-    enumValues.push_back( "By Surface" );
-    SetPropertyAttribute( "Direction", "enumValues", enumValues );
-
-    AddProperty( "DataMapping", 0, "Data Mapping");
-    enumValues.clear();
-    enumValues.push_back( "Map Scalar Data" );
-    enumValues.push_back( "Map Volume Flux Data" );
-    SetPropertyAttribute( "DataMapping", "enumValues", enumValues );
-
-    AddProperty( "Mode", 0, "Mode" );
-    enumValues.clear();
-    enumValues.push_back( "Specify a Single Plane" );
-    enumValues.push_back( "Use All Precomputed Surfaces" );
-    SetPropertyAttribute( "Mode", "enumValues", enumValues );
-    
-    AddProperty( "UseGPUTools", false, "Use GPU Tools" );
-
-    // Connect SignalValueChanged of "Mode" to a function that enables and disables
-    // its sub-properties as appropriate
-    PropertyPtr mode = mPropertyMap["Mode"];
-    if( mode )
-    {
-        mode->SignalValueChanged.connect( boost::bind( &VizBasePropertySet::UpdateModeOptions, this, _1 ) );
-    }
-
-    AddProperty( "Mode_UseNearestPrecomputedPlane", false, "Use Nearest Precomputed Plane" );
-
-    AddProperty( "Mode_CyclePrecomputedSurfaces", false, "Cycle Precomputed Surfaces" );
-    // We disable this one by default since the selected Mode,
-    // "Specify a Single Plane", does not support this option.
-    mPropertyMap["Mode_CyclePrecomputedSurfaces"]->SetDisabled();
-
-
-    AddProperty( "PlaneLocation", 0.00, "Plane Location" );
-    SetPropertyAttribute( "PlaneLocation", "minimumValue", 0.00 );
-    SetPropertyAttribute( "PlaneLocation", "maximumValue", 100.00 );
-
-    AddProperty( "Advanced", boost::any(), "Advanced" );
-    SetPropertyAttribute( "Advanced", "isUIGroupOnly", true );
-
-    AddProperty( "Advanced_VectorThreshold", boost::any(), "Vector Threshold" );
-    SetPropertyAttribute( "Advanced_VectorThreshold", "isUIGroupOnly", true );
-    SetPropertyAttribute( "Advanced_VectorThreshold", "setExpanded", true );
-    AddProperty( "Advanced_VectorThreshold_Min",   1.0f, "Vector Threshold Min" );
-    AddProperty( "Advanced_VectorThreshold_Max", 100.0f, "Vector Threshold Max" );
-    //SetPropertyAttribute( "Advanced_VectorThreshold", "minimumValue", 0.0 );
-    //SetPropertyAttribute( "Advanced_VectorThreshold", "maximumValue", 1.0 );
-
-    AddProperty( "Advanced_VectorScale", 200.0, "Vector Scale" );
-    SetPropertyAttribute( "Advanced_VectorScale", "minimumValue",   1.0 );
-    SetPropertyAttribute( "Advanced_VectorScale", "maximumValue", 400.0 );
-
-    AddProperty( "Advanced_VectorRatio", 1.0, "Vector Ratio" );
-    SetPropertyAttribute( "Advanced_VectorRatio", "minimumValue",   1.0 );
-    SetPropertyAttribute( "Advanced_VectorRatio", "maximumValue", 200.0 );
-
-    AddProperty( "Advanced_ScaleByVectorMagnitude", false, "Scale By Vector Magnitude" );
-}*/
-////////////////////////////////////////////////////////////////////////////////
 void VizBasePropertySet::UpdateScalarDataOptions( PropertyPtr property )
 {
     boost::ignore_unused_variable_warning( property );
@@ -279,8 +164,8 @@ void VizBasePropertySet::UpdateScalarDataRange( PropertyPtr property )
         SetPropertyAttribute( "DataSet_ScalarRange_Max", "maximumValue", max );
 
         // Set min and max to the lower and upper boundary values, respectively
-        bool success = SetPropertyValue( "DataSet_ScalarRange_Min", min + 0.00001 );
-        success = SetPropertyValue( "DataSet_ScalarRange_Max", max - 0.00001 );
+        bool success = SetPropertyValue( "DataSet_ScalarRange_Min", min );
+        success = SetPropertyValue( "DataSet_ScalarRange_Max", max );
     }
 }
 ////////////////////////////////////////////////////////////////////////////////
