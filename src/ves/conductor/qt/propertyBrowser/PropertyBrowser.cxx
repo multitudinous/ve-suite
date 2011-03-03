93e30661a5fe57e58e150336ebc9b7c0203e5b8e
Penn Taylor <rpenn3@gmail.com>
2011-03-03 05:41:34 +0000

    Turns off extra debug logging info in this class.
    Former-commit-id: c1900aaee8198c8acf249625535a7aedeed8f532

--- src/ves/conductor/qt/propertyBrowser/PropertyBrowser.cxx
+++ src/ves/conductor/qt/propertyBrowser/PropertyBrowser.cxx
@@ -30,7 +30,6 @@
  * -----------------------------------------------------------------
  *
  *************** <auto-copyright.rb END do not edit this line> ***************/
-#define VES_DEBUG
 #include <ves/conductor/qt/propertyBrowser/PropertyBrowser.h>
 
 #include <gmtl/Math.h>
