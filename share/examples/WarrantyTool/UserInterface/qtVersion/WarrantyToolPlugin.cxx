1ee32041e5feec6cdf028a7e2fe2d814f5f1a22b
Penn Taylor <rpenn3@gmail.com>
2011-08-04 21:04:19 +0000

    Initial checkin of Qt version of WarrantyTool plugin UI.
    Former-commit-id: 39d2ef8142c819fa3736719223fd6719fa027590

---
+++ share/examples/WarrantyTool/UserInterface/qtVersion/WarrantyToolPlugin.cxx
@@ -0,0 +1,109 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2011 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+//#include <ves/conductor/qt/WarrantyToolPlugin/WarrantyToolPlugin.h>
+//#include <ves/conductor/qt/WarrantyToolPlugin/WarrantyToolPlugin_UIDialog.h>
+#include "WarrantyToolPlugin.h"
+#include "WarrantyToolPlugin_UIDialog.h"
+
+#include <osg/Geode>
+
+namespace ves
+{
+namespace conductor
+{
+
+
+WarrantyToolPlugin::WarrantyToolPlugin():
+    m_UIWidget(0),
+    m_ResultWidget(0),
+    m_FinancialDataWidget(0),
+    m_InputVariablesWidget(0),
+    m_ResultsVariablesWidget(0),
+    m_name("Deere Analytics")
+{
+    m_UIWidget = new WarrantyToolPlugin_UIDialog( 0 );
+}
+
+void WarrantyToolPlugin::SetName( const std::string& pluginName )
+{
+    m_name = pluginName;
+}
+
+
+std::string WarrantyToolPlugin::GetName()
+{
+    return m_name;
+}
+
+
+QWidget* WarrantyToolPlugin::GetUIWidget( QWidget* parent )
+{
+    return m_UIWidget;
+}
+
+
+QWidget* WarrantyToolPlugin::GetResultWidget( QWidget* parent )
+{
+    return m_ResultWidget;
+}
+
+
+QWidget* WarrantyToolPlugin::GetFinancialDataWidget()
+{
+    return m_FinancialDataWidget;
+}
+
+
+QWidget* WarrantyToolPlugin::ViewInputVariables( QWidget* parent )
+{
+    return m_InputVariablesWidget;
+}
+
+QWidget* WarrantyToolPlugin::ViewResultsVariables( QWidget* parent )
+{
+    return m_ResultsVariablesWidget;
+}
+
+void WarrantyToolPlugin::DeleteWidgets()
+{
+    delete m_UIWidget;
+    delete m_ResultWidget;
+    delete m_FinancialDataWidget;
+    delete m_InputVariablesWidget;
+    delete m_ResultsVariablesWidget;
+    m_UIWidget = m_ResultWidget = m_FinancialDataWidget =
+                 m_InputVariablesWidget = m_ResultsVariablesWidget = 0;
+}
+
+}
+}
---
+++ share/examples/WarrantyTool/UserInterface/qtVersion/WarrantyToolPluginFactory.cxx
@@ -0,0 +1,84 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2011 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+#include "WarrantyToolPluginFactory.h"
+#include "WarrantyToolPlugin.h"
+
+namespace ves
+{
+namespace conductor
+{
+
+UIPluginInterface* WarrantyToolPluginFactory::CreateInstance()
+{
+    return new WarrantyToolPlugin;
+}
+
+std::string WarrantyToolPluginFactory::GetFactoryName()
+{
+    return std::string( "DeereAnalytics" );
+}
+
+double WarrantyToolPluginFactory::GetVersion()
+{
+    return 1.0;
+}
+
+
+std::string WarrantyToolPluginFactory::GetDescription()
+{
+    return std::string( "The generic tool to display product data." );
+}
+
+
+std::string WarrantyToolPluginFactory::GetHelpURL()
+{
+    return std::string( "" );
+}
+
+
+QIcon WarrantyToolPluginFactory::GetIcon( )
+{
+    QIcon result(":/Icons/TractorIcon.png");
+    return result;
+}
+
+osg::Geode* WarrantyToolPluginFactory::GetGeometry()
+{
+    // We don't have custom geometry yet, so just return a null ptr.
+    return 0;
+}
+
+}
+}
+
+Q_EXPORT_PLUGIN2( WarrantyToolPluginFactory, ves::conductor::WarrantyToolPluginFactory );
