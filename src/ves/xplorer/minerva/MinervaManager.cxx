fd9dd6a172c450bc4706c8f1d43716e03b75bb92
Doug McCorkle <doug.mccorkle@gmail.com>
2011-08-16 17:30:14 +0000

    more minerva rendering updates to use the default render bin for the raster layers of minerva
    Former-commit-id: 77131d8717b8a5a9a897fe8179d90360fa9143e4

--- src/ves/xplorer/minerva/MinervaManager.cxx
+++ src/ves/xplorer/minerva/MinervaManager.cxx
@@ -276,7 +276,7 @@ void MinervaManager::AddEarthToScene()
   _scene = _body->scene();
 
     osg::ref_ptr< osg::StateSet > tempStateSet = _scene->getOrCreateStateSet();
-    tempStateSet->setRenderBinDetails ( 0, "RenderBin" );
+    //tempStateSet->setRenderBinDetails ( 0, "RenderBin" );
 
     std::string shaderName = osgDB::findDataFile( "null_glow.fs" );
     osg::ref_ptr< osg::Shader > fragShader = 
