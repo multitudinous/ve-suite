e6b56c9de0197cb23b911827b46fe723a9b5dff0
Doug McCorkle <doug.mccorkle@gmail.com>
2011-11-15 23:26:49 +0000

    code cleanup
    Former-commit-id: 21d9ee77b710d53212fb205194a8cdef88aff631

--- src/ves/conductor/qt/NetworkLoader.cxx
+++ src/ves/conductor/qt/NetworkLoader.cxx
@@ -146,15 +146,7 @@ void NetworkLoader::LoadVesFile( const std::string& fileName )
         reinterpret_cast< xplorer::eventmanager::SignalWrapper< ves::util::StringSignal_type >* >
         ( xplorer::eventmanager::EventFactory::instance()->GetSignal( "VesFileLoading" ) )
         ->mSignal->operator()( m_filename );
-/*        CommandPtr loadVesFile( new Command() );
-        loadVesFile->SetCommandName( "LOAD_VES_FILE" );
-        // Dummy DVP to prevent crashes since xplorer assumes existence of 
-        // valid DVP without testing.
-        DataValuePairPtr nullDVP( new DataValuePair() );
-        nullDVP->SetData( "LOAD_VES_FILE", "NULL" );
-        loadVesFile->AddDataValuePair( nullDVP );
-        ves::xplorer::command::CommandManager::instance( )->AddXMLCommand( loadVesFile );
-*/
+
         //Send a new start position for all apps
         //do this before loading the ves data
         // so in case a file has a start position it will be used
@@ -191,30 +183,6 @@ void NetworkLoader::LoadVesFile( const std::string& fileName )
         ves::xplorer::command::CommandManager::instance( )->AddXMLCommand( centerPointUpdateData );
     }
 
-//+!
-    //Reloading plugins
-    //av_modules->ResetPluginTree();
-    // RPT: The following block appears to be the key code from AvailableModules::ResetPluginTree
-    /*DataValuePairPtr dvp( new DataValuePair( std::string( "STRING" ) ) );
-    dvp->SetData( "Reload_Plugin_Objects", "Reload" );
-    ves::open::xml::CommandPtr vec( new ves::open::xml::Command() );
-    vec->SetCommandName( "Plugin_Control" );
-    vec->AddDataValuePair( dvp );
-    ves::xplorer::command::CommandManager::instance( )->AddXMLCommand( vec );*/
-//-!
-
-//+!
-//     if( newCanvas )
-//     {
-//         //clear any current tree
-//         hierarchyTree->Clear();
-//         SetTitle( _( "VE-Suite: www.vesuite.org" ) );
-//         canvas->CreateDefaultNetwork();
-//     }
-//     else
-    //{
-    //Now load the xml data now that we are in the correct directory
-    //canvas->PopulateNetworks( fileName );
     // RPT: Following line replaces key functionality of canvas->PopulateNetworks
     XMLDataBufferEngine::instance()->LoadVESData( m_filename );
 
@@ -266,33 +234,11 @@ void NetworkLoader::LoadVesFile( const std::string& fileName )
         //AspenSimOpen = true;
     }
 
-//         wxCommandEvent submitEvent;
-//         SubmitToServer( submitEvent );
-// RPT: Following block duplicates key code from SubmitToServer
-//{
     const std::string nw_str = XMLDataBufferEngine::instance()->
                     SaveVESData( std::string( "returnString" ) );
-    //serviceList->SetNetwork( nw_str ); <-- old conductor
-    //ves::xplorer::network::GraphicalPluginManager::instance()->
-    //              GetCORBAInterface()->SetNetworkString( nw_str );
-    // <-- CORBA version with Qt
+
     ves::xplorer::network::GraphicalPluginManager::instance()->
             SetCurrentNetwork( nw_str );
-
-    /*std::vector< double > xplorerColor;
-    xplorerColor.push_back( 0.0 );
-    xplorerColor.push_back( 0.0 );
-    xplorerColor.push_back( 0.0 );
-    xplorerColor.push_back( 1.0 );
-    */
-
-    /*DataValuePairPtr dataValuePair( new DataValuePair() );
-    dataValuePair->SetData( std::string( "Load Data" ), xplorerColor );
-    CommandPtr veCommand( new Command() );
-    veCommand->SetCommandName( std::string( "veNetwork Update" ) );
-    veCommand->AddDataValuePair( dataValuePair );
-    ves::xplorer::command::CommandManager::instance( )->AddXMLCommand( veCommand );
-    */
 
     // This signal replaces the above block. xplorerColor doesn't appear to be
     // processed anywhere, so was left out of the signal.
@@ -321,15 +267,6 @@ void NetworkLoader::LoadVesFile( const std::string& fileName )
             ( eventmanager::EventFactory::instance()->GetSignal( "ChangeActiveModel" ) )
             ->mSignal->operator()( modelID );
     }
-
-           //}
-//         
-//         if( recordScenes )
-//         {
-//             recordScenes->_buildPage();
-//         }
-    //}
-//-!
     
     //Now manage the data that is user specific to this ves file
     UserPtr userInfo = 
@@ -345,15 +282,6 @@ void NetworkLoader::LoadVesFile( const std::string& fileName )
         backgroundColor.push_back( 0.0f );
         backgroundColor.push_back( 0.0f );
         backgroundColor.push_back( 1.0f );
-        /*
-        DataValuePairPtr dataValuePair( new DataValuePair() );
-        dataValuePair->SetData( std::string( "Background Color" ), backgroundColor );
-        CommandPtr veCommand( new Command() );
-        veCommand->SetCommandName( std::string( "CHANGE_BACKGROUND_COLOR" ) );
-        veCommand->AddDataValuePair( dataValuePair );
-        UserPreferencesDataBuffer::instance()->
-            SetCommand( std::string( "CHANGE_BACKGROUND_COLOR" ), veCommand );
-        */
         
         reinterpret_cast< eventmanager::SignalWrapper< ves::util::BoolAndDoubleVectorSignal_type >* >
         ( eventmanager::EventFactory::instance()->GetSignal( "PreferencesPropertySet.UsePreferredBackgroundColor" ) )
@@ -419,32 +347,31 @@ void NetworkLoader::LoadVesFile( const std::string& fileName )
     
     // Initialze Minerva.
     {
-      CommandPtr earthCommand ( UserPreferencesDataBuffer::instance()->GetCommand ( ves::util::commands::ADD_EARTH_COMMAND_NAME ) );
-      if ( earthCommand )
-      {
-        ves::xplorer::command::CommandManager::instance( )->AddXMLCommand( earthCommand );
-
-        CommandPtr rasterGroupCommand ( UserPreferencesDataBuffer::instance()->GetCommand ( ves::util::commands::ADD_RASTER_GROUP ) );
-        CommandPtr elevationGroupCommand ( UserPreferencesDataBuffer::instance()->GetCommand ( ves::util::commands::ADD_ELEVATION_GROUP ) );
-
-        if ( rasterGroupCommand && rasterGroupCommand->GetNumberOfDataValuePairs() > 0  )
+        CommandPtr earthCommand( UserPreferencesDataBuffer::instance()->GetCommand( ves::util::commands::ADD_EARTH_COMMAND_NAME ) );
+        if( earthCommand )
         {
-          ves::xplorer::command::CommandManager::instance( )->AddXMLCommand( rasterGroupCommand );
-        }
+            ves::xplorer::command::CommandManager::instance()->AddXMLCommand( earthCommand );
 
-        if ( elevationGroupCommand && elevationGroupCommand->GetNumberOfDataValuePairs() > 0 )
-        {
-          ves::xplorer::command::CommandManager::instance( )->AddXMLCommand( elevationGroupCommand );
-        }
+            CommandPtr rasterGroupCommand( UserPreferencesDataBuffer::instance()->GetCommand( ves::util::commands::ADD_RASTER_GROUP ) );
+            CommandPtr elevationGroupCommand( UserPreferencesDataBuffer::instance()->GetCommand( ves::util::commands::ADD_ELEVATION_GROUP ) );
 
-        //MinervaDialog* dialog ( this->GetMinervaDialog() );
-        //dialog->InitalizeFromCommands ( elevationGroupCommand, rasterGroupCommand );
-      }
+            if( rasterGroupCommand && rasterGroupCommand->GetNumberOfDataValuePairs() > 0  )
+            {
+                ves::xplorer::command::CommandManager::instance()->AddXMLCommand( rasterGroupCommand );
+            }
+
+            if( elevationGroupCommand && elevationGroupCommand->GetNumberOfDataValuePairs() > 0 )
+            {
+                ves::xplorer::command::CommandManager::instance()->AddXMLCommand( elevationGroupCommand );
+            }
+
+            //MinervaDialog* dialog ( this->GetMinervaDialog() );
+            //dialog->InitalizeFromCommands ( elevationGroupCommand, rasterGroupCommand );
+        }
     }
 
     OnActiveModelChanged( "null" );
-
-   }
+}
 
 // At the moment, this shouldn't need to be a separate function since we're
 // no longer connecting to the ActiveModelChanged signal. Leaving it as-is
