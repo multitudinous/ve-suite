1018f0acbfeeec9ac9bded21e2eb3d935fd19e69
Doug McCorkle <doug.mccorkle@gmail.com>
2012-01-03 02:37:08 +0000

    update copyright
    Former-commit-id: 60ea585879f0054e7157dd221ce7d9a2898fe8e5

--- src/ves/xplorer/volume/NoiseTexture2D.cxx
+++ src/ves/xplorer/volume/NoiseTexture2D.cxx
@@ -1,12 +1,11 @@
 
 /*************** <auto-copyright.rb BEGIN do not edit this line> **************
  *
- * VE-Suite is (C) Copyright 1998-2010 by Iowa State University
+ * VE-Suite is (C) Copyright 1998-2011 by Iowa State University
  *
  * Original Development Team:
  *   - ISU's Thermal Systems Virtual Engineering Group,
  *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
- *   - Reaction Engineering International, www.reaction-eng.com
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
