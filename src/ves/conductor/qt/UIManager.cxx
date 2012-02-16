d250f4d5fcb65a1ba930102dff68c09dc2932ce3
Doug McCorkle <doug.mccorkle@gmail.com>
2012-02-16 18:54:01 +0000

    added more debug output.
    Former-commit-id: a768f9366ad4f04d73ea9c1bc936dfeeaef43bcc

--- src/ves/conductor/qt/UIManager.cxx
+++ src/ves/conductor/qt/UIManager.cxx
@@ -1058,6 +1058,8 @@ bool UIManager::ButtonPressEvent( gadget::Keys button, int x, int y, int state )
     if( ( visible ) && ( !minimized ) )
     {
         //LOG_INFO( "UIManager::ButtonPressEvent point " << m_intersectionPoint.x() << " " << m_intersectionPoint.z() );
+        //LOG_INFO( "UIManager::ButtonPressEvent start point " << m_startPoint.x() << " " << m_startPoint.y() << " " << m_startPoint.z() );
+        //LOG_INFO( "UIManager::ButtonPressEvent end point " << m_endPoint.x() << " " << m_endPoint.y() << " " << m_endPoint.z() );
         // Translate mouse coordinates to window coordinates
         m_selectedUIElement->GetPointIntersectionInPixels( x, y, m_intersectionPoint );
         // Flip y mouse coordinate to origin GUI expects
