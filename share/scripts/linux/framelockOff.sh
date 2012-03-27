09712fb4a496b19bb5b2ae8ec8e0075daca76394
Doug McCorkle <doug.mccorkle@gmail.com>
2012-03-27 18:44:06 +0000

    control framelock on linux.
    Former-commit-id: 39449ecd2cc64e0a3c3907ef1b1837e1b95a894c

---
+++ share/scripts/linux/framelockOff.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+${SHELL} << eof
+source /ves/VE_Software/VE_Suite/ves.setup.sh
+framelock --off
+sleep 5
+framelock --off
+sleep 5
+eof
+exit
---
+++ share/scripts/linux/framelockOn.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+${SHELL} << eof
+source /ves/VE_Software/VE_Suite/ves.setup.sh
+framelock --on
+sleep 15
+eof
+exit
