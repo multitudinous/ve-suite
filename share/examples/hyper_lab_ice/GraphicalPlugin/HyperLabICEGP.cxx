5638420b9cfe0e15bd9927de3e4187f44065d228
Doug McCorkle <doug.mccorkle@gmail.com>
2012-07-22 22:20:09 +0000

    update the hyper lab model to reflect the final models.
    Former-commit-id: fc8a92e77a4ee93d66c43ca3073583e5fbbc2dc3

--- share/examples/hyper_lab_ice/GraphicalPlugin/HyperLabICEGP.cxx
+++ share/examples/hyper_lab_ice/GraphicalPlugin/HyperLabICEGP.cxx
@@ -68,93 +68,19 @@ extern osg::ref_ptr<osg::Texture2D> RTTtex;
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-#define NETL_DEMO
+//#define NETL_DEMO
 
 #include "HyperLabICEGP.h"
 #ifdef NETL_DEMO
 #include "RenderPrep.h"
 #endif
 
-#include <ves/xplorer/scenegraph/physics/PhysicsSimulator.h>
-
-#include <osgbDynamics/MotionState.h>
-#include <osgbDynamics/CreationRecord.h>
-#include <osgbDynamics/RigidBody.h>
-#include <osgbCollision/CollisionShapes.h>
-#include <osgbCollision/RefBulletObject.h>
-#include <osgbDynamics/GroundPlane.h>
-#include <osgbCollision/GLDebugDrawer.h>
-#include <osgbCollision/Utils.h>
-#include <osgbInteraction/DragHandler.h>
-#include <osgbInteraction/LaunchHandler.h>
-#include <osgbInteraction/SaveRestoreHandler.h>
-
-#include <btBulletDynamicsCommon.h>
-
-#include <osgwTools/InsertRemove.h>
-#include <osgwTools/FindNamedNode.h>
-#include <osgwTools/GeometryOperation.h>
-#include <osgwTools/GeometryModifier.h>
-#include <osgwTools/Shapes.h>
-
 using namespace Poco::Data;
 using namespace ves::xplorer::scenegraph;
 using namespace warrantytool;
 
-//
-// BEGIN WALL FIX
-//
-
-// The input model consists of two separate walls. However, the OSG scene
-// graph for this is a single Geode with a single Geometry and a single
-// QUADS PrimitiveSet. Our app needs to make this into two separate static
-// collision shapes. One way to handle this situation would be to parse the
-// geometry data and code directly to the Bullet API.
-//
-// Howver, for this example, I have instead chosen to sacrifice a little bit
-// of rendering efficiency by fixing the scene graph, which will allow the
-// example to use osgbDynamics::createRigidBody() to automatically generate
-// the collision shapes. In order for this to work, the scene graph must contain
-// two Geodes, each with its own Geometry and PrimitiveSet. This allows the
-// osgBullet rigid body create code to simple create one rigid body for
-// each branch of the graph.
-//
-// The FindGeomOp is a GeometryOperation that returns a reference to the last
-// Geometry found, which is all we need to locate the Geometry in question in
-// this branch of the scene graph. The FixWalls function makes a copy of this
-// scene graph branch, then uses FindGeomOp to locate the Geometry, then
-// modifies the PrimitiveSet on each to only reference the vertices needed for
-// each wall segment.
-//
-// Obviously, this code is very model specific, and is not intended for
-// re-use with other models. Therefore I have wrapped it with BEGIN WALL FIX
-// and END WALL FIX.
-
-/* \cond */
-class FindGeomOp : public osgwTools::GeometryOperation
-{
-public:
-    FindGeomOp() {}
-    FindGeomOp( const FindGeomOp& rhs, const osg::CopyOp& copyOp=osg::CopyOp::SHALLOW_COPY ) 
-        :
-        osgwTools::GeometryOperation( rhs )
-        {;}
-
-    META_Object(osgBulletExamples,FindGeomOp);
-    
-    virtual osg::Geometry* operator()( osg::Geometry& geom )
-    {
-        _target = &geom;
-        return( &geom );
-    }
-    
-    osg::ref_ptr< osg::Geometry > _target;
-};
-/* \endcond */
-
 #define CM2FEET 0.0328
 
-////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 HyperLabICEGP::HyperLabICEGP()
     :
@@ -166,10 +92,10 @@ HyperLabICEGP::HyperLabICEGP()
 ////////////////////////////////////////////////////////////////////////////////
 HyperLabICEGP::~HyperLabICEGP()
 {
+    ;
 }
 ////////////////////////////////////////////////////////////////////////////////
-void HyperLabICEGP::InitializeNode(
-    osg::Group* veworldDCS )
+void HyperLabICEGP::InitializeNode( osg::Group* veworldDCS )
 {
     PluginBase::InitializeNode( veworldDCS );
     
@@ -178,8 +104,8 @@ void HyperLabICEGP::InitializeNode(
 ////////////////////////////////////////////////////////////////////////////////
 int HyperLabICEGP::InitializeLabModels()
 {    
-    btDiscreteDynamicsWorld* bulletWorld = 
-        dynamic_cast< btDiscreteDynamicsWorld* >( mPhysicsSimulator->GetDynamicsWorld() );
+    //btDiscreteDynamicsWorld* bulletWorld = 
+    //    dynamic_cast< btDiscreteDynamicsWorld* >( mPhysicsSimulator->GetDynamicsWorld() );
     osg::Group* root = new osg::Group();
 
     mDCS->addChild( root );
@@ -192,15 +118,13 @@ int HyperLabICEGP::InitializeLabModels()
     if( !( models.valid() ) )
     {
         osg::notify( osg::FATAL ) << "Can't open model file(s)." << std::endl;
-        //return( 1 );
     }
     renderRoot->addChild( models.get() );
 
-    models = osgDB::readNodeFile( "Models/HyperLab_Facility_v5.osg", options.get() );
+    models = osgDB::readNodeFile( "Models/HyperLab_Facility_v6.osg", options.get() );
     if( !( models.valid() ) )
     {
         osg::notify( osg::FATAL ) << "Can't open model file(s)." << std::endl;
-        //return( 1 );
     }
     renderRoot->addChild( models.get() );
 
@@ -208,7 +132,6 @@ int HyperLabICEGP::InitializeLabModels()
     if( !( models.valid() ) )
     {
         osg::notify( osg::FATAL ) << "Can't open model file(s)." << std::endl;
-        //return( 1 );
     }
     renderRoot->addChild( models.get() );
 
@@ -221,32 +144,18 @@ int HyperLabICEGP::InitializeLabModels()
 #endif
     }
 
-    {
-        //util::MaterialPresent materialPresent( equipment.get() );
-    }
-    /*osg::ref_ptr< osg::Node > collisions = osgDB::readNodeFile( "Models/HyperLab_Collision_v1.osg", options.get() );
-    if( !( collisions.valid() ) )
-    {
-        osg::notify( osg::FATAL ) << "Can't open model file(s)." << std::endl;
-        return( 1 );
-    }*/
-
     // Scale to feet.
     {
         osg::ref_ptr< osg::MatrixTransform > mt = new osg::MatrixTransform(
             osg::Matrix::scale( osg::Vec3( CM2FEET, CM2FEET, CM2FEET ) ) );
         mt->setDataVariance( osg::Object::STATIC );
         mt->addChild( renderRoot.get() );
-        //mt->addChild( equipment.get() );
-        //mt->addChild( collisions.get() );
         
         osgUtil::Optimizer optimizer;
         optimizer.optimize( mt.get(), osgUtil::Optimizer::FLATTEN_STATIC_TRANSFORMS );
     }
     
     root->addChild( renderRoot.get() );
-    //root->addChild( equipment.get() );
-    //root->addChild( collisions.get() );
 
     std::cout << "Loaded all of the models and physics for the Gate." << std::endl;
     return( 0 );
