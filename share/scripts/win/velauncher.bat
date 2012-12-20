30603c5657a52f3a10eb4bc55a3f68702c5c0156
Doug McCorkle <doug.mccorkle@gmail.com>
2012-12-20 20:17:19 +0000

    update the path with the osgplugins directory.
    Former-commit-id: 6f46134185ea7a5219975cceba3815840fd7af22

--- share/scripts/win/velauncher.bat
+++ share/scripts/win/velauncher.bat
@@ -2,5 +2,5 @@
 set OSG_FILE_PATH=%CD%/share/vesuite;%CD%/share/backdropFX/data
 set OSG_FILE_PATH=%OSG_FILE_PATH%;%CD%/share/osgBullet/data;%CD%/share/osgWorks/data
 
-set Path=%CD%/bin;%CD%/lib;%CD%/lib64;%CD%;%Path%
+set Path=%CD%/bin;%CD%/lib;%CD%/lib/osgPLugins-2.8.5;%CD%/lib64;%Path%
 ves_xplorer --jconf=%CD%/share/vesuite/vrj_configs/stereo_desktop/desktop.jconf --VESDesktopMode --VESRTT
