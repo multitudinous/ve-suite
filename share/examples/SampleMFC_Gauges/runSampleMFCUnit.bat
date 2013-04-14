c9a2563d6ab0443badf9eedad368f491cb30a764
Doug McCorkle <doug.mccorkle@gmail.com>
2013-04-14 03:02:50 +0000

    remove unused example.
    Former-commit-id: f658620462f84940ca964be0ce6ce89e0cb39f63

--- share/examples/SampleMFC_Gauges/runSampleMFCUnit.bat
+++
@@ -1,7 +0,0 @@
-REM Specify the environment variables
-call .\setup.bat
-
-cd C:\TSVEG\VE_Suite\examples\SampleMFC_Gauges\Units
-
-start SampleMFCUnit.exe -ORBInitRef NameService=corbaloc:iiop:%TAO_MACHINE%:%TAO_PORT%/NameService -ORBDottedDecimalAddresses 1 
-cmd
