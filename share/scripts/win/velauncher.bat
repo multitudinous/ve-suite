25ac703b821fa2fe2102dd3131157b74353dfc76
Doug McCorkle <doug.mccorkle@gmail.com>
2013-06-16 23:02:05 +0000

    correct issue where lfx shaders cannot be found
    Former-commit-id: 08193f295a2356722749aebf726acfd12c2b4938

--- share/scripts/win/velauncher.bat
+++ share/scripts/win/velauncher.bat
@@ -1,6 +1,9 @@
 @echo OFF
-set OSG_FILE_PATH=%CD%/share/vesuite;%CD%/share/backdropFX/data
-set OSG_FILE_PATH=%OSG_FILE_PATH%;%CD%/share/osgBullet/data;%CD%/share/osgWorks/data
+set OSG_FILE_PATH=%CD%/share/vesuite
+set OSG_FILE_PATH=%OSG_FILE_PATH%;%CD%/share/backdropFX/data
+set OSG_FILE_PATH=%OSG_FILE_PATH%;%CD%/share/osgBullet/data
+set OSG_FILE_PATH=%OSG_FILE_PATH%;%CD%/share/osgWorks/data
+set OSG_FILE_PATH=%OSG_FILE_PATH%;%CD%/share/latticefx/data
 
 set Path=%CD%/bin;%CD%/lib;%CD%/lib/osgPLugins-2.8.5;%CD%/lib64;%Path%
 ves_xplorer --jconf=%CD%/share/vesuite/vrj_configs/stereo_desktop/desktop.jconf --VESDesktopMode --VESRTT
