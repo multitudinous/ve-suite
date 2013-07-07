c172dd7f46ee260e0c3e77ca5d57275bcb63c160
Doug McCorkle <doug.mccorkle@gmail.com>
2013-07-07 03:09:44 +0000

    remove unneeded code.
    Former-commit-id: 3739ea6daf52bd5458f8dbe1317dbde022b89dee

--- src/ves/web/VE_WebClient/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	// Kill all values in the session cookie.
-
-	session_start();
-	session_unset();
-	header ("location: index.php?logout");
-
-?>
--- src/ves/web/VE_WebClient/vv/database_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-	
-	@mysql_pconnect("vracs001.vrac.iastate.edu", "tsvegweb", "web99") or die ("Could not connect to server.");
-	@mysql_select_db("tsveg") or die("Could not select database.");
-
-?>
