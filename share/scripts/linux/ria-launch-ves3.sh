05f78e148507907a3f1c4d293436f9ad59b33f90
Karl Svec <karl@agsolver.com>
2014-07-24 11:49:39 -0500

    add example VES3 launcher script for RIA

---
+++ share/scripts/linux/ria-launch-ves3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /ves/path/to/ves-env.sh
+ves_xplorer --jconf=/ves/system_configs/cave_configs_optitrak_active/vecr_optitrak_active.jconf --VESRTT --DesktopClusterControl --DesktopWindowName "RIA IEL Preview"
